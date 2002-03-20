110b05e1d0d1d9e18d8ec7e5bcd1c7187eb33b6c
fil <fil@rezo.net>
2002-03-20 10:58:04 +0000

    essais...
    git-svn-id: svn://trac.rezo.net/spip/spip@134 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/test_cvs.php
+++
@@ -1,8 +0,0 @@
-<?  // test cvs version auto 
-
-	// dernier tag connu ou date si cvs
-	if (! ereg("Name: v(.*) ","$Name$", $regs))
-		ereg("(20../../.. ..:..:..)", "$Id$", $regs);
-	$spip_version_affichee = $regs[1];
-
-?>

5fa8f7ceba67a1c944b83954ab2db5bfc51ca3a2
fil <fil@rezo.net>
2003-06-26 18:58:29 +0000

    test cvs
    git-svn-id: svn://trac.rezo.net/spip/spip@1875 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- oo/index.php
+++ oo/index.php
@@ -2,7 +2,7 @@
 
 // ACCESSIBILITE
 // la page /oo offre une lecture en mode "texte seul"
-// des 3 articles les plus recents.
+// des 3 articles les plus recents...
 
 @header("Location: ../sommaire_texte.php3");
 

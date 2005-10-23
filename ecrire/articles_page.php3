987b7c8636dd1255db8348acccfc6bfa5bc4198b
esj <esj@rezo.net>
2005-10-23 13:42:46 +0000

    surcharche possible du filtre articles_page. Remarque: a mon avis ce script est redondant avec index.php3 (i.e. inc_accueil) et devrait etre remplace par une redirection vers lui)
    git-svn-id: svn://trac.rezo.net/spip/spip@4906 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/articles_page.php3
+++ ecrire/articles_page.php3
@@ -10,79 +10,22 @@
  *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
 \***************************************************************************/
 
-
 include ("inc.php3");
 
-debut_page(_T('titre_page_articles_page'), "documents", "articles");
+// prendre $var_* comme variables pour eviter les conflits avec les http_vars
 
-debut_gauche();
+$var_nom = "articles_page";
+$var_f = find_in_path('inc_' . $var_nom . '.php');
 
-//
-// Afficher le bouton de creation d'article
-//
+if ($var_f) 
+  include($var_f);
+elseif (file_exists($var_f = (_DIR_INCLUDE . 'inc_' . $var_nom . '.php')))
+  include($var_f);
 
-$query = "SELECT id_rubrique FROM spip_rubriques LIMIT 1";
-$result = spip_query($query);
-
-if (spip_num_rows($result) > 0) {
-	debut_raccourcis();
-	icone_horizontale (_T('icone_ecrire_article'), "articles_edit.php3?new=oui", "article-24.gif", "creer.gif");
-	fin_raccourcis();
-}
-else {
-	if ($connect_statut == '0minirezo') {
-		echo _T('texte_creer_rubrique');
-		icone_horizontale (_T('icone_creer_rubrique'), "rubriques_edit.php3?new=oui&retour=nav", "rubrique-24.gif", "creer.gif");
-	}
-}
-
-debut_droite();
-
-
-
-
-//
-// Vos articles en cours de redaction
-//
-
-echo "<P align=left>";
-afficher_articles(_T('info_en_cours_validation'),
-	", spip_auteurs_articles AS lien ".
-	"WHERE articles.id_article=lien.id_article AND lien.id_auteur=$connect_id_auteur AND articles.statut='prepa' ORDER BY articles.date DESC");
-
-
-
-
-//
-// Vos articles soumis au vote
-//
-
-echo "<p>";
-afficher_articles(_T('info_attente_validation'),
-	", spip_auteurs_articles AS lien ".
-	"WHERE articles.id_article=lien.id_article AND lien.id_auteur=$connect_id_auteur AND articles.statut='prop' ORDER BY articles.date");
-
-
-//
-// Vos articles publies
-//
-
-echo "<p>";
-afficher_articles(_T('info_publies'),
-	", spip_auteurs_articles AS lien ".
-	"WHERE articles.id_article=lien.id_article AND lien.id_auteur=\"$connect_id_auteur\" AND articles.statut='publie' ORDER BY articles.date DESC", true);
-
-//
-//  Vos articles refuses
-//
-
-echo "<p>";
-afficher_articles(_T('info_refuses'),
-	", spip_auteurs_articles AS lien ".
-	"WHERE articles.id_article=lien.id_article AND lien.id_auteur=\"$connect_id_auteur\" AND articles.statut='refuse' ORDER BY articles.date DESC");
-
-
-fin_page();
-
-
+if (function_exists($var_nom))
+  $var_nom($connect_id_auteur);
+elseif (function_exists($var_f = $var_nom . "_dist"))
+  $var_f($connect_id_auteur);
+else
+   spip_log("fonction $var_nom indisponible");
 ?>
---
+++ ecrire/inc_articles_page.php
@@ -0,0 +1,86 @@
+<?php
+
+/***************************************************************************\
+ *  SPIP, Systeme de publication pour l'internet                           *
+ *                                                                         *
+ *  Copyright (c) 2001-2005                                                *
+ *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
+ *                                                                         *
+ *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
+ *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
+\***************************************************************************/
+
+
+function articles_page_dist($id_auteur)
+{
+	global $connect_statut;
+
+	debut_page(_T('titre_page_articles_page'), "documents", "articles");
+
+	debut_gauche();
+
+//
+// Afficher le bouton de creation d'article
+//
+
+	$result = spip_query("SELECT id_rubrique FROM spip_rubriques LIMIT 1");
+
+	if (spip_num_rows($result) > 0) {
+		debut_raccourcis();
+		icone_horizontale (_T('icone_ecrire_article'), "articles_edit.php3?new=oui", "article-24.gif", "creer.gif");
+		fin_raccourcis();
+	} else {
+		if ($connect_statut == '0minirezo') {
+		echo _T('texte_creer_rubrique');
+		icone_horizontale (_T('icone_creer_rubrique'), "rubriques_edit.php3?new=oui&retour=nav", "rubrique-24.gif", "creer.gif");
+	}
+}
+
+debut_droite();
+
+//
+// Vos articles en cours de redaction
+//
+
+echo "<P align=left>";
+afficher_articles(_T('info_en_cours_validation'),
+	", spip_auteurs_articles AS lien ".
+	"WHERE articles.id_article=lien.id_article AND lien.id_auteur=$id_auteur AND articles.statut='prepa' ORDER BY articles.date DESC");
+echo "</p>";
+
+
+
+//
+// Vos articles soumis au vote
+//
+
+echo "<p>";
+afficher_articles(_T('info_attente_validation'),
+	", spip_auteurs_articles AS lien ".
+	"WHERE articles.id_article=lien.id_article AND lien.id_auteur=$id_auteur AND articles.statut='prop' ORDER BY articles.date");
+echo "</p>";
+
+//
+// Vos articles publies
+//
+
+echo "<p>";
+afficher_articles(_T('info_publies'),
+	", spip_auteurs_articles AS lien ".
+	"WHERE articles.id_article=lien.id_article AND lien.id_auteur=\"$id_auteur\" AND articles.statut='publie' ORDER BY articles.date DESC", true);
+echo "</p>";
+
+//
+//  Vos articles refuses
+//
+
+echo "<p>";
+afficher_articles(_T('info_refuses'),
+	", spip_auteurs_articles AS lien ".
+	"WHERE articles.id_article=lien.id_article AND lien.id_auteur=\"$id_auteur\" AND articles.statut='refuse' ORDER BY articles.date DESC");
+echo "</p>";
+
+fin_page();
+}
+
+?>

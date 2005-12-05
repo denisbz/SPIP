36c8cb3282d2487299bb329697ce248bae0e996b
fil <fil@rezo.net>
2005-12-05 23:40:42 +0000

    un bug vexant : quand un article etait trop long, les magic-quotes n'etaient plus effaces des parties "tableau" des donnees. Le bug provenait du fait que le nettoyage de magic_quotes s'effectuait uniquement sur les donnees "texte" des $_POST etc. Mais le correctif est complique car ces tableaux sont *recursifs*, et php ne propose pas de mecanisme pour detecter la recursion...
    git-svn-id: svn://trac.rezo.net/spip/spip@5094 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc_magicquotes.php
+++ ecrire/inc_magicquotes.php
@@ -14,25 +14,27 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 // Magic quotes : on n'en veut pas sur la base
 // et on nettoie les GET/POST/COOKIE le cas echeant
-function magic_unquote($_table) {
-	// Certains hebergeurs n'activent pas $GLOBALS['GLOBALS']
-	if ($_table == 'GLOBALS'
-	AND !isset($GLOBALS['GLOBALS']))
-		$GLOBALS['GLOBALS'] = &$GLOBALS;
-
-	if (is_array($GLOBALS[$_table])) {
-		foreach ($GLOBALS[$_table] as $key => $val) {
-			if (is_string($val))
-				$GLOBALS[$_table][$key] = stripslashes($val);
+function magic_unquote(&$t) {
+	if (is_array($t)) {
+		foreach ($t as $key => $val) {
+			if (!is_array($val)
+			OR !($t['spip_recursions']++)) # interdire les recursions
+				magic_unquote($t[$key], $key);
 		}
-	}
+	} else
+		$t = stripslashes($t);
 }
 
-magic_unquote('_GET');
-magic_unquote('_POST');
-magic_unquote('_COOKIE');
+magic_unquote($_GET);
+magic_unquote($_POST);
+magic_unquote($_COOKIE);
 #	if (@ini_get('register_globals')) // pas fiable
-	magic_unquote('GLOBALS');
+	magic_unquote($GLOBALS);
 
+# et a la fin supprimer la variable anti-recursion devenue inutile (et meme nuisible, notamment si on teste $_POST)
+unset($_GET['spip_recursions']);
+unset($_POST['spip_recursions']);
+unset($_COOKIE['spip_recursions']);
+unset($_GLOBALS['spip_recursions']);
 
 ?>

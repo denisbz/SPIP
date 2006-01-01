dabe3ee9a2c637e3e2ccd85eef3de4bd0971471a
fil <fil@rezo.net>
2006-01-01 21:36:35 +0000

    il  faut vivre avec son temps : (c) 2001-2006
    git-svn-id: svn://trac.rezo.net/spip/spip@5272 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc_message_edit.php
+++ ecrire/inc_message_edit.php
@@ -3,7 +3,7 @@
 /***************************************************************************\
  *  SPIP, Systeme de publication pour l'internet                           *
  *                                                                         *
- *  Copyright (c) 2001-2005                                                *
+ *  Copyright (c) 2001-2006                                                *
  *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
  *                                                                         *
  *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
--- inc-urls-propres2.php3
+++ inc-urls-propres2.php3
@@ -4,7 +4,7 @@
 /***************************************************************************\
  *  SPIP, Systeme de publication pour l'internet                           *
  *                                                                         *
- *  Copyright (c) 2001-2005                                                *
+ *  Copyright (c) 2001-2006                                                *
  *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
  *                                                                         *
  *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *

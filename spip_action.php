9b6a78705e421d96659e8b1ebb46c57136c09971
fil <fil@rezo.net>
2006-01-23 08:59:01 +0000

    probleme de redirection sur "vider le cache"
    git-svn-id: svn://trac.rezo.net/spip/spip@5466 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- spip_action.php
+++ spip_action.php
@@ -15,9 +15,8 @@ include ("ecrire/inc_version.php3");
 if (isset($hash)) {
 	include_ecrire("inc_session");
 	if (!verifier_action_auteur("$action $arg", $hash, $id_auteur)) {
-		$texte = _T('info_acces_interdit');
 		include_ecrire('inc_minipres');
-		minipres($texte);
+		minipres(_T('info_acces_interdit'));
 		exit;
 	}
  }
@@ -25,7 +24,10 @@ if (isset($hash)) {
 $var_f = include_fonction('spip_action_' . $action);
 $var_f();
 
-#if ($redirect) redirige_par_entete($redirect);
-if ($redirect) redirige_par_entete(_DIR_RESTREINT.$redirect);
+## le lien $redirect peut etre relatif a ecrire/ ou absolu : a nettoyer !!
+if ($redirect) {
+	include_ecrire('inc_filtres');
+	redirige_par_entete(suivre_lien(url_de_base()._DIR_RESTREINT,$redirect));
+}
 
 ?>

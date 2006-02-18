04a59b086422838c6e1684a29fd048a1139dc6c7
fil <fil@rezo.net>
2006-02-18 21:49:20 +0000

    lien du mail "article proposé" contenait un &amp; (N-K)
    git-svn-id: svn://trac.rezo.net/spip/spip@5689 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc_mail.php
+++ ecrire/inc_mail.php
@@ -249,7 +249,7 @@ function envoyer_mail_proposition($id_article) {
 				     ."\n" 
 				     . _T('info_propose_5')
 				     ."\n" 
-				     . generer_url_ecrire("articles", "id_article=$id_article")
+				     . generer_url_ecrire("articles", "id_article=$id_article", true)
 				     . "\n\n\n" 
 				     . extrait_article($row));
 			changer_langue($lang_utilisateur);

bbfce78e0501f7286a3cf86e9118e5a96e1791a3
fil <fil@rezo.net>
2006-02-20 09:51:31 +0000

    et un fichier qui se laisse appeler...
    git-svn-id: svn://trac.rezo.net/spip/spip@5717 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

---
+++ ecrire/page.php
@@ -0,0 +1,30 @@
+<?php
+
+// Appel spip
+if (!function_exists('find_in_path')) {
+	if (@file_exists('ecrire/inc_version.php')) {
+		include 'ecrire/inc_version.php';
+	} else exit;
+}
+
+// Reglage du $fond
+if (isset($contexte_inclus['fond']))
+	$fond = $contexte_inclus['fond'];
+else if (isset($_GET['page']))
+	$fond = $_GET['page'];
+else
+	$fond = 'sommaire';
+
+// Securite 
+if (strstr($fond, '/'))
+	die (_L("Faut pas se gener"));
+
+if (!find_in_path($fond.'.html')) {
+	spip_log("page: find_in_path ne trouve pas le squelette $fond");
+	echo _T('info_erreur_squelette2',
+		array('fichier' => htmlspecialchars($fond)));
+	$fond = '404';
+}
+include (_DIR_INCLUDE . 'public.php');
+
+?>

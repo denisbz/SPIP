69e88ef7d137f7eadecf145ee67ae5bfe40749db
esj <esj@rezo.net>
2006-03-14 16:44:40 +0000

    Amélioration de l'API pour les surcharges (les constantes en dur dans le code, c'est pas top)
    git-svn-id: svn://trac.rezo.net/spip/spip@5966 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/exec/articles_forum.php
+++ ecrire/exec/articles_forum.php
@@ -16,107 +16,31 @@ include_spip('inc/forum'); // pour boutons_controle_forum
 
 function exec_articles_forum_dist()
 {
-  global $connect_statut, $debut, $id_article;
+  global $connect_statut, $debut, $id_article, $pack, $enplus;
 
-  $id_article = intval($id_article);
-  $debut = intval($debut);
-  
-$query = "SELECT titre, id_rubrique FROM spip_articles WHERE id_article=$id_article";
-$result = spip_query($query);
+	$id_article = intval($id_article);
+	$debut = intval($debut);
+	$pack = intval($pack);
+	$enplus = intval($enplus);
 
-if ($row = spip_fetch_array($result)) {
-	$titre = $row["titre"];
-	$id_rubrique = $row["id_rubrique"];
-}
+	if (!$pack) $pack = 5; // nb de forums affiches par page
+	if (!$enplus) $enplus = 200;	// intervalle affiche autour du debut
 
+	$result = spip_query("SELECT titre, id_rubrique FROM spip_articles WHERE id_article=$id_article");
 
-debut_page($titre, "documents", "articles");
-
-
-
-debut_grand_cadre();
-
-afficher_hierarchie($id_rubrique);
-
-fin_grand_cadre();
-
-
-
-debut_gauche();
-
-
-debut_boite_info();
-
-echo "<FONT FACE='Verdana,Arial,Sans,sans-serif' SIZE=2>";
-echo "<P align=left>"._T('info_gauche_suivi_forum');
-
-echo aide ("suiviforum");
-echo "</FONT>";
-
-fin_boite_info();
-
-
-debut_droite();
-
-
-echo "\n<table cellpadding=0 cellspacing=0 border=0 width='100%'>";
-echo "<tr width='100%'>";
-echo "<td>";
-	icone(_T('icone_retour'), generer_url_ecrire("articles","id_article=$id_article&id_rubrique=$id_rubrique"), "article-24.gif", "rien.gif");
-
-echo "</td>";
-echo "<td>" . http_img_pack('rien.gif', " ", "width='10'") ."</td>\n";
-echo "<td width='100%'>";
-echo _T('texte_messages_publics');
-gros_titre($titre);
-echo "</td></tr></table>";
-echo "<p>";
-
-// Ne pas donner les cles du forum a des non-admins
-if (! ($connect_statut=='0minirezo' AND acces_rubrique($id_rubrique)))
-	return;
-
-// reglages
-if (!$debut) $debut = 0;
-$pack = 5;		// nb de forums affiches par page
-$enplus = 200;	// intervalle affiche autour du debut
-$limitdeb = ($debut > $enplus) ? $debut-$enplus : 0;
-$limitnb = $debut + $enplus - $limitdeb;
-
-$result_forum = spip_query("SELECT id_forum FROM spip_forum WHERE id_article='$id_article' AND id_parent=0 AND statut IN ('publie', 'off', 'prop')" .
-#	" LIMIT  $limitnb OFFSET $limitdeb" # PG
-	" LIMIT $limitdeb, $limitnb"
-			   ); 
-
-$i = $limitdeb;
-
-echo "<div class='serif2'>";
-
-if ($i>0)
-	echo "<A href='" . generer_url_ecrire("articles_forum","id_article=$id_article") . "'>0</A> ... | ";
-while ($row = spip_fetch_array($result_forum)) {
-
-	// barre de navigation
-	if ($i == $pack*floor($i/$pack)) {
-		if ($i == $debut)
-			echo "<FONT SIZE=3><B>$i</B></FONT>";
-		else
-			echo "<A href='" . generer_url_ecrire("articles_forum","id_article=$id_article&debut=$i") . "'>$i</A>";
-		echo " | ";
+	if ($row = spip_fetch_array($result)) {
+		$titre = $row["titre"];
+		$id_rubrique = $row["id_rubrique"];
 	}
 
-	// elements a controler
+	debut_page($titre, "documents", "articles");
 
-	$i ++;
-}
+	articles_forum_cadres($id_article, $id_rubrique, $titre);
 
- $forum_retour = generer_url_ecrire("articles_forum","id_article=$id_article&debut=$i");
+	if (! ($connect_statut=='0minirezo' AND acces_rubrique($id_rubrique)))
+		return;
+	articles_forum_liens($id_article, $debut, $pack, $enplus);
 
-echo "<A href='$forum_retour'>...</A>";
-
-echo "</div>";
-
-if ($connect_statut == "0minirezo") {
 	$result_forum = spip_query("SELECT pied.*, max(thread.date_heure) AS date
 		FROM spip_forum AS pied, spip_forum AS thread
 		WHERE pied.id_article='$id_article'
@@ -127,11 +51,83 @@ if ($connect_statut == "0minirezo") {
 		ORDER BY date DESC LIMIT $debut, $pack");
 
 	afficher_forum($result_forum,"", $id_article);
+	
+	fin_page();
 }
 
-echo "</FONT>";
+function articles_forum_cadres($id_article, $id_rubrique, $titre)
+{
+	debut_grand_cadre();
 
-fin_page();
+	afficher_hierarchie($id_rubrique);
+
+	fin_grand_cadre();
+
+	debut_gauche();
+
+	debut_boite_info();
+
+	echo "<p align=left>",
+	  "<font FACE='Verdana,Arial,Sans,sans-serif' SIZE='2'>",
+	  _T('info_gauche_suivi_forum'),
+	  aide ("suiviforum"),
+	  "</font></p>";
+
+	fin_boite_info();
+
+	debut_droite();
+
+	echo "\n<table cellpadding=0 cellspacing=0 border=0 width='100%'>";
+	echo "<tr width='100%'>";
+	echo "<td>";
+	icone(_T('icone_retour'),
+		generer_url_ecrire("articles","id_article=$id_article&id_rubrique=$id_rubrique"),
+		"article-24.gif", "rien.gif");
+	echo "</td>";
+	echo "<td>" . http_img_pack('rien.gif', " ", "width='10'") ."</td>\n";
+	echo "<td width='100%'>";
+	echo _T('texte_messages_publics');
+	gros_titre($titre);
+	echo "</td></tr></table>";
+	echo "<p>";
+}
+
+function articles_forum_liens($id_article, $debut, $pack, $enplus)
+{
+	$limitdeb = ($debut > $enplus) ? $debut-$enplus : 0;
+	$limitnb = $debut + $enplus - $limitdeb;
+
+	$result_forum = spip_query("SELECT id_forum FROM spip_forum WHERE id_article='$id_article' AND id_parent=0 AND statut IN ('publie', 'off', 'prop')" .
+#	" LIMIT  $limitnb OFFSET $limitdeb" # PG
+	" LIMIT $limitdeb, $limitnb"
+			   ); 
+
+	$i = $limitdeb;
+
+	echo "<div class='serif2'>";
+
+	if ($i>0)
+		echo "<A href='" . generer_url_ecrire("articles_forum","id_article=$id_article") . "'>0</A> ... | ";
+	while ($row = spip_fetch_array($result_forum)) {
+
+	// barre de navigation
+		if ($i == $pack*floor($i/$pack)) {
+			if ($i == $debut)
+				echo "<FONT SIZE=3><B>$i</B></FONT>";
+			else
+				echo "<A href='" . generer_url_ecrire("articles_forum","id_article=$id_article&debut=$i") . "'>$i</A>";
+			echo " | ";
+		}
+
+		$i ++;
+	}
+
+	echo "<A href='",
+		generer_url_ecrire("articles_forum","id_article=$id_article&debut=$i"),
+		"'>...</A>",
+		"</div>";
+
+	echo "</FONT>";
 }
 
 ?>

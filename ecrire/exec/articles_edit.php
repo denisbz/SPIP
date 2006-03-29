10e4ff3a81edd727de2008378d89b7224c986c4e
esj <esj@rezo.net>
2006-03-29 17:37:48 +0000

    séparation pour permettre de greffer d'autres modes d'édition. Gros ménage en passant,  il y avait beaucoup de code mort anterieure à la 1.7.2, en particulier un changer_lang qui mériterait une étude d'intention.
    git-svn-id: svn://trac.rezo.net/spip/spip@6088 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/exec/articles_edit.php
+++ ecrire/exec/articles_edit.php
@@ -13,6 +13,7 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 include_spip('inc/presentation');
+include_spip('inc/article_update');
 include_spip('inc/rubriques');
 include_spip('inc/documents');
 include_spip('inc/barre');
@@ -96,7 +97,7 @@ function chapo_articles_edit($chapo, $articles_chapeau)
 	}
 }
 //// a TESTER
-function formulaire_articles_edit($id_article, $id_rubrique, $titre, $soustitre, $surtitre, $descriptif, $url, $chapo, $texte, $ps, $new, $nom_site, $url_site, $extra, $id_secteur, $date, $onfocus, $lier_trad, $champs_article)
+function formulaire_articles_edit($id_article, $id_rubrique, $titre, $soustitre, $surtitre, $descriptif, $chapo, $texte, $ps, $new, $nom_site, $url_site, $extra, $id_secteur, $date, $onfocus, $lier_trad, $champs_article)
 {
  global   $champs_extra, $spip_lang, $options , $spip_ecran;
 
@@ -106,7 +107,6 @@ function formulaire_articles_edit($id_article, $id_rubrique, $titre, $soustitre,
  $articles_urlref = $champs_article['articles_urlref'];
  $articles_chapeau = $champs_article['articles_chapeau'];
  $articles_ps = $champs_article['articles_ps'];
- $articles_redac = $champs_article['articles_redac'];
  $articles_mots = $champs_article['articles_mots'];
  $articles_modif = $champs_article['articles_modif'];
 
@@ -129,7 +129,6 @@ echo "<P><HR><P>";
 	$titre = entites_html($titre);
 	$soustitre = entites_html($soustitre);
 	$surtitre = entites_html($surtitre);
-
 	$descriptif = entites_html($descriptif);
 	$nom_site = entites_html($nom_site);
 	$url_site = entites_html($url_site);
@@ -272,176 +271,63 @@ echo "<P><HR><P>";
 }
 
 
-function exec_affiche_articles_edit_dist($flag_editable, $id_article, $id_rubrique, $titre, $soustitre, $surtitre, $descriptif, $url, $chapo, $texte, $ps, $new, $nom_site, $url_site, $extra, $id_secteur, $date, $onfocus, $lier_trad, $champs_article)
-{
-  global $champs_extra;
-debut_page(_T('titre_page_articles_edit', array('titre' => $titre)), "documents", "articles", "hauteurTextarea();", "", $id_rubrique);
+function exec_affiche_articles_edit_dist($row, $lier_trad, $new, $champs_article) {
+	global $champs_extra;
+	$id_article = $row['id_article'];
+	$id_rubrique = $row['id_rubrique'];
+	$titre = $row['titre'];
+	$soustitre = $row['soustitre'];
+	$surtitre = $row['surtitre'];
+	$descriptif = $row['descriptif'];
+	$chapo = $row['chapo'];
+	$texte = $row['texte'];
+	$ps = $row['ps'];
+	$nom_site = $row['nom_site'];
+	$url_site = $row['url_site'];
+	$extra = $row['extra'];
+	$id_secteur = $row['id_secteur'];
+	$date = $row['date'];
+	$onfocus = $row['onfocus'];
 
-debut_grand_cadre();
+	debut_page(_T('titre_page_articles_edit', array('titre' => $titre)), "documents", "articles", "hauteurTextarea();", "", $id_rubrique);
 
-afficher_hierarchie($id_rubrique);
+	debut_grand_cadre();
 
-fin_grand_cadre();
+	afficher_hierarchie($id_rubrique);
 
-debut_gauche();
+	fin_grand_cadre();
+
+	debut_gauche();
 
 //
 // Pave "documents associes a l'article"
 //
 
- if ($new != 'oui'){
+	if ($new != 'oui'){
 	# modifs de la description d'un des docs joints
-	if ($flag_editable) maj_documents($id_article, 'article');
+		maj_documents($id_article, 'article');
 
 	# affichage
-	afficher_documents_colonne($id_article, 'article', $flag_editable);
+		afficher_documents_colonne($id_article, 'article', true);
+	}
+	$GLOBALS['id_article_bloque'] = $id_article;	// globale dans debut_droite
+	debut_droite();
+	debut_cadre_formulaire();
+
+	formulaire_articles_edit($id_article, $id_rubrique, $titre, $soustitre, $surtitre, $descriptif, $chapo, $texte, $ps, $new, $nom_site, $url_site, $extra, $id_secteur, $date, $onfocus, $lier_trad, $champs_article);
+	fin_cadre_formulaire();
+
+	fin_page();
 }
- $GLOBALS['id_article_bloque'] = $id_article;	// globale dans debut_droite
- debut_droite();
- debut_cadre_formulaire();
 
- formulaire_articles_edit($id_article, $id_rubrique, $titre, $soustitre, $surtitre, $descriptif, $url, $chapo, $texte, $ps, $new, $nom_site, $url_site, $extra, $id_secteur, $date, $onfocus, $lier_trad, 
-$champs_article);
-fin_cadre_formulaire();
-
-fin_page();
-}
-
-
-//
-// Creation de l'objet article
-//
 
 function exec_articles_edit_dist()
 {
-  global $connect_id_auteur, $spip_lang, $id_article, $id_rubrique, $lier_trad, $new;
-  $id_article = intval($id_article);
-  $id_rubrique =  intval($id_rubrique);
-  $lier_trad =  intval($lier_trad);
+	$row = article_update(_request('id_article'), _request('id_rubrique'), _request('lier_trad'), _request('new'));
 
-// ESSAI pour "Joindre un document" depuis l'espace prive (UPLOAD_DIRECT)
-/*if ($GLOBALS['action'] AND $GLOBALS['doc']) {
-	global $action, $doc;
-	if ($var_nom = include_fonction($action, 'action'))
-		$var_nom($doc);
-	else
-		spip_log("fonction $var_nom indisponible");
-#	return;
-}*/
+	if (!$row) die ("<h3>"._T('info_acces_interdit')."</h3>");
 
-
-  if ($id_article) {
-	// Recuperer les donnees de l'article
-	$query = "SELECT * FROM spip_articles WHERE id_article=$id_article";
-	$result = spip_query($query);
-
-	if ($row = spip_fetch_array($result)) {
-		$id_article = $row["id_article"];
-		$surtitre = $row["surtitre"];
-		$titre = $row["titre"];
-		$soustitre = $row["soustitre"];
-		$id_rubrique = $row["id_rubrique"];
-		$id_secteur = $row['id_secteur'];
-		$descriptif = $row["descriptif"];
-		$nom_site = $row["nom_site"];
-		$url_site = $row["url_site"];
-		$chapo = $row["chapo"];
-		$texte = $row["texte"];
-		$ps = $row["ps"];
-		$date = $row["date"];
-		$statut = $row['statut'];
-		$date_redac = $row['date_redac'];
-	    	if (ereg("([0-9]{4})-([0-9]{2})-([0-9]{2})",$date_redac,$regs)){
-		        $mois_redac = $regs[2];
-		        $jour_redac = $regs[3];
-		        $annee_redac = $regs[1];
-		        if ($annee_redac > 4000) $annee_redac -= 9000;
-		}
-		$extra=$row["extra"];
-
-		$query = "SELECT * FROM spip_auteurs_articles WHERE id_article=$id_article AND id_auteur=$connect_id_auteur";
-		$result_auteur = spip_query($query);
-		$flag_auteur = (spip_num_rows($result_auteur) > 0);
-
-		$flag_editable = (acces_rubrique($id_rubrique) OR ($flag_auteur > 0 AND ($statut == 'prepa' OR $statut == 'prop' OR $new == 'oui')));
-	}
-}
-else if ($new=='oui') {
-	if ($lier_trad) {
-		// Pas de langue choisie par defaut
-		$changer_lang = '';
-
-		// Recuperer les donnees de la traduction
-		$query = "SELECT * FROM spip_articles WHERE id_article=$lier_trad";
-		$result = spip_query($query);
-	
-		if ($row = spip_fetch_array($result)) {
-			$surtitre = $row["surtitre"];
-			$titre = filtrer_entites(_T('info_nouvelle_traduction')).' '.$row["titre"];
-			$soustitre = $row["soustitre"];
-			$id_rubrique_trad = $row["id_rubrique"];
-			$descriptif = $row["descriptif"];
-			$nom_site = $row["nom_site"];
-			$url_site = $row["url_site"];
-			$chapo = $row["chapo"];
-			$texte = $row["texte"];
-			$ps = $row["ps"];
-			$date = $row["date"];
-			$date_redac = $row['date_redac'];
-			if (ereg("([0-9]{4})-([0-9]{2})-([0-9]{2})",$date_redac,$regs)) {
-				$mois_redac = $regs[2];
-				$jour_redac = $regs[3];
-				$annee_redac = $regs[1];
-				if ($annee_redac > 4000) $annee_redac -= 9000;
-			}
-			$extra = $row["extra"];
-		}
-		$langues_autorisees = $GLOBALS['meta']['langues_multilingue'];
-		
-		// Regler la langue, si possible
-		if (ereg(",$spip_lang,", ",$langues_autorisees,")) {
-			if ($GLOBALS['meta']['multi_articles'] == 'oui') {
-				// Si le menu de langues est autorise sur les articles,
-				// on peut changer la langue quelle que soit la rubrique
-				$changer_lang = $spip_lang;
-			}
-			else if ($GLOBALS['meta']['multi_rubriques'] == 'oui') {
-				// Chercher la rubrique la plus adaptee pour accueillir l'article
-				if ($GLOBALS['meta']['multi_secteurs'] == 'oui') 
-					$id_parent = 0;
-				else {
-					$query = "SELECT id_parent FROM spip_rubriques WHERE id_rubrique=$id_rubrique";
-					$row_rub = spip_fetch_array(spip_query($query));
-					$id_parent = $row_rub['id_parent'];
-				}
-				$query = "SELECT id_rubrique FROM spip_rubriques WHERE lang='$spip_lang' AND id_parent=$id_parent";
-				if ($row_rub = spip_fetch_array(spip_query($query))) {
-					$id_rubrique = $id_secteur = $row_rub['id_rubrique'];
-					$changer_lang = 'herit';
-				}
-			}
-		}
-	}
-	else {
-		// Nouvel article : titre par defaut
-		$titre = filtrer_entites(_T('info_nouvel_article'));
-		$onfocus = " onfocus=\"if(!antifocus){this.value='';antifocus=true;}\"";
-	}
-	if (!$id_secteur) {
-		$row_rub = spip_fetch_array(spip_query("SELECT id_secteur FROM spip_rubriques WHERE id_rubrique=$id_rubrique"));
-		$id_secteur = $row_rub['id_secteur'];
-	}
-	$flag_editable = true;
-}
-
-if (!$flag_editable) {
-	die ("<H3>"._T('info_acces_interdit')."</H3>");
-}
-
-
-spip_query("UPDATE spip_articles SET date_modif=NOW(), auteur_modif=$connect_id_auteur WHERE id_article=$id_article");
-
- exec_affiche_articles_edit_dist($flag_editable, $id_article, $id_rubrique, $titre, $soustitre, $surtitre, $descriptif, $url, $chapo, $texte, $ps, $new, $nom_site, $url_site, $extra, $id_secteur, $date, $onfocus, $lier_trad, $GLOBALS['meta']);
+	exec_affiche_articles_edit_dist($row, $lier_trad, $new,$GLOBALS['meta']);
 }
 
 ?>
---
+++ ecrire/inc/article_update.php
@@ -0,0 +1,111 @@
+<?php
+
+/***************************************************************************\
+ *  SPIP, Systeme de publication pour l'internet                           *
+ *                                                                         *
+ *  Copyright (c) 2001-2006                                                *
+ *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
+ *                                                                         *
+ *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
+ *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
+\***************************************************************************/
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function article_update($id_article, $id_rubrique, $lier_trad, $new)
+{
+  global $connect_id_auteur, $spip_lang; 
+  $id_article = intval($id_article);
+  $id_rubrique =  intval($id_rubrique);
+  $lier_trad =  intval($lier_trad);
+
+// ESSAI pour "Joindre un document" depuis l'espace prive (UPLOAD_DIRECT)
+/*if ($GLOBALS['action'] AND $GLOBALS['doc']) {
+	global $action, $doc;
+	if ($var_nom = include_fonction($action, 'action'))
+		$var_nom($doc);
+	else
+		spip_log("fonction $var_nom indisponible");
+#	return;
+}*/
+
+  if ($id_article) {
+	$result = spip_query("SELECT * FROM spip_articles WHERE id_article=$id_article");
+
+	if ($row = spip_fetch_array($result)) {
+		$titre = $row["titre"];
+		$id_rubrique = $row["id_rubrique"];
+		$id_secteur = $row['id_secteur'];
+		$statut = $row['statut'];
+
+		$result_auteur = spip_query("SELECT * FROM spip_auteurs_articles WHERE id_article=$id_article AND id_auteur=$connect_id_auteur");
+
+		$flag_auteur = (spip_num_rows($result_auteur) > 0);
+
+		$flag_editable = (acces_rubrique($id_rubrique) OR ($flag_auteur > 0 AND ($statut == 'prepa' OR $statut == 'prop' OR $new == 'oui')));
+ 	}
+}
+else if ($new=='oui') {
+	if ($lier_trad) {
+		// Pas de langue choisie par defaut
+		$changer_lang = '';
+
+		// Recuperer les donnees de la traduction
+		$query = "SELECT * FROM spip_articles WHERE id_article=$lier_trad";
+		$result = spip_query($query);
+	
+		if ($row = spip_fetch_array($result)) {
+			$titre = filtrer_entites(_T('info_nouvelle_traduction')).' '.$row["titre"];
+
+			$id_rubrique_trad = $row["id_rubrique"];
+
+
+
+
+		}
+		$langues_autorisees = $GLOBALS['meta']['langues_multilingue'];
+		
+		// Regler la langue, si possible
+		if (ereg(",$spip_lang,", ",$langues_autorisees,")) {
+			if ($GLOBALS['meta']['multi_articles'] == 'oui') {
+				// Si le menu de langues est autorise sur les articles,
+				// on peut changer la langue quelle que soit la rubrique
+				$changer_lang = $spip_lang;
+			}
+			else if ($GLOBALS['meta']['multi_rubriques'] == 'oui') {
+				// Chercher la rubrique la plus adaptee pour accueillir l'article
+				if ($GLOBALS['meta']['multi_secteurs'] == 'oui') 
+					$id_parent = 0;
+				else {
+					$query = "SELECT id_parent FROM spip_rubriques WHERE id_rubrique=$id_rubrique";
+					$row_rub = spip_fetch_array(spip_query($query));
+					$id_parent = $row_rub['id_parent'];
+				}
+				$query = "SELECT id_rubrique FROM spip_rubriques WHERE lang='$spip_lang' AND id_parent=$id_parent";
+				if ($row_rub = spip_fetch_array(spip_query($query))) {
+					$id_rubrique = $row['id_secteur'] = $row['id_rubrique'] = $row_rub['id_rubrique'];
+					$changer_lang = 'herit';
+				}
+			}
+		}
+	}
+	else {
+		// Nouvel article : titre par defaut
+		$row['titre'] = filtrer_entites(_T('info_nouvel_article'));
+		$row['onfocus'] = " onfocus=\"if(!antifocus){this.value='';antifocus=true;}\"";
+	}
+	if (!$row['id_secteur']) {
+		$row_rub = spip_fetch_array(spip_query("SELECT id_secteur FROM spip_rubriques WHERE id_rubrique=$id_rubrique"));
+		$row['id_secteur'] = $row_rub['id_secteur'];
+	}
+	$flag_editable = true;
+}
+
+	if (!$flag_editable) return false;
+
+	spip_query("UPDATE spip_articles SET date_modif=NOW(), auteur_modif=$connect_id_auteur WHERE id_article=$id_article");
+
+	return $row;
+}
+
+?>

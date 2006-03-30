a958a80c4d5de89d74344e5a95f11ad2258db50e
fil <fil@rezo.net>
2006-03-30 16:14:42 +0000

    bug sur articles_edit: mauvais include
    git-svn-id: svn://trac.rezo.net/spip/spip@6094 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/exec/articles_edit.php
+++ ecrire/exec/articles_edit.php
@@ -13,8 +13,7 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 include_spip('inc/presentation');
-include_spip('inc/article_update');
-#include_spip('inc/article_select');
+include_spip('inc/article_select');
 include_spip('inc/rubriques');
 include_spip('inc/documents');
 include_spip('inc/barre');

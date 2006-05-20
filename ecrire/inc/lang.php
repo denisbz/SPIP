2caa89838ed669908de169637b7e19e2a7fad5e3
fil <fil@rezo.net>
2006-05-20 21:49:42 +0000

    plus de langues RTL (N-K)
    git-svn-id: svn://trac.rezo.net/spip/spip@6406 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/lang.php
+++ ecrire/inc/lang.php
@@ -188,10 +188,14 @@ function traduire_nom_langue($lang) {
 // Filtres de langue
 //
 
-// afficher 'gaucher' si la langue est arabe, hebreu, kurde, persan,
-// 'droitier' sinon ; utilise par #LANG_DIR, #LANG_LEFT, #LANG_RIGHT
+// Donne la direction d'ecriture a partir de la langue. Retourne 'gaucher' si
+// la langue est arabe, persan, kurde, pachto, ourdou (langues ecrites en
+// alphabet arabe a priori), hebreu, yiddish (langues ecrites en alphabet
+// hebreu a priori), 'droitier' sinon.
+// C'est utilise par #LANG_DIR, #LANG_LEFT, #LANG_RIGHT.
 function lang_dir($lang, $droitier='ltr', $gaucher='rtl') {
-	if ($lang=='fa' OR $lang=='ar' OR $lang == 'he' OR $lang == 'ku')
+	if ($lang=='ar' OR $lang=='fa' OR $lang == 'ku' OR $lang == 'ps'
+	OR $lang == 'ur' OR $lang == 'he' OR $lang == 'yi')
 		return $gaucher;
 	else
 		return $droitier;

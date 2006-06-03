c88fadc3e24673e54b58702c737ca95c042060b2
fil <fil@rezo.net>
2006-06-03 10:07:30 +0000

    meilleure detection d'erreur dans #PAGINATION, et au passage on jette l'interface de départ, qui permettait #PAGINATION{nom_boucle} ... l'argument servira peut-être à autre chose plus tard (comme de la personnalisation du rendu)
    git-svn-id: svn://trac.rezo.net/spip/spip@6512 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/public/balises.php
+++ ecrire/public/balises.php
@@ -561,33 +561,34 @@ function balise_POPULARITE_dist ($p) {
 function balise_PAGINATION_dist($p, $liste='true') {
 	$b = $p->nom_boucle ? $p->nom_boucle : $p->descr['id_mere'];
 
+	// s'il n'y a pas de nom de boucle, on ne peut pas paginer
 	if ($b === '') {
 		erreur_squelette(
 			_T('zbug_champ_hors_boucle',
-				array('champ' => "#$b" . 'PAGINATION')
+				array('champ' => '#PAGINATION')
 			), $p->id_boucle);
 		$p->code = "''";
+		return $p;
 	}
 
-	if ($p->param) {
-		$option = calculer_liste($p->param[0][1],
-			$p->descr,
-			$p->boucles,
-			$p->id_boucle);
-		$option = str_replace("'", '', $option);
-	} else  $option = '';
+	// s'il n'y a pas de total_parties, c'est qu'on se trouve
+	// dans un boucle recurive ou qu'on a oublie le critere {pagination}
+	if (!$p->boucles[$b]->total_parties) {
+		erreur_squelette(
+			_L('zbug_xx: #PAGINATION sans critere {pagination}
+				ou employe dans une boucle recursive',
+				array('champ' => '#PAGINATION')
+			), $p->id_boucle);
+		$p->code = "''";
+		return $p;
+	}
 
 	$p->boucles[$b]->numrows = true;
-
-	if ($option)
-		$nom_boucle = $option;
-	else
-		$nom_boucle = $b;
 
 	$p->code = "calcul_pagination(
 	(isset(\$Numrows['$b']['grand_total']) ?
 		\$Numrows['$b']['grand_total'] : \$Numrows['$b']['total']
-	), '$nom_boucle', "
+	), '$b', "
 	. $p->boucles[$b]->total_parties
 	. ", $liste)";
 

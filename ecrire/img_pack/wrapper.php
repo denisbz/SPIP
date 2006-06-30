aef655bfa93499ec49ec4a767d45be97c37b4d4c
cedric <cedric@yterium.com>
2006-06-30 20:37:52 +0000

    renforcement securité du wrapper image
    git-svn-id: svn://trac.rezo.net/spip/spip@6789 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/img_pack/wrapper.php
+++ ecrire/img_pack/wrapper.php
@@ -2,22 +2,27 @@
 
 	// wrapper image pour scintillement MSIE, cf.
 	// http://www.ultra-fluide.com/ressources/css/css-hacks.htm#scintillement
-	if (!isset($_GET['file'])
-	OR !preg_match(',^[^/\0]+\.(gif|jpg|png)$,i', $_GET['file'], $r)
-	OR !@file_exists('./'.$_GET['file']))
-		die('404 not found');
-
-	$mime = array(
-		'jpg' => 'image/jpeg',
-		'gif' => 'image/gif',
-		'png' => 'image/png'
-	);
-
-	header('Content-Type: '.$mime[strtolower($r[1])]);
-	header('Content-Length: '.filesize('./'.$_GET['file']));
-	header('Cache-Control: max-age=36000');
-	header('Pragma: public');
-	readfile('./'.$_GET['file']);
-	exit;
+	if (isset($_GET['file'])){
+		$f = $_GET['file']; // pas de urldecode ici car on accepte de toute facon que les caracteres alphanumeriques
+		if (preg_match(',^[a-z_0-9\-]+\.(gif|jpg|png)$,i', $f, $r)
+				AND @file_exists('./'.$f)){
+	
+			$mime = array(
+				'jpg' => 'image/jpeg',
+				'gif' => 'image/gif',
+				'png' => 'image/png'
+			);
+		
+			header('Content-Type: '.$mime[strtolower($r[1])]);
+			header('Content-Length: '.filesize('./'.$f));
+			header('Cache-Control: max-age=36000');
+			header('Pragma: public');
+			readfile('./'.$f);
+			exit;
+		}
+	}
+	
+	header("Status : 404 Not Found"); 
+	die('404 not found');
 
 ?>

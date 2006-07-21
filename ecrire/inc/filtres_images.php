254991e94f9eb2598cbef243f280a5658ba0de5e
fil <fil@rezo.net>
2006-07-21 19:56:03 +0000

    tartine GPL
    git-svn-id: svn://trac.rezo.net/spip/spip@6899 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/filtres_images.php
+++ ecrire/inc/filtres_images.php
@@ -1,5 +1,18 @@
 <?php
 
+/***************************************************************************\
+ *  SPIP, Systeme de publication pour l'internet                           *
+ *                                                                         *
+ *  Copyright (c) 2001-2006                                                *
+ *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
+ *                                                                         *
+ *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
+ *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
+\***************************************************************************/
+
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
 // Fonctions de traitement d'image
 // uniquement pour GD2
 function image_valeurs_trans($img, $effet, $forcer_format = false) {

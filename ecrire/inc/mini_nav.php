7492512074a9e34af3ad04e0da7f1f41ea04de9a
esj <esj@rezo.net>
2006-08-29 08:04:54 +0000

    Encore un peu d'optimisation dans le mini-navigateur de rubrique
    git-svn-id: svn://trac.rezo.net/spip/spip@7254 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/mini_nav.php
+++ ecrire/inc/mini_nav.php
@@ -13,62 +13,47 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 // http://doc.spip.org/@mini_afficher_rubrique
-function mini_afficher_rubrique ($id_rubrique, $rac="", $list=array(), $col = 1, $rub_exclus=0) {
+function mini_afficher_rubrique ($id_rubrique, $rac="", $list=array(), $col = 1, $exclu=0) {
 	global  $spip_lang_left;
 	
 	if ($list) $id_rubrique = $list[$col-1];
 	
-	$nom_col = $rac . "_col_".($col+1);
-
-	$ret = "<div id='"
-	. $rac
-	. "_col_"
-	. $col
-	."' class='arial1'>" 
-	. http_img_pack("searching.gif", "*", "style='visibility: hidden; position: absolute; $spip_lang_left: "
-		.(($col*150)-30)
-		."px; top: 2px; z-index: 2;' id='img_$nom_col'")
-	. "<div style='width: 150px; height: 100%; overflow: auto; position: absolute; top: 0px; $spip_lang_left: "
-	.(($col-1)*150)
-	."px;'>";
+	$ret = '';
 
 	# recherche les filles et petites-filles de la rubrique donnee
 	$ordre = array();
 
-	$res = spip_query("SELECT rub1.id_rubrique, rub1.titre, rub1.id_parent, rub1.lang, rub1.langue_choisie FROM spip_rubriques AS rub1, spip_rubriques AS rub2 WHERE ((rub1.id_parent = $id_rubrique) OR (rub2.id_parent = $id_rubrique AND rub1.id_parent=rub2.id_rubrique)) AND rub1.id_rubrique!=$rub_exclus GROUP BY rub1.id_rubrique ORDER BY rub1.titre");
+	$res = spip_query("SELECT rub1.id_rubrique, rub1.titre, rub1.id_parent, rub1.lang, rub1.langue_choisie FROM spip_rubriques AS rub1, spip_rubriques AS rub2 WHERE ((rub1.id_parent = $id_rubrique) OR (rub2.id_parent = $id_rubrique AND rub1.id_parent=rub2.id_rubrique)) AND rub1.id_rubrique!=$exclu GROUP BY rub1.id_rubrique");
 
 	while ($row = spip_fetch_array($res)) {
-		$id = $row['id_rubrique'];
-		$row['enfants'] = $rub[$id]['enfants'];
-		$rub[$id] = $row;
 		$rub[$row['id_parent']]['enfants'] = true;
-
 		if ($row['id_parent'] == $id_rubrique)
-			$ordre[$id] = trim(typo($row['titre']))
-			. (($row['langue_choisie'] != 'oui') ? '' : (' [' . $row['lang'] . ']'));
-
+			$ordre[$row['id_rubrique']]= trim(typo($row['titre']))
+			. (($row['langue_choisie'] != 'oui')
+			   ? '' : (' [' . $row['lang'] . ']'));
 	}
 
+	$next = $list[$col];
 	if ($ordre) {
 		asort($ordre);
 
-		while (list($id, $titre) = each($ordre)) {
+		while (list($id, $titrebrut) = each($ordre)) {
 
 			$titre = "<div class='"
 			. ($id_rubrique ? 'petite-rubrique' : "petit-secteur")
 			. "'>"
-			. supprimer_numero($titre)
+			. supprimer_numero($titrebrut)
 			. "</div>";
 
 			if ($rub[$id]["enfants"]) {
 				$titre = "\n<div class='rub-ouverte'>$titre</div>";
 
 		# ensuite, ouverture ou fermeture du menu des sous-rubriques
-				$url = generer_url_ecrire('plonger',"&var_ajax=1&rac=$rac&exclus=$rub_exclus&id=$id&col=".($col+1), true);
+				$url = generer_url_ecrire('plonger',"&var_ajax=1&rac=$rac&exclus=$exclu&id=$id&col=".($col+1), true);
 
 			} else {  $url = ''; }
 
-			$class = ($id == $list[$col]) ? "highlight" : "pashighlight";
+			$class= ($id == $next) ? "highlight" : "pashighlight";
 
 			$onClick = "\naff_selection_provisoire($id,\n\t'$rac',\n\t'$url',\n\t$col,\n\t'$spip_lang_left');";
 				
@@ -77,22 +62,34 @@ function mini_afficher_rubrique ($id_rubrique, $rac="", $list=array(), $col = 1,
 			$ondbClick = "aff_selection_titre($id,'"
 			. strtr(str_replace("'", "&#8217;",
 						str_replace('"', "&#34;",
-							textebrut($titre))),
+							textebrut($titrebrut))),
 					"\n\r", "  ")
 			. "');";
 
 			$ret .= "\n<div class='$class'\nonClick=\"changerhighlight(this);$onClick\"\nondblclick=\"$ondbClick$onClick\">$titre\n</div>";
 		}
 	}
-	
-	$ret .= "\n</div>";
 
-	if ($list[$col]) $ret .= mini_afficher_rubrique ($id_rubrique, $rac, $list, $col+1, $rub_exclus);
-	else $ret .= "\n<div id='$nom_col'></div>";
-	
-	$ret .= "\n</div>";
+	$nom_col = $rac . "_col_".($col+1);
+	$left = ($col*150);
 
-	return $ret;
+	return "<div id='"
+	. $rac
+	. "_col_"
+	. $col
+	."' class='arial1'>" 
+	. http_img_pack("searching.gif", "*", "style='visibility: hidden; position: absolute; $spip_lang_left: "
+		.($left-30)
+		."px; top: 2px; z-index: 2;' id='img_$nom_col'")
+	. "<div style='width: 150px; height: 100%; overflow: auto; position: absolute; top: 0px; $spip_lang_left: "
+	.($left-150)
+	."px;'>"
+	. $ret
+	. "\n</div>"
+	. ($next
+	   ? mini_afficher_rubrique($id_rubrique, $rac, $list, $col+1, $exclu)
+	   : "\n<div id='$nom_col'></div>")
+	. "\n</div>";
 }
 
 

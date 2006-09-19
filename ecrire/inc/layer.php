1cbafa62dba3cfe5638b5f702170199c970614a6
esj <esj@rezo.net>
2006-09-19 21:51:23 +0000

    Migration des icones et des fichiers .js:
    L'utilisation de Spip avec LDAP et plus généralement avec un .htaccess dans ecrire/ provoquait des demandes d'authentification dans l'espace public,
    suite à la migration (pour cause de mutualisation) dans ecrire/img_pack/icones et ecrire/img_pack/icones_barre, des icones autrefois dans IMG/icones et IMG/icones_barre. Problème similaire avec les fichiers Javascript qui ne peuvent plus etre dans ecrire/ lorsqu'ils sont référencés dans le squelette agenda. 
    
    En conséquence, création d'un répertoire dist/img comportant trois sous-répertoires: les 2 d'icones mentionnés ci-dessus, et un répertoire javascript/ contenant tous les .js auparavant dans img_pack. Le changement est transparent pour les icones, car ils étaient référencés par deux constantes qu'il a suffit de redéfinir:
    
    define('_DIR_IMG_ICONES_DIST', _DIR_RACINE . "dist/img/icones/");
    define('_DIR_IMG_ICONES_BARRE', _DIR_RACINE . "dist/img/icones_barre/");
    
    Pour Javascript, introduction de la constante:
    
    define('_DIR_JAVASCRIPT', (_DIR_RACINE . 'dist/javascript/'));
    
    et utilisation de celle-ci dans les squelettes et les .php référençant ces fichiers.
    
    A terme il faudra prévoir la migration de tout le reste de img_pack, le squlette agenda ne faisant que préfigurer un partage des ressources entre les deux espaces.
    
    
    git-svn-id: svn://trac.rezo.net/spip/spip@7435 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/layer.php
+++ ecrire/inc/layer.php
@@ -136,32 +136,32 @@ function verif_butineur() {
 	if (!eregi("opera", $browser_description)&&eregi("opera", $browser_name)) {
 		$browser_name = "Opera";
 		$browser_version = $match[2];
-		$browser_layer = (($browser_version < 7) ? '' :  http_script('', _DIR_IMG_PACK . 'layer.js',''));
+		$browser_layer = (($browser_version < 7) ? '' :  http_script('', _DIR_JAVASCRIPT . 'layer.js',''));
 		$browser_barre = ($browser_version >= 8.5); 
 	}
 	else if (eregi("opera", $browser_description)) {
 		eregi("Opera ([^\ ]*)", $browser_description, $match);
 		$browser_name = "Opera";
 		$browser_version = $match[1];
-		$browser_layer = (($browser_version < 7) ? '' :  http_script('', _DIR_IMG_PACK . 'layer.js',''));
+		$browser_layer = (($browser_version < 7) ? '' :  http_script('', _DIR_JAVASCRIPT . 'layer.js',''));
 		$browser_barre = ($browser_version >= 8.5); 
 	}
 	else if (eregi("msie", $browser_description)) {
 		eregi("MSIE ([^;]*)", $browser_description, $match);
 		$browser_name = "MSIE";
 		$browser_version = $match[1];
-		$browser_layer = (($browser_version < 5) ? '' :  http_script('', _DIR_IMG_PACK . 'layer.js',''));
+		$browser_layer = (($browser_version < 5) ? '' :  http_script('', _DIR_JAVASCRIPT . 'layer.js',''));
 		$browser_barre = ($browser_version >= 5.5);
 	}
 	else if (eregi("KHTML", $browser_description) &&
 		eregi("Safari/([^;]*)", $browser_description, $match)) {
 		$browser_name = "Safari";
 		$browser_version = $match[1];
-		$browser_layer = http_script('', _DIR_IMG_PACK . 'layer.js','');
+		$browser_layer = http_script('', _DIR_JAVASCRIPT . 'layer.js','');
 		$browser_barre = ($browser_version >= 5.0);
 	}
 	else if (eregi("mozilla", $browser_name) AND $browser_version >= 5) {
-		$browser_layer = http_script('', _DIR_IMG_PACK . 'layer.js','');
+		$browser_layer = http_script('', _DIR_JAVASCRIPT . 'layer.js','');
 		// Numero de version pour Mozilla "authentique"
 		if (ereg("rv:([0-9]+\.[0-9]+)", $browser_description, $match))
 			$browser_rev = doubleval($match[1]);

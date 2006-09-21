212f0f3c48a06c766254b9497d825cf7943f02de
fil <fil@rezo.net>
2006-09-21 16:16:36 +0000

    calendrier casse car aussi bien exec que fragments
    git-svn-id: svn://trac.rezo.net/spip/spip@7444 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

---
+++ ecrire/fragments/calendrier.php
@@ -0,0 +1,21 @@
+<?php
+
+/***************************************************************************\
+ *  SPIP, Systeme de publication pour l'internet                           *
+ *                                                                         *
+ *  Copyright (c) 2001-2006                                                *
+ *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
+ *                                                                         *
+ *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
+ *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
+\***************************************************************************/
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+# le calendrier parle aussi bien exec/ que fragments
+function fragments_calendrier_dist() {
+	$f = charger_fonction('calendrier','exec');
+	return $f();
+}
+
+?>

4df6c88bb2c57a0a483356c97ad09707cdc1410a
renato <renato@rezo.net>
2006-09-23 18:39:21 +0000

    Fix a bug with the link that show the translation status of the articles
    git-svn-id: svn://trac.rezo.net/spip/branches/spip-1.9@7460 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/presentation.php
+++ ecrire/inc/presentation.php
@@ -79,10 +79,14 @@ function debut_cadre($style, $icone = "", $fonction = "", $titre = "") {
 	global $spip_display, $spip_lang_left;
 	static $accesskey = 97; // a
 
+	$style_cadre = " style='zoom:1;";
 	if ($spip_display != 1 AND $spip_display != 4 AND strlen($icone) > 1) {
 		$style_gauche = " padding-$spip_lang_left: 38px;";
-		$style_cadre = " style='margin-top: 14px;'";
-	} else $style_cadre = $style_gauche = '';
+		$style_cadre .= "margin-top: 14px;'";
+	} else {
+		$style_cadre .= "'";  
+		$style_gauche = '';
+	}
 	
 	// accesskey pour accessibilite espace prive
 	if ($accesskey <= 122) // z
@@ -125,7 +129,7 @@ function debut_cadre($style, $icone = "", $fonction = "", $titre = "") {
 	
 	$ret .= "</div>";
 	
-	$ret .= "<div class='cadre-padding' style='overflow:hidden;'>";
+	$ret .= "<div class='cadre-padding' style='overflow:hidden;zoom:1'>";
 
 
 	return $ret;
@@ -830,7 +834,7 @@ function afficher_articles($titre_table, $requete, $afficher_visites = false, $a
 
 			if ($afficher_trad) {
 				$texte_img .= http_img_pack("searching.gif", "*", "style='visibility: hidden; float: $spip_lang_right' id = 'img_$div_trad'");
-				$texte_img .= "<div style='float: $spip_lang_right;'><a href=\"javascript:charger_id_url('" . generer_url_ecrire('memoriser',"id_ajax_fonc=$id_ajax_trad"). "','$div_trad');\"><img src='". _DIR_IMG_PACK . "langues-12.gif' /></a></div>";
+				$texte_img .= "<div style='float: $spip_lang_right;'><a href=\"#\" onclick=\"return charger_id_url('" . generer_url_ecrire('memoriser',"id_ajax_fonc=$id_ajax_trad"). "','$div_trad');\"><img src='". _DIR_IMG_PACK . "langues-12.gif' /></a></div>";
 			}
 			bandeau_titre_boite2($texte_img.$titre_table, "article-24.gif");
 
@@ -1054,7 +1058,7 @@ function afficher_articles_trad($titre_table, $requete, $afficher_visites = fals
 			
 			$texte_img .= http_img_pack("searching.gif", "*", "style='visibility: hidden; float: $spip_lang_right' id = 'img_$div_trad'");
 
-			$texte_img .= "<div style='float: $spip_lang_right;'><a href=\"javascript:charger_id_url('" . generer_url_ecrire('memoriser',"id_ajax_fonc=$id_ajax_trad") . "','$div_trad');\"><img src='". _DIR_IMG_PACK . "langues-off-12.gif' /></a></div>";
+			$texte_img .= "<div style='float: $spip_lang_right;'><a href=\"#\" onclick=\"return charger_id_url('" . generer_url_ecrire('memoriser',"id_ajax_fonc=$id_ajax_trad") . "','$div_trad');\"><img src='". _DIR_IMG_PACK . "langues-off-12.gif' /></a></div>";
 
 			bandeau_titre_boite2($texte_img.$titre_table, "article-24.gif");
 

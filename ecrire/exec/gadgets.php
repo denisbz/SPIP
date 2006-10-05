314718d9e1048c1c4a396f2a7f5870b4a8aa8584
esj <esj@rezo.net>
2006-10-05 21:48:29 +0000

    Le JavaScript asynchrone n'oblige pas à des dépots asynchrones ....
    git-svn-id: svn://trac.rezo.net/spip/spip@7556 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

---
+++ ecrire/exec/gadgets.php
@@ -0,0 +1,23 @@
+<?php
+
+/***************************************************************************\
+ *  SPIP, Systeme de publication pour l'internet                           *
+ *                                                                         *
+ *  Copyright (c) 2001-2006                                                *
+ *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
+ *                                                                         *
+ *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
+ *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
+\***************************************************************************/
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function exec_gadgets_dist()
+{
+	$id_rubrique = intval(_request('id_rubrique'));
+	$gadget = _request('gadget');
+	$gadgets = charger_fonction('gadgets', 'inc');
+
+	return $gadgets($id_rubrique, $gadget);
+}
+?>

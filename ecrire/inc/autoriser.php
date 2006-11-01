feac1e9995f41182ae47effb9dd91072421d4b39
fil <fil@rezo.net>
2006-11-01 08:45:35 +0000

    demarrage de l'API d'autorisations, tire' de widgets
    git-svn-id: svn://trac.rezo.net/spip/spip@7741 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

---
+++ ecrire/inc/autoriser.php
@@ -0,0 +1,137 @@
+<?php
+
+/***************************************************************************\
+ *  SPIP, Systeme de publication pour l'internet                           *
+ *                                                                         *
+ *  Copyright (c) 2001-2006                                                *
+ *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
+ *                                                                         *
+ *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
+ *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
+\***************************************************************************/
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+
+// API pour une fonction generique d'autorisation :
+// $qui est : vide (on prend alors auteur_session)
+//            un id_auteur (on regarde dans la base)
+//            un tableau auteur complet
+// $faire est une action ('modifier', 'publier'...)
+// $type est un type d'objet ou nom de table ('article')
+// $id est l'id de l'objet sur lequel on veut agir
+// $opt (inutilise pour le moment) = options sous forme de tableau associatif
+// (par exemple pour preciser si l'autorisation concerne tel ou tel champ)
+//
+// Seul le premier argument est obligatoire
+define ('_DEBUG_AUTORISER', false);
+function autoriser($faire, $type='', $id=0, $qui = NULL, $opt = NULL) {
+	static $restreint = array();
+
+	// Qui ? auteur_session ?
+	if (!is_array($qui)) {
+		if (is_int($qui)) {
+			$qui = spip_fetch_array(spip_query(
+			"SELECT * FROM spip_auteurs WHERE id_auteur=".$qui));
+		} else {
+			$qui = $GLOBALS['auteur_session'];
+		}
+	}
+
+	// Admins restreints, les verifier ici (pas generique mais...)
+	// Par convention $restreint est un array des rubriques autorisees
+	// (y compris leurs sous-rubriques), ou 0 si admin complet
+	if (is_array($qui)
+	AND $qui['statut'] == '0minirezo'
+	AND !isset($qui['rubriques'])) {
+		if (!isset($restreint[$qui['id_auteur']])) {
+			include_spip('inc/auth'); # pour auth_rubrique
+			$restreint[$qui['id_auteur']] = auth_rubrique($qui['id_auteur'], $qui['statut']);
+		}
+		$qui['restreint'] = $restreint[$qui['id_auteur']];
+	}
+
+	if (_DEBUG_AUTORISER) spip_log("autoriser $faire $type $id ?");
+
+	// Chercher une fonction d'autorisation explicite
+	if (
+
+	// 1. Sous la forme "autoriser_faire_type"
+		(
+		$f = 'autoriser_'.$faire.'_'.$type
+		AND (function_exists($f) OR function_exists($f.='_dist'))
+		)
+
+	// 2. Sous la forme "autoriser_faire"
+	OR (
+		$f = 'autoriser_'.$faire
+		AND (function_exists($f) OR function_exists($f.='_dist'))
+		)
+
+	// 3. Sinon autorisation generique
+	OR (
+		$f = 'autoriser_defaut'
+		AND (function_exists($f) OR function_exists($f.='_dist'))
+		)
+	)
+		$a = $f($faire,$type,$id,$qui,$opt);
+
+	if (_DEBUG_AUTORISER) spip_log("$f($faire,$type,$id): ".($a?'OK':'niet'));
+
+	return $a;
+}
+
+// Autorisation par defaut : les admins complets OK, les autres non
+function autoriser_defaut_dist($faire, $type, $id, $qui, $opt) {
+	return
+		$qui['statut'] == '0minirezo'
+		AND !$qui['restreint'];
+}
+
+// Autoriser a publier dans la rubrique $id
+function autoriser_publier_dans_rubrique_dist($faire, $type, $id, $qui, $opt) {
+	return
+		($qui['statut'] == '0minirezo')
+		AND (!$qui['restreint']
+			? true
+			: in_array($id, $qui['restreint'])
+		);
+}
+
+// Autoriser a modifier la rubrique $id
+// = publier_dans rubrique $id
+function autoriser_modifier_rubrique_dist($faire, $type, $id, $qui, $opt) {
+	return
+		autoriser('publier_dans', 'rubrique', $id, $qui, $opt);
+}
+
+// Autoriser a modifier la breve $id
+// = admins & redac si la breve n'est pas publiee
+// = admins de rubrique parente si publiee
+function autoriser_modifier_breve_dist($faire, $type, $id, $qui, $opt) {
+	$s = spip_query(
+	"SELECT id_rubrique,statut FROM spip_breves WHERE id_breve=".intval($id));
+	$r = spip_fetch_array($s);
+	return
+		($r['statut'] == 'publie')
+			? autoriser('publier_dans', 'rubrique', $r['id_rubrique'], $qui, $opt)
+			: in_array($qui['statut'], array('0minirezo', '1comite'));
+}
+
+// Autoriser a modifier l'article $id
+// = publier_dans rubrique parente
+// = ou statut 'prop,prepa' et $qui est auteur
+function autoriser_modifier_article_dist($faire, $type, $id, $qui, $opt) {
+	$s = spip_query(
+	"SELECT id_rubrique,statut FROM spip_articles WHERE id_article=".intval($id));
+	$r = spip_fetch_array($s);
+	return
+		autoriser('publier_dans', 'rubrique', $r['id_rubrique'], $qui, $opt)
+		OR (
+			in_array($qui['statut'], array('0minirezo', '1comite'))
+			AND in_array($r['statut'], array('prop','prepa'))
+			AND spip_num_rows(auteurs_article($id, "id_auteur=".$qui['id_auteur']))
+		);
+}
+
+?>

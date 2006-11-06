3beae7dc7815b6fd23d10f2c03d19f75ecc276fa
fil <fil@rezo.net>
2006-11-06 13:53:38 +0000

    la puce de changement de statut dans les breves tombait sur une erreur 'action instituer_breve non trouvee'
    git-svn-id: svn://trac.rezo.net/spip/spip@7804 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

---
+++ ecrire/action/instituer_breve.php
@@ -0,0 +1,36 @@
+<?php
+
+/***************************************************************************\
+ *  SPIP, Systeme de publication pour l'internet                           *
+ *                                                                         *
+ *  Copyright (c) 2001-2006                                                *
+ *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
+ *                                                                         *
+ *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
+ *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
+\***************************************************************************/
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+// http://doc.spip.org/@action_instituer_breve_dist
+function action_instituer_breve_dist() {
+
+	include_spip('inc/actions');
+	$var_f = charger_fonction('controler_action_auteur', 'inc');
+	$var_f();
+
+	$arg = _request('arg');
+
+	list($id_breve, $statut) = preg_split('/\W/', $arg);
+	if (!$statut) $statut = _request('statut_nouv'); // cas POST
+	if (!$statut) return; // impossible mais sait-on jamais
+
+	$id_breve = intval($id_breve);
+
+	include_spip('action/editer_breve');
+
+	revisions_breves($id_breve, array('statut' => $statut));
+
+}
+
+?>

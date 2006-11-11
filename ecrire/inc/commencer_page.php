7c896a9f27520891f6a607c30a80fd3049d82456
cedric <cedric@yterium.com>
2006-11-11 17:55:49 +0000

    ajouter deux classes sur le body pour permettre de styler une page en particulier (fil)
    git-svn-id: svn://trac.rezo.net/spip/spip@7856 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/commencer_page.php
+++ ecrire/inc/commencer_page.php
@@ -82,7 +82,7 @@ function init_body($rubrique='accueil', $sous_rubrique='accueil', $id_rubrique='
 
 	if ($spip_ecran == "large") $largeur = 974; else $largeur = 750;
 
-	$res = pipeline('body_prive',"<body ". _ATTRIBUTES_BODY . '>')
+	$res = pipeline('body_prive',"<body class='$rubrique $sous_rubrique'". _ATTRIBUTES_BODY . '>')
 	. "\n<map name='map_layout' id='map_layout'>"
 	. lien_change_var (self(), 'set_disp', 1, '1,0,18,15', _T('lien_afficher_texte_seul'), "onmouseover=\"changestyle('bandeauvide');\"")
 	. lien_change_var (self(), 'set_disp', 2, '19,0,40,15', _T('lien_afficher_texte_icones'), "onmouseover=\"changestyle('bandeauvide');\"")

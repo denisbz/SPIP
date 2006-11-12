3a42aefbbbf03d0b453a42f71f6f635b885d6a03
fil <fil@rezo.net>
2006-11-12 20:02:08 +0000

    pour le css c'est le premier /head qui nous tombe sous la main (toggg)
    git-svn-id: svn://trac.rezo.net/spip/spip@7867 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/public/admin.php
+++ ecrire/public/admin.php
@@ -13,37 +13,47 @@
 
 // Inserer la feuille de style selon les normes, dans le <head>
 // puis les boutons
-// Feuilles de style admin : d'abord la CSS officielle, puis la perso,
+// Feuilles de style admin : d'abord la CSS officielle, puis la perso
 
 // http://doc.spip.org/@affiche_boutons_admin
 function affiche_boutons_admin($contenu) {
 	include_spip('inc/filtres');
+
+	// Compatibilite : on utilise stripos/strripos() qui n'existent pas en php4
+	if (!function_exists('strripos')) {
+		function strripos($botte, $aiguille) {
+			return strrpos(strtolower($botte), $aiguille);
+		}
+	}
+	if (!function_exists('stripos')) {
+		function stripos($botte, $aiguille) {
+			return strpos(strtolower($botte), $aiguille);
+		}
+	}
+
+	// Inserer le css d'admin
 	$css = "<link rel='stylesheet' href='".url_absolue(find_in_path('spip_admin.css'))
 	. "' type='text/css' />\n";
 	if ($f = find_in_path('spip_admin_perso.css'))
 		$css .= "<link rel='stylesheet' href='"
 		. url_absolue($f) . "' type='text/css' />\n";
 
-	// Compatibilite : on utilise strripos() qui n'existe pas en php4
-	if (!function_exists('strripos')) {
-		function strripos($botte, $aiguille) {
-			return strrpos(strtolower($botte), $aiguille);
-		}
-	}
-
-	($pos = strripos($contenu, '</head>'))
-	    || ($pos = strripos($contenu, '<body>'))
+	($pos = stripos($contenu, '</head>'))
+	    || ($pos = stripos($contenu, '<body>'))
 	    || ($pos = 0);
 	$contenu = substr_replace($contenu, $css, $pos, 0);
 
-	// Recuperer sans l'afficher la balise #FORMULAIRE_ADMIN, en float
+
+	// Inserer la balise #FORMULAIRE_ADMIN, en float
 	$boutons_admin = inclure_balise_dynamique(
 		balise_FORMULAIRE_ADMIN_dyn('spip-admin-float'),
 	false);
+
 	($pos = strripos($contenu, '</body>'))
 	    || ($pos = strripos($contenu, '</html>'))
 	    || ($pos = strlen($contenu));
 	$contenu = substr_replace($contenu, $boutons_admin, $pos, 0);
+
 
 	return $contenu;
 }

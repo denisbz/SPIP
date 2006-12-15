f904b659a1eefe9555b374d2cbebdf60859b569c
christian_lefebvre <christian_lefebvre@laposte.net>
2006-12-15 22:42:34 +0000

    autodoc
    git-svn-id: svn://trac.rezo.net/spip/spip@8083 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/exec/info_plugin.php
+++ ecrire/exec/info_plugin.php
@@ -4,6 +4,7 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 include_spip('inc/plugin');
 
+// http://doc.spip.org/@exec_info_plugin
 function exec_info_plugin() {
 	$plug = _request('plug');
 	$info = plugin_get_infos($plug);

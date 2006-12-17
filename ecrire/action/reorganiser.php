b40def836959d799ce367e144d4128f513192b71
fil <fil@rezo.net>
2006-12-17 22:08:17 +0000

    le (c) c'est important, en 2007 comme en 2006
    git-svn-id: svn://trac.rezo.net/spip/spip@8104 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/action/reorganiser.php
+++ ecrire/action/reorganiser.php
@@ -3,7 +3,7 @@
 /***************************************************************************\
  *  SPIP, Systeme de publication pour l'internet                           *
  *                                                                         *
- *  Copyright (c) 2001-2006                                                *
+ *  Copyright (c) 2001-2007                                                *
  *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
  *                                                                         *
  *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *

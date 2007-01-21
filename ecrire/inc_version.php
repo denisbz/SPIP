a1da40bb71f97d45783a830d4f042b022147dcb0
cedric <cedric@yterium.com>
2007-01-21 18:11:46 +0000

    "on incremente pour le support generalise de autoriser()"
    git-svn-id: svn://trac.rezo.net/spip/spip@8451 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc_version.php
+++ ecrire/inc_version.php
@@ -295,7 +295,7 @@ $spip_version = 1.925;
 // (ce qui marche pour yy ne marchera pas forcement sur une version plus ancienne)
 // type nouvelles fonctionnalites, deplacement de fonctions ...
 $spip_version_affichee = '1.9.2 beta 3';
-$spip_version_code = 1.9205;
+$spip_version_code = 1.9206;
 
 // ** Securite **
 $auteur_session = $connect_statut = $connect_toutes_rubriques = $connect_id_rubrique = $hash_recherche = $hash_recherche_strict = '';

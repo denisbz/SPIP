84f1fb24b84f83af43cd43556975d5cb4be0d2ab
cedric <cedric@yterium.com>
2007-01-24 16:32:04 +0000

    parametre_url pour ajouter un argument, ca evite un clash depuis une url publique
    git-svn-id: svn://trac.rezo.net/spip/spip@8490 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/action/joindre.php
+++ ecrire/action/joindre.php
@@ -60,7 +60,7 @@ function action_joindre_dist()
 
      $redirect = urldecode($redirect);
      if ($documents_actifs) {
-	$redirect .= '&show_docs=' . join(',',$documents_actifs);
+	$redirect = parametre_url($redirect,'show_docs',join(',',$documents_actifs));
      }
      
     if (!$ancre) {
@@ -80,7 +80,7 @@ function action_joindre_dist()
      }
 
 	if(_request("iframe") == 'iframe') {
-		$redirect = urldecode($iframe_redirect)."&show_docs=".join(',',$documents_actifs)."&iframe=iframe";
+		$redirect = parametre_url(urldecode($iframe_redirect),"show_docs",join(',',$documents_actifs)."&iframe=iframe");
 	}
 
 	redirige_par_entete($redirect);

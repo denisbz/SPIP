d420518feed089beb7c572d9f30b3417833ab779
esj <esj@rezo.net>
2007-02-09 14:19:25 +0000

    Seuls les articles travaillent du chapeau.
    git-svn-id: svn://trac.rezo.net/spip/spip@8637 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/public/balises.php
+++ ecrire/public/balises.php
@@ -515,11 +515,15 @@ function balise_SPIP_CRON_dist ($p) {
 function balise_INTRODUCTION_dist ($p) {
 	$type = $p->type_requete;
 	$_texte = champ_sql('texte', $p);
-	$_chapo = champ_sql('chapo', $p);
-	$_descriptif = champ_sql('descriptif', $p);
+	if ($type == 'articles') {
+	  $_chapo = champ_sql('chapo', $p);
+	  $_descriptif =  champ_sql('descriptif', $p);
+	} else {
+	  $_chapo = "''";
+	  $_descriptif =  "''";
+	}
 	$p->code = "calcul_introduction('$type', $_texte, $_chapo, $_descriptif)";
 
-	#$p->interdire_scripts = true;
 	return $p;
 }
 

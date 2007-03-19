0d6fd81bcf780c62949d4ae62ba133bea2b2155d
fil <fil@rezo.net>
2007-03-19 17:09:44 +0000

    report de [8906]
    git-svn-id: svn://trac.rezo.net/spip/branches/spip-1.9.2@8907 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc_version.php
+++ ecrire/inc_version.php
@@ -365,6 +365,8 @@ OR _request('action') == 'test_dirs')) {
 	} else {
 	// Si on est dans le site public, dire que qq s'en occupe
 		include_spip('inc/minipres');
+		include_spip('inc/lang');
+		utiliser_langue_visiteur();
 		echo minipres(_T('info_travaux_titre'), "<p style='text-align: center;'>"._T('info_travaux_texte')."</p>");
 		exit;
 	}

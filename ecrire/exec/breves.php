d9f4bf4559a50d40fedcc48b5192b191918929e7
cedric <cedric@yterium.com>
2007-04-17 21:36:36 +0000

    "une icone a faire floater au bon endroit"
    git-svn-id: svn://trac.rezo.net/spip/spip@9070 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/exec/breves.php
+++ ecrire/exec/breves.php
@@ -48,7 +48,9 @@ function exec_breves_dist()
 
 		if ($editable) {
 		  if (!$res) echo bandeau_titre_boite2($titre, "breve-24.gif", 'toile_foncee', "ligne_blanche");
-		  echo icone(_T('icone_nouvelle_breve'), generer_url_ecrire("breves_edit","new=oui&id_rubrique=$id_rubrique"), "breve-24.gif", "creer.gif", $spip_lang_right);
+		  echo "<div style='float:$spip_lang_right'>"
+		  . icone(_T('icone_nouvelle_breve'), generer_url_ecrire("breves_edit","new=oui&id_rubrique=$id_rubrique"), "breve-24.gif", "creer.gif", $spip_lang_right)
+		  . "</div><br class='nettoyeur' />";
 		}
 
 	}

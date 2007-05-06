d9c7316d0bb1cc3bf8fb2212e7ef8c0f5e2b0361
fil <fil@rezo.net>
2007-05-06 01:20:53 +0000

    up version code pour les autoriser(configurer, backup, destroy)
    git-svn-id: svn://trac.rezo.net/spip/spip@9175 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc_version.php
+++ ecrire/inc_version.php
@@ -297,7 +297,7 @@ $spip_version = 1.935;
 // (ce qui marche pour yy ne marchera pas forcement sur une version plus ancienne)
 // type nouvelles fonctionnalites, deplacement de fonctions ...
 $spip_version_affichee = '1.9.3 dev';
-$spip_version_code = '1.9251';
+$spip_version_code = '1.9252';
 
 // ** Securite **
 $auteur_session = $connect_statut = $connect_toutes_rubriques =  $hash_recherche = $hash_recherche_strict = $ldap_present ='';

c89b91f98d2f1679400ed677d88f007238a02a5b
christian_lefebvre <christian_lefebvre@laposte.net>
2007-05-11 21:42:43 +0000

    autodoc
    git-svn-id: svn://trac.rezo.net/spip/spip@9239 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/exec/accueil.php
+++ ecrire/exec/accueil.php
@@ -376,6 +376,7 @@ function etat_base_accueil()
 	return $res ;
 }
 
+// http://doc.spip.org/@acceuil_liste_participants
 function acceuil_liste_participants()
 {
 	$q = spip_query("SELECT COUNT(*) AS cnt, statut FROM spip_auteurs GROUP BY statut HAVING cnt <>0 AND statut IN ('" . join("','", $GLOBALS['liste_des_statuts']) . "')");

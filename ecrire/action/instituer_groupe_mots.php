a93e8a282b3df96f1f64c241531e89e001cd0653
esj <esj@rezo.net>
2007-07-03 05:19:43 +0000

    Pb 8 avec [9649].
    git-svn-id: svn://trac.rezo.net/spip/spip@9658 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/action/instituer_groupe_mots.php
+++ ecrire/action/instituer_groupe_mots.php
@@ -25,16 +25,25 @@ function action_instituer_groupe_mots_dist()
 	  action_instituer_groupe_mots_get($arg);
 	elseif (!preg_match(",^(-?\d+)$,", $arg, $r)) {
 		 spip_log("action_instituer_groupe_mots_dist $arg pas compris");
-	} else action_instituer_groupe_mots_post($r);
+	} else action_instituer_groupe_mots_post($r[1]);
 }
 
 
 // http://doc.spip.org/@action_instituer_groupe_mots_post
-function action_instituer_groupe_mots_post($r)
+function action_instituer_groupe_mots_post($id_groupe)
 {
-	global $acces_comite, $acces_forum, $acces_minirezo, $new, $articles, $breves, $change_type, $descriptif, $id_groupe, $obligatoire, $rubriques, $syndic, $texte, $unseul;
-
-	$id_groupe = $r[1];
+	$acces_comite = _request('acces_comite');
+	$acces_forum = _request('acces_forum');
+	$acces_minirezo = _request('acces_minirezo');
+	$articles = _request('articles');
+	$breves = _request('breves');
+	$change_type = _request('change_type');
+	$descriptif = _request('descriptif');
+	$obligatoire = _request('obligatoire');
+	$rubriques = _request('rubriques');
+	$syndic = _request('syndic');
+	$texte = _request('texte');
+	$unseul = _request('unseul');
 
 	if ($id_groupe < 0){
 		spip_query("DELETE FROM spip_groupes_mots WHERE id_groupe=" . (0- $id_groupe));
@@ -49,7 +58,7 @@ function action_instituer_groupe_mots_post($r)
 			spip_query("UPDATE spip_groupes_mots SET titre=" . _q($change_type) . ", texte=" . _q($texte) . ", descriptif=" . _q($descriptif) . ", unseul=" . _q($unseul) . ", obligatoire=" . _q($obligatoire) . ", articles=" . _q($articles) . ", breves=" . _q($breves) . ", rubriques=" . _q($rubriques) . ", syndic=" . _q($syndic) . ",	minirezo=" . _q($acces_minirezo) . ", comite=" . _q($acces_comite) . ", forum=" . _q($acces_forum) . " WHERE id_groupe=$id_groupe");
 
 		} else {	// creation groupe
-		  spip_abstract_insert('spip_groupes_mots', "(titre, texte, descriptif, unseul,  obligatoire, articles, breves, rubriques, syndic, minirezo, comite, forum)", "(" . _q($change_type) . ", " . _q($texte) . " , " . _q($descriptif) . " , " . _q($unseul) . " , " . _q($obligatoire) . " , " . _q($articles) . " ," . _q($breves) . " , " . _q($rubriques) . " , " . _q($syndic) . " , " . _q($acces_minirezo) . " ,  " . _q($acces_comite) . " , " . _q($acces_forum) . " )");
+			spip_abstract_insert('spip_groupes_mots', "(titre, texte, descriptif, unseul,  obligatoire, articles, breves, rubriques, syndic, minirezo, comite, forum)", "(" . _q($change_type) . ", " . _q($texte) . " , " . _q($descriptif) . " , " . _q($unseul) . " , " . _q($obligatoire) . " , " . _q($articles) . " ," . _q($breves) . " , " . _q($rubriques) . " , " . _q($syndic) . " , " . _q($acces_minirezo) . " ,  " . _q($acces_comite) . " , " . _q($acces_forum) . " )");
 		}
 	}
 }

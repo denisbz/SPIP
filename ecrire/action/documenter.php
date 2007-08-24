df470ebc7ba79c186a936227a48d6173daf8fb01
fil <fil@rezo.net>
2007-08-24 20:07:37 +0000

    un NULL dans spip_documents_articles.vu faisait qu'on ne pouvvait plus effacer un document : je suppose qu'il faudrait corriger ailleurs qu'ici ???
    git-svn-id: svn://trac.rezo.net/spip/spip@10119 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/action/documenter.php
+++ ecrire/action/documenter.php
@@ -29,21 +29,25 @@ function action_documenter_dist()
 function action_documenter_post($r)
 {
 	list($x, $sign, $id, $type, $vignette) = $r;
-	if ($vignette){
+
+	if ($vignette) {
 		// on ne supprime pas, on dissocie
 		// supprimer_document_et_vignette($vignette);
 		// on dissocie, mais si le doc est utilise dans le texte, il sera reassocie ..., donc condition sur vu !
-		spip_query("DELETE FROM spip_documents_".$type."s WHERE id_$type=$id AND id_document=$vignette AND vu='non'");
+		spip_query("DELETE FROM spip_documents_".$type."s WHERE id_$type=$id AND id_document=$vignette AND (vu='non' OR vu IS NULL)");
 	}
 	else {
-			if ($sign)
-				$x = spip_query("SELECT docs.id_document FROM spip_documents AS docs, spip_documents_".$type."s AS l WHERE l.id_$type=$id AND l.id_document=docs.id_document AND docs.mode='document' AND docs.extension IN ('gif', 'jpg', 'png')");
-			else $x = spip_query("SELECT docs.id_document FROM spip_documents AS docs, spip_documents_".$type."s AS l WHERE l.id_$type=$id AND l.id_document=docs.id_document AND docs.mode='document'  AND docs.extension NOT IN ('gif', 'jpg', 'png')");
-			while($r = sql_fetch($x)) {
-				//supprimer_document_et_vignette($r['id_document']);
-				// on dissocie, mais si le doc est utilise dans le texte, il sera reassocie ..., donc condition sur vu !
-				spip_query("DELETE FROM spip_documents_".$type."s WHERE id_$type=$id AND id_document=".$r['id_document']." AND vu='non'");
-			}
+		if ($sign)
+			$x = spip_query("SELECT docs.id_document FROM spip_documents AS docs, spip_documents_".$type."s AS l WHERE l.id_$type=$id AND l.id_document=docs.id_document AND docs.mode='document' AND docs.extension IN ('gif', 'jpg', 'png')");
+		else
+			$x = spip_query("SELECT docs.id_document FROM spip_documents AS docs, spip_documents_".$type."s AS l WHERE l.id_$type=$id AND l.id_document=docs.id_document AND docs.mode='document'  AND docs.extension NOT IN ('gif', 'jpg', 'png')");
+
+		while ($r = sql_fetch($x)) {
+			// supprimer_document_et_vignette($r['id_document']);
+			// on dissocie, mais si le doc est utilise dans le texte,
+			// il sera reassocie ..., donc condition sur vu !
+			spip_query("DELETE FROM spip_documents_".$type."s WHERE id_$type=$id AND id_document=".$r['id_document']." AND (vu='non' OR vu IS NULL)");
+		}
 	}
 	if ($type == 'rubrique') {
 		include_spip('inc/rubriques');

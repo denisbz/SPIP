8db93485b798acfa88b8a636860a82f8e007af82
james <james@rezo.net>
2007-08-25 11:59:33 +0000

    erreur sur l'incrémentation de spip_version_code (cedric)
    git-svn-id: svn://trac.rezo.net/spip/spip@10132 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc_version.php
+++ ecrire/inc_version.php
@@ -315,7 +315,7 @@ $spip_version = 1.945;
 // (ce qui marche pour yy ne marchera pas forcement sur une version plus ancienne)
 // type nouvelles fonctionnalites, deplacement de fonctions ...
 $spip_version_affichee = '1.9.3 dev';
-$spip_version_code = '1.9301';
+$spip_version_code = '1.9262';
 
 // ** Securite **
 $auteur_session = $connect_statut = $connect_toutes_rubriques =  $hash_recherche = $hash_recherche_strict = $ldap_present ='';

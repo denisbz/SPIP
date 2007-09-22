ca835edbf3c60a9cc648f25c4af6c7750525962d
fil <fil@rezo.net>
2007-09-22 21:26:10 +0000

    ne pas empecher un fonctionnement en sous-dossier quand on vient de restaurer un site dont l'url n'est pas en sous-dossier (touti, gregoire)
    git-svn-id: svn://trac.rezo.net/spip/spip@10391 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/utils.php
+++ ecrire/inc/utils.php
@@ -1337,13 +1337,15 @@ function spip_initialisation($pi=NULL, $pa=NULL, $ti=NULL, $ta=NULL) {
 	}
 
 	// nombre de repertoires depuis la racine
-	if (isset($_SERVER['REQUEST_URI'])
-	AND isset($GLOBALS['meta']['adresse_site']))
-		$GLOBALS['profondeur_url'] =
-			substr_count(reset(explode('?', $_SERVER['REQUEST_URI'])),'/')
-			- substr_count($GLOBALS['meta']['adresse_site'],'/') + 1;
-	else
-		$GLOBALS['profondeur_url'] = _DIR_RESTREINT ? 0 : 1;
+	// on compare a l'adresse donnee en meta ; si celle-ci est fausse
+	// le calcul est faux. Meilleure idee ??
+	$GLOBALS['profondeur_url'] = !_DIR_RESTREINT
+		? 1
+		: ((isset($_SERVER['REQUEST_URI']) AND isset($GLOBALS['meta']['adresse_site']))
+			? substr_count(reset(explode('?', $_SERVER['REQUEST_URI'])),'/')
+				- substr_count($GLOBALS['meta']['adresse_site'],'/') + 1
+			: 0
+		);
 
 	// s'il y a un cookie ou PHP_AUTH, initialiser auteur_session
 	if (_FILE_CONNECT) verifier_visiteur();

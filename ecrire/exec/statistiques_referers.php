86a0917f722bf0901c27ed78528bd261f80bfa0f
esj <esj@rezo.net>
2007-10-04 10:59:02 +0000

    Abstraction d'un Where, mais surtout du code mort depuis la 1.8.
    git-svn-id: svn://trac.rezo.net/spip/spip@10505 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/exec/statistiques_referers.php
+++ ecrire/exec/statistiques_referers.php
@@ -18,8 +18,6 @@ include_spip('inc/statistiques');
 // http://doc.spip.org/@exec_statistiques_referers_dist
 function exec_statistiques_referers_dist()
 {
-	global $connect_statut;
-
 	$id_article = intval(_request('id_article'));
 	$jour = _request('jour');
 	$limit  = _request('limit');
@@ -28,19 +26,6 @@ function exec_statistiques_referers_dist()
 		include_spip('inc/minipres');
 		echo minipres();
 		exit;
-	}
-	if ($id_article){
-		$result = sql_select("titre, visites, popularite", "spip_articles", "statut='publie' AND id_article ='$id_article'");
-
-		if ($row = sql_fetch($result)) {
-			$total_absolu = $row['visites'];
-		}
-	}  else {
-		$result = spip_query("SELECT SUM(visites) AS total_absolu FROM spip_visites");
-
-		if ($row = sql_fetch($result)) {
-			$total_absolu = $row['total_absolu'];
-		}
 	}
 
 	$commencer_page = charger_fonction('commencer_page', 'inc');
@@ -51,12 +36,12 @@ function exec_statistiques_referers_dist()
 
 //barre_onglets("statistiques", "referers");
 
-echo debut_gauche('', true);
-echo debut_boite_info(true);
-echo "<p style='font-size:small; text-align:left;' class='verdana1'>"._T('info_gauche_statistiques_referers')."</p>";
-echo fin_boite_info(true);
+	echo debut_gauche('', true);
+	echo debut_boite_info(true);
+	echo "<p style='font-size:small; text-align:left;' class='verdana1'>"._T('info_gauche_statistiques_referers')."</p>";
+	echo fin_boite_info(true);
 
-echo debut_droite('', true);
+	echo debut_droite('', true);
 
 
 //
@@ -64,25 +49,25 @@ echo debut_droite('', true);
 //
 
 // nombre de referers a afficher
-$limit = intval($limit);	//secu
-if ($limit == 0) $limit = 100;
+	$limit = intval($limit);	//secu
+	if ($limit == 0) $limit = 100;
 
-if ($jour<>'veille')
-	$jour='jour';
+	if ($jour<>'veille')
+		$jour='jour';
 
-echo barre_onglets("stat_referers", $jour);
+	echo barre_onglets("stat_referers", $jour);
 
 
 // afficher quels referers ?
 
- $result = spip_query("SELECT referer_md5, referer, SUM(visites_$jour) AS vis FROM spip_referers WHERE visites_$jour>0 GROUP BY referer ORDER BY vis DESC LIMIT $limit");
+	$result = sql_select("referer_md5, referer, SUM(visites_$jour) AS vis", "spip_referers", "visites_$jour>0 ", "referer", "vis DESC", $limit);
 
- echo "<br /><div style='font-size:small;' class='verdana1'>";
- echo aff_referers ($result, $limit, generer_url_ecrire('statistiques_referers', ("jour=$jour&limit=" . strval($limit+200))));
+	echo "<br /><div style='font-size:small;' class='verdana1'>";
+	echo aff_referers ($result, $limit, generer_url_ecrire('statistiques_referers', ("jour=$jour&limit=" . strval($limit+200))));
 
- echo "</div><br />";
+	echo "</div><br />";
 
- echo fin_gauche(), fin_page();
+	echo fin_gauche(), fin_page();
 }
 
 ?>

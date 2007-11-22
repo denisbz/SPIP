e0fd650258a327c4f65756449a0da6bb745ae2cc
christian_lefebvre <christian_lefebvre@laposte.net>
2007-11-22 22:44:07 +0000

    autodoc
    git-svn-id: svn://trac.rezo.net/spip/spip@10842 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/utils.php
+++ ecrire/inc/utils.php
@@ -1290,6 +1290,7 @@ function exec_info_dist() {
 // on recupere le resultat sous la forme d'une $page['texte', 'headers'...]
 // options :
 // 'protect_xml' => false,  conserver le \1 du xml-hack
+// http://doc.spip.org/@assembler
 function assembler($fond, $contexte=array(), $options=array(), $connect=null) {
 	charger_fonction('assembler', 'public');
 

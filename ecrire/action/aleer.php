b74a6ab25b755823237eb154cd74c21e38e4c40b
fil <fil@rezo.net>
2007-12-01 13:45:42 +0000

    le login en une passe retrouve l'affichage du logo
    git-svn-id: svn://trac.rezo.net/spip/spip@10898 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/action/aleer.php
+++ ecrire/action/aleer.php
@@ -74,16 +74,24 @@ include_spip('base/abstract_sql');
 function action_aleer_dist() {
 	$row = array();
 	if ($login=_request('var_login')) {
-		$row =  sql_fetsel('login,alea_actuel,alea_futur,prefs', 'spip_auteurs', "login=" . sql_quote($login));
+		$row =  sql_fetsel('id_auteur,login,alea_actuel,alea_futur,prefs', 'spip_auteurs', "login=" . sql_quote($login));
 		// Retrouver ceux qui signent de leur nom ou email
 		if (!$row AND !spip_connect_ldap()) {
-			$row = sql_fetsel('login,alea_actuel,alea_futur,prefs', 'spip_auteurs', "(nom = " . sql_quote($login) . " OR email = " . sql_quote($login) . ") AND login<>'' AND statut<>'5poubelle'");
+			$row = sql_fetsel('id_auteur,login,alea_actuel,alea_futur,prefs', 'spip_auteurs', "(nom = " . sql_quote($login) . " OR email = " . sql_quote($login) . ") AND login<>'' AND statut<>'5poubelle'");
 		}
 		if ($row) {
 			$prefs = unserialize($row['prefs']);
 			$row['cnx'] = $prefs['cnx'] == 'perma' ? '1' : '0';
 			unset($row['prefs']);
+
+			if ($chercher_logo = charger_fonction('chercher_logo', 'inc')
+			AND list($logo) = $chercher_logo($row['id_auteur'], 'id_auteur', 'on')) {
+				include_spip('inc/filtres');
+				$row['logo'] = reduire_image($logo,100,80);
+			}
 		}
+		unset($row['id_auteur']);
+
 		echo json_export($row);
 	}
 }

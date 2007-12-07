a3e45bcdce8fda3f606f83effb4d9de66348634a
fil <fil@rezo.net>
2007-12-07 08:34:04 +0000

    une periode de flou autour de 4h du mat, histoire de ne pas plomber un herbergeur ayant plein de sites spip
    git-svn-id: svn://trac.rezo.net/spip/spip@10927 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/genie/optimiser.php
+++ ecrire/genie/optimiser.php
@@ -21,7 +21,9 @@ function genie_optimiser_dist($t) {
 
 	// la date souhaitee pour le tour suivant = apres-demain a 4h du mat ;
 	// sachant qu'on a un delai de 48h, on renvoie aujourd'hui a 4h du mat
-	return -mktime(4,0,0);
+	// avec une periode de flou entre 2h et 6h pour ne pas saturer un hebergeur
+	// qui aurait beaucoup de sites SPIP
+	return -(mktime(2,0,0) + rand(3600*4));
 }
 
 // heure de reference pour le garbage collector = 24h auparavant

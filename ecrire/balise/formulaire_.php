b4596deba592a8d99502542b6f644daf09d490d2
cedric <cedric@yterium.com>
2008-02-21 14:41:25 +0000

    "le find in path doit porter sur le .html"
    git-svn-id: svn://trac.rezo.net/spip/spip@11236 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/balise/formulaire_.php
+++ ecrire/balise/formulaire_.php
@@ -29,10 +29,10 @@ function balise_FORMULAIRE__dyn($form)
 		
 	// on enleve le premier qui est le nom de la balise et deja recupere ci-dessus
 	array_shift($args);
-	
-	if (!find_in_path("formulaires/$form"))
+
+	if (!find_in_path("formulaires/$form.html"))
 		return '';
-	
+
 	$erreurs = isset($_POST["erreurs_$form"])?$_POST["erreurs_$form"]:array();
 	$message_ok = isset($_POST["message_ok_$form"])?$_POST["message_ok_$form"]:"";
 	$message_erreur = isset($erreurs['message_erreur'])?$erreurs['message_erreur']:"";
@@ -40,6 +40,7 @@ function balise_FORMULAIRE__dyn($form)
 	$editable = (!isset($_POST["erreurs_$form"])) || count($erreurs) || 
 		(isset($_POST["editable_$form"]) && $_POST["editable_$form"]);
 
+	$valeurs = array();
 	if ($charger_valeurs = charger_fonction("charger","formulaires/$form/",true))
 		$valeurs = call_user_func_array($charger_valeurs,$args);
 	if ($valeurs===false) {

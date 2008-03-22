c2958513aa4216802154a6b6db17622a148568e4
arno <arno@rezo.net>
2008-03-22 08:57:08 +0000

    Couleur_saturation...
    git-svn-id: svn://trac.rezo.net/spip/spip@11344 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/filtres.php
+++ ecrire/inc/filtres.php
@@ -198,6 +198,7 @@ $GLOBALS['spip_matrice']['couleur_eclaircir'] = 'inc/filtres_images.php';
 $GLOBALS['spip_matrice']['couleur_foncer'] = 'inc/filtres_images.php';
 $GLOBALS['spip_matrice']['couleur_foncer_si_claire'] = 'inc/filtres_images.php';
 $GLOBALS['spip_matrice']['couleur_eclaircir_si_foncee'] = 'inc/filtres_images.php';
+$GLOBALS['spip_matrice']['couleur_saturation'] = 'inc/filtres_images.php';
 
 // charge les fonctions graphiques et applique celle demandee
 // http://doc.spip.org/@filtrer

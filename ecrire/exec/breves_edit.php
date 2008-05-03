0979cae8f0cd3f94690f6e71b7cd92498f2bfe30
cedric <cedric@yterium.com>
2008-05-03 12:05:37 +0000

    "code obsolete"
    git-svn-id: svn://trac.rezo.net/spip/spip@11559 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/exec/breves_edit.php
+++ ecrire/exec/breves_edit.php
@@ -120,94 +120,9 @@ function breves_edit_ok($row, $id_breve, $id_rubrique, $new)
 	);
 	$page = evaluer_fond("prive/editer/breve", $contexte, $connect);
 	echo $page['texte'];
-	/*
-if ($new != "oui") {
-	echo icone_inline(_T('icone_retour'), generer_url_ecrire("breves_voir","id_breve=$id_breve"), "breve-24.gif", "rien.gif",$spip_lang_right);
-	echo _T('info_modifier_breve');
-	echo gros_titre($titre,'', false);
-	echo "<br class='nettoyeur' />";
-}
 
-if ($connect_statut=="0minirezo" OR $statut=="prop" OR $new == "oui") {
-	if ($id_breve) $lien = "id_breve=$id_breve";
-
-	$titre = entites_html($titre);
-	$lien_titre = entites_html($lien_titre);
-
-	if ($id_rubrique == 0) $logo_parent = "racine-site-24.gif";
-	else {
-		$result=sql_select("id_parent", "spip_rubriques", "id_rubrique=$id_rubrique");
-
-		while($t=sql_fetch($result)){
-			$parent_parent=$t['id_parent'];
-		}
-		if ($parent_parent == 0) $logo_parent = "secteur-24.gif";
-		else $logo_parent = "rubrique-24.gif";
-	}
-
-	// selecteur de rubrique (en general pas d'ajax car toujours racine)
-	$chercher_rubrique = charger_fonction('chercher_rubrique', 'inc');
-
-	$form = "<ol class='formfx'>";
-	$form .= "<li class='gauche obligatoire'><label for='titre'>" . _T('info_titre') . "</label>"
-	. "<input type='text' class='formo' name='titre' id='titre' value=\"$titre\" size='40' $onfocus /></li>"
-	 . "<input type='hidden' name='id_rubrique_old' value=\"$id_rubrique\" />"
-	 ."<li class='haut'>"
-	. debut_cadre_couleur($logo_parent, true, "",_T('entree_interieur_rubrique').aide ("brevesrub"))
-	. $chercher_rubrique($id_rubrique, 'breve', ($statut == 'publie')) 
-	. fin_cadre_couleur(true)
-	."</li>"
-	 . pipeline('affiche_gauche',array('args'=>array('exec'=>'breves_edit','id_breve'=>$id_breve),'data'=>''))
-	. "<li class='haut'><label for='texte'>"._T('entree_texte_breve')."</label>\n"
-	  . afficher_textarea_barre($texte, false,
-				    "document.getElementById('form_breve')")
-	. "</li>\n"
-	. "<fieldset><legend>"
-	. _T('entree_liens_sites')
-	. aide ("breveslien")
-	. "</legend>\n"
-	. "<li class='gauche'><label for='lien_titre'>" . _T('info_titre')."</label>\n"
-	. "<input type='text' class='forml' name='lien_titre' id='lien_titre' value=\"$lien_titre\" size='40' /></li>\n"
-	. "<li class='gauche'><label for='lien_url'>" . _T('info_url')."</label>\n"
-	. "<input type='text' class='forml' name='lien_url' id='lien_url' value=\"$lien_url\" size='40' /></li></fieldset>";
-
-	if ($GLOBALS['champs_extra']) {
-		include_spip('inc/extra');
-		$form .= extra_saisie($extra, 'breves', $id_rubrique);
-	}
-
-	// Ajouter le controles md5
-	if ($new != 'oui') {
-		include_spip('inc/editer');
-		$form .= controles_md5($row);
-	}
-
-	if (autoriser('publierdans','rubrique',$id_rubrique)) {
-		$form .= debut_cadre_relief('', true)
-		. "<b><label for='statut'>"._T('entree_breve_publiee')."</label></b>\n"
-		. "<select name='statut' id='statut' size='1' class='fondl'>\n"
-		. "<option".mySel("prop",$statut)." style='background-color: white'>"._T('item_breve_proposee')."</option>\n"
-		. "<option".mySel("refuse",$statut). " class='danger'>"._T('item_breve_refusee')."</option>\n"
-		. "<option".mySel("publie",$statut)." style='background-color: #B4E8C5'>"._T('item_breve_validee')."</option>\n"
-		. "</select>".aide ("brevesstatut")."<br />\n"
-		. fin_cadre_relief(true);
-	}
-	$form .= "<div style='text-align: right'><input type='submit' value='"._T('bouton_enregistrer')."' class='fondo' /></div>";
-	$form .= "</ol>";
-	echo generer_action_auteur('editer_breve',
-		$new ? $new : $id_breve,
-		generer_url_ecrire('breves_voir'),
-		$form,
-		" method='post' id='form_breve'"
-	);
-
-}
-else
-	echo "<h2>"._T('info_page_interdite')."</h2>";
-	*/
-
-echo fin_cadre_formulaire(true);
-echo fin_gauche(), fin_page();
+	echo fin_cadre_formulaire(true);
+	echo fin_gauche(), fin_page();
 
 }
 

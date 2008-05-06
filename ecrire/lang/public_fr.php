162ac84661dcde7e8bb2d46a32b6c7added71085
fil <fil@rezo.net>
2008-05-06 17:49:09 +0000

    lien_connecter, par salvatore
    git-svn-id: svn://trac.rezo.net/spip/spip@11577 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/lang/public_fr.php
+++ ecrire/lang/public_fr.php
@@ -1,9 +1,9 @@
 <?php
-
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://trac.rezo.net/spip/spip/ecrire/lang/
+if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-
 
 // A
 'accueil_site' => 'Accueil du site',
@@ -12,65 +12,59 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'articles_populaires' => 'Articles les plus populaires',
 'articles_recents' => 'Articles les plus r&eacute;cents',
 'articles_rubrique' => 'Articles de cette rubrique',
+'aucune_breve' => 'Il n\'y a pas de br&egrave;ve &agrave; cette adresse',
+'aucune_rubrique' => 'Il n\'y a pas de rubrique &agrave; cette adresse',
 'aucun_article' => 'Il n\'y a pas d\'article &agrave; cette adresse',
 'aucun_auteur' => 'Il n\'y a pas d\'auteur &agrave; cette adresse',
 'aucun_site' => 'Il n\'y a pas de site &agrave; cette adresse',
-'aucune_breve' => 'Il n\'y a pas de br&egrave;ve &agrave; cette adresse',
-'aucune_rubrique' => 'Il n\'y a pas de rubrique &agrave; cette adresse',
 'autres' => 'Autres',
 'autres_breves' => 'Autres br&egrave;ves',
 'autres_groupes_mots_clefs' => 'Autres groupes de mots-cl&eacute;s',
 'autres_sites' => 'Autres sites',
 
-
 // B
 'bonjour' => 'Bonjour',
 'breves' => 'Br&egrave;ves',
 
-
 // C
 'commenter_site' => 'Commenter ce site',
 
-
 // D
 'date' => 'Date',
-'dernier_ajout' => 'Dernier ajout',
 'dernieres_breves' => 'Derni&egrave;res br&egrave;ves',
 'derniers_articles' => 'Derniers articles',
 'derniers_commentaires' => 'Derniers commentaires',
 'derniers_messages_forum' => 'Derniers messages publi&eacute;s dans les forums',
-
+'dernier_ajout' => 'Dernier ajout',
 
 // E
 'edition_mode_texte' => '&Eacute;dition en mode texte de',
+'envoyer_message' => 'Envoyer un message',
 'en_reponse' => 'En r&eacute;ponse &agrave;&nbsp;:',
 'en_resume' => 'En r&eacute;sum&eacute;',
-'envoyer_message' => 'Envoyer un message',
 'espace_prive' => 'Espace priv&eacute;',
-
 
 // F
 'forum' => 'Forum',
 
-
 // H
 'hierarchie_site' => 'Hi&eacute;rarchie du site',
-
 
 // J
 'jours' => 'jours',
 
+// L
+'lien_connecter' => 'Se connecter',
 
 // M
+'memes_auteurs' => 'Des m&ecirc;mes auteurs',
 'meme_auteur' => 'Du m&ecirc;me auteur',
 'meme_rubrique' => 'Dans la m&ecirc;me rubrique',
-'memes_auteurs' => 'Des m&ecirc;mes auteurs',
 'message' => 'Message',
 'messages_forum' => 'Messages de forum',
 'messages_recents' => 'Messages de forums les plus r&eacute;cents',
 'mots_clefs' => 'Mots-cl&eacute;s',
 'mots_clefs_meme_groupe' => 'Mots-cl&eacute;s dans le m&ecirc;me groupe',
-
 
 // N
 'navigation' => 'Navigation',
@@ -80,17 +74,15 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'nouveaux_articles' => 'Nouveaux articles',
 'nouvelles_breves' => 'Nouvelles br&egrave;ves',
 
-
 // P
 'page_precedente' => 'page pr&eacute;c&eacute;dente',
 'page_suivante' => 'page suivante',
-'par_auteur' => 'par ',
 'participer_site' => 'Vous pouvez participer &agrave; la vie de ce site et proposer vos propres articles en vous inscrivant ci-dessous. Vous recevrez imm&eacute;diatement un email vous indiquant vos codes d\'acc&egrave;s &agrave; l\'espace priv&eacute; du site.',
+'par_auteur' => 'par ',
 'plan_site' => 'Plan du site',
 'popularite' => 'Popularit&eacute;',
 'poster_message' => 'Poster un message',
 'proposer_site' => 'Vous pouvez proposer un site &agrave; ajouter dans cette rubrique&nbsp;:',
-
 
 // R
 'repondre_article' => 'R&eacute;pondre &agrave; cet article',
@@ -101,11 +93,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'rubrique' => 'Rubrique',
 'rubriques' => 'Rubriques',
 
-
 // S
 'signatures_petition' => 'Signatures',
-'site_realise_avec_spip' => 'Site r&eacute;alis&eacute; avec SPIP',
 'sites_web' => 'Sites Web',
+'site_realise_avec_spip' => 'Site r&eacute;alis&eacute; avec SPIP',
 'sous_rubriques' => 'Sous-rubriques',
 'spam' => 'Spam',
 'suite' => 'suite',
@@ -113,18 +104,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'syndiquer_rubrique' => 'Syndiquer cette rubrique',
 'syndiquer_site' => 'Syndiquer tout le site',
 
-
 // T
 'texte_lettre_information' => 'Voici la lettre d\'information du site',
 'texte_lettre_information_2' => 'Cette lettre recense les articles et les br&egrave;ves publi&eacute;s depuis',
-
 
 // V
 'ver_imprimer' => 'Version &agrave; imprimer',
 'voir_en_ligne' => 'Voir en ligne',
 'voir_squelette' => 'voir le squelette de cette page'
-
 );
-
 
 ?>

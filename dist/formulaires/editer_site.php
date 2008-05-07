9ddf3161782cd35cc71ce20fdc8b4d331067bb17
cedric <cedric@yterium.com>
2008-05-07 15:45:41 +0000

    "evitons la multiplication de petits fichiers et regroupons charger/verifier/traiter dans un seul fichier grace a [11582]"
    git-svn-id: svn://trac.rezo.net/spip/spip@11586 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- dist/formulaires/editer_site.php
+++ dist/formulaires/editer_site.php
@@ -15,10 +15,32 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 include_spip('inc/actions');
 include_spip('inc/editer');
 
+function formulaires_editer_site_charger_dist($id_syndic='new', $id_rubrique=0, $lier_trad=0, $retour='', $config_fonc='sites_edit_config', $row=array(), $hidden=''){
+	$valeurs = formulaires_editer_objet_charger('site',$id_syndic,$id_rubrique,$lier_trad,$retour,$config_fonc,$row,$hidden);
+	return $valeurs;
+}
+
+// Choix par defaut des options de presentation
+function sites_edit_config($row)
+{
+	global $spip_ecran, $spip_lang, $spip_display;
+
+	$config = $GLOBALS['meta'];
+	$config['lignes'] = ($spip_ecran == "large")? 8 : 5;
+	$config['afficher_barre'] = $spip_display != 4;
+	$config['langue'] = $spip_lang;
+
+	$config['restreint'] = false;
+	return $config;
+}
+
 function formulaires_editer_site_verifier_dist($id_syndic='new', $id_rubrique=0, $lier_trad=0, $retour='', $config_fonc='sites_edit_config', $row=array(), $hidden=''){
 
 	$erreurs = formulaires_editer_objet_verifier('site',$id_syndic,_request('url_auto')?array():array('nom_site','url_site'));
 	return $erreurs;
 }
 
+function formulaires_editer_site_traiter_dist($id_syndic='new', $id_rubrique=0, $lier_trad=0, $retour='', $config_fonc='sites_edit_config', $row=array(), $hidden=''){
+	return formulaires_editer_objet_traiter('site',$id_syndic,$id_rubrique,$lier_trad,$retour,$config_fonc,$row,$hidden);
+}
 ?>

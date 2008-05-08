855f2ba63021cf75366f50ef0775ca29c4da2636
cedric <cedric@yterium.com>
2008-05-08 12:13:40 +0000

    "eviter les ambiguite de class posix dans les expressions regulieres car a partir de php 5.2.6, pcre admet tout caractere comme non de classe entre [: et :] [:[:space:]] etait interprete comme classe '[:space' (inexistante), crochet et non tout caractere parmi :, :space: (neofutur)"
    git-svn-id: svn://trac.rezo.net/spip/spip@11595 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/syndic.php
+++ ecrire/inc/syndic.php
@@ -44,7 +44,7 @@ function analyser_backend($rss, $url_syndic='') {
 	$rss = preg_replace(',<(/?)(dc):,i', '<\1', $rss);
 
 	// chercher auteur/lang dans le fil au cas ou les items n'en auraient pas
-	list($header) = preg_split(',<(item|entry)[:[:space:]>],', $rss, 2);
+	list($header) = preg_split(',<(item|entry)[\:[:space:]>],', $rss, 2);
 	if (preg_match_all(
 	',<(author|creator)>(.*)</\1>,Uims',
 	$header, $regs, PREG_SET_ORDER)) {
@@ -171,11 +171,11 @@ function analyser_backend($rss, $url_syndic='') {
 			$data['lesauteurs'] = $les_auteurs_du_site;
 
 		// Description
-		if (preg_match(',<((description|summary)([:[:space:]][^>]*)?)'
+		if (preg_match(',<((description|summary)([\:[:space:]][^>]*)?)'
 		.'>(.*)</\2[:>[:space:]],Uims',$item,$match)) {
 			$data['descriptif'] = trim($match[4]);
 		}
-		if (preg_match(',<((content)([:[:space:]][^>]*)?)'
+		if (preg_match(',<((content)([\:[:space:]][^>]*)?)'
 		.'>(.*)</\2[:>[:space:]],Uims',$item,$match)) {
 			$data['content'] = trim($match[4]);
 		}

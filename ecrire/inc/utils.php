caca626ea4b2465c1ac7f747dd9e9fac7d76faa1
cedric <cedric@yterium.com>
2008-07-02 20:39:29 +0000

    dist/ ne doit contenir que des squelettes par defaut du public et etre supprimable on deplace dans prive/ tout ce qui est indispensable a l'espace prive prive/ est ajoute au path juste apres dist/
    git-svn-id: svn://trac.rezo.net/spip/spip@11932 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/utils.php
+++ ecrire/inc/utils.php
@@ -569,6 +569,7 @@ function _chemin($dir_path=NULL){
 		$path = defined('_SPIP_PATH') ? _SPIP_PATH :
 			_DIR_RACINE.':'.
 			_DIR_RACINE.'dist/:'.
+			_DIR_RACINE.'prive/:'.
 			_DIR_RESTREINT.':';
 		// Ajouter squelettes/
 		if (@is_dir(_DIR_RACINE.'squelettes'))

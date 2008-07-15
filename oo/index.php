fcc06188069cefb4591e21ec3fdb8c340767a07f
cedric <cedric@yterium.com>
2008-07-15 18:46:00 +0000

    #1102 dit 'supprimer oo/ et sommaire_texte' qui sont carrement obsoletes, voire gentiment desuets
    git-svn-id: svn://trac.rezo.net/spip/spip@12075 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- oo/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// ACCESSIBILITE
-// la page /oo offre une lecture en mode "texte seul"
-// des 3 articles les plus recents...
-
-@header("Location: ../spip.php?page=sommaire_texte");
-
-?>

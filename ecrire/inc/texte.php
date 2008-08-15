e03825fbd14c671ccc0e1afd579dc22541cb5aad
fil <fil@rezo.net>
2008-08-15 08:15:41 +0000

    report de [12323] et [12324] sur les liens auto
    git-svn-id: svn://trac.rezo.net/spip/spip@12325 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/texte.php
+++ ecrire/inc/texte.php
@@ -1249,7 +1249,7 @@ function autoliens_callback($r) {
 		if (preg_match(
 		'/^(?:[^\W_]((?:[^\W_]|-){0,61}[^\W_,])?\.)+[a-z]{2,6}\b/Si', $l)) {
 			// supprimer les ponctuations a la fin d'une URL
-			preg_match('/^(.*?)([,.;?])$/', $l, $k);
+			preg_match('/^(.*?)([,.;?]?)$/', $l, $k);
 			$l = inserer_attribut(
 				expanser_liens('[->'.$protocol.'://'.$k[1].']'),
 			'rel', 'nofollow')
@@ -1267,7 +1267,8 @@ function autoliens_callback($r) {
 // http://doc.spip.org/@traiter_raccourci_liens
 function traiter_raccourci_liens($texte) {
 	return preg_replace_callback(
-	';\[[^\[\]]*->.*?\]|<a\b.*?</a>|((?:https?:/|www\.)[^"\'\s\[\]\}\)<>]*);Si',
+	';\[[^\[\]]*(?:<-|->).*?\]|<a\b.*?</a>|<.*?>|'
+	.'((?:https?:/|www\.)[^"\'\s\[\]\}\)<>]*);Si',
 	'autoliens_callback', $texte);
 	return $texte;
 }

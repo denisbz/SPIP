adceaf76a77f7f75980e8b69a49bd76c8c25b570
arno <arno@rezo.net>
2008-08-23 13:44:00 +0000

    CSS avec adresses absolues
    git-svn-id: svn://trac.rezo.net/spip/spip@12368 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/utils.php
+++ ecrire/inc/utils.php
@@ -1039,7 +1039,7 @@ function spip_initialisation($pi=NULL, $pa=NULL, $ti=NULL, $ta=NULL) {
 
 	static $too_late = 0;
 	if ($too_late++) return;
-
+	
 	define('_DIR_IMG', $pa);
 	define('_DIR_LOGOS', $pa);
 	define('_DIR_IMG_ICONES', _DIR_LOGOS . "icones/");

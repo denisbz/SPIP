5837aae03fcd3a4b8d5cab2b3b8a510c6606ac8d
fil <fil@rezo.net>
2008-08-26 07:51:15 +0000

    deux mises a jour de la base
    git-svn-id: svn://trac.rezo.net/spip/spip@12410 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc_version.php
+++ ecrire/inc_version.php
@@ -328,7 +328,7 @@ $spip_version_branche = "2.0.0 dev";
 // (= numero SVN de leur derniere modif cassant la compatibilite et/ou necessitant un recalcul des squelettes)
 $spip_version_code = 12282;
 // version de la base SQL (= numero SVN de sa derniere modif)
-$spip_version_base = 12008;
+$spip_version_base = 12010;
 
 // version de l'interface a la base
 $spip_sql_version = 1;

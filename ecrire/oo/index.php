bc1eb7a5aceb796dcf5a897ee603c8df5d06f37e
fil <fil@rezo.net>
2008-09-06 11:34:07 +0000

    eviter toute injection de redirection
    git-svn-id: svn://trac.rezo.net/spip/spip@12547 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/oo/index.php
+++ ecrire/oo/index.php
@@ -3,7 +3,6 @@
 // ACCESSIBILITE
 // la page /oo offre une lecture en mode "texte seul"
 
-header("Location: ../?action=preferer&arg=display:4&redirect=" . 
-       urlencode("./?exec=" . @$_REQUEST['exec']));
+header("Location: ../?action=preferer&arg=display:4");
 
 ?>

0c3777f3c8e516328026cb99fbfa503e92018ea7
cedric <cedric@yterium.com>
2008-09-12 13:32:25 +0000

    ne pas melanger les contextes qui n'ont rien a voir, mais passer le contexte du squelette dans un ['contexte']
    git-svn-id: svn://trac.rezo.net/spip/spip@12612 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/public/assembler.php
+++ ecrire/public/assembler.php
@@ -241,13 +241,13 @@ function inclure_balise_dynamique($texte, $echo=true, $ligne=0) {
 		// on se refere a $page['contexte'] a la place
 		if (isset($page['contexte']['_pipeline'])) {
 			$pipe = is_array($page['contexte']['_pipeline'])?reset($page['contexte']['_pipeline']):$page['contexte']['_pipeline'];
-			$contexte = is_array($page['contexte']['_pipeline'])?end($page['contexte']['_pipeline']):array();
-			$contexte = array_merge($page['contexte'], $contexte);
-			unset($contexte['_pipeline']); // par precaution, meme si le risque de boucle infinie est a priori nul
+			$args = is_array($page['contexte']['_pipeline'])?end($page['contexte']['_pipeline']):array();
+			$args['contexte'] = $page['contexte'];
+			unset($args['contexte']['_pipeline']); // par precaution, meme si le risque de boucle infinie est a priori nul
 			if (isset($GLOBALS['spip_pipeline'][$pipe]))
 				$texte = pipeline($pipe,array(
 				  'data'=>$texte,
-				  'args'=>$contexte));
+				  'args'=>$args));
 		}
 	}
 

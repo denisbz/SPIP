325b9bcc2a5c33ead83b492b20065d81b3b79d92
esj <esj@rezo.net>
2008-09-18 21:32:06 +0000

    Pour garder sa généralité à la mécanique des puces de changements de statuts, ne pas préjuger du caractère numérique du paramètre $id dans les cas non répertoriés.  Et ne pas laisser trainer du code mort, ça aide pas à la mise à jour.
    git-svn-id: svn://trac.rezo.net/spip/branches/spip-2.0@12728 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/exec/puce_statut.php
+++ ecrire/exec/puce_statut.php
@@ -17,18 +17,20 @@ include_spip('inc/presentation');
 // http://doc.spip.org/@exec_puce_statut_dist
 function exec_puce_statut_dist()
 {
-	$id = intval(_request('id'));
-	$type = _request('type');
+	exec_puce_statut_args(_request('id'),  _request('type'));
+}
 
+function exec_puce_statut_args($id, $type)
+{
 	if (in_array($type,array('article','breve','site'))) {
 		$table = table_objet_sql($type);
 		$prim = id_table_objet($type);
+		$id = intval($id);
 		$r = sql_fetsel("id_rubrique,statut", "$table", "$prim=$id");
 		$statut = $r['statut'];
 		$id_rubrique = $r['id_rubrique'];
 	} else {
-		$id_rubrique = $id;
-		$id = 0;
+		$id_rubrique = intval($id);
 		$statut = 'prop'; // arbitraire
 	}
 	$puce_statut = charger_fonction('puce_statut', 'inc');

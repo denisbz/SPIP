729d3c33179144c04f96b91ccb697e955d1f89e0
cedric <cedric@yterium.com>
2008-09-18 12:36:34 +0000

    s'en tenir a un nombre de decimales fixes pour ne pas tromper version_compare
    git-svn-id: svn://trac.rezo.net/spip/branches/spip-2.0@12723 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc_version.php
+++ ecrire/inc_version.php
@@ -324,7 +324,8 @@ $liste_des_authentifications = array(
 
 // numero de branche, utilise par les plugins 
 // pour specifier les versions de SPIP necessaire
-$spip_version_branche = "2.0 beta2";
+// il faut s'en tenir a un nombre de decimales fixe ex : 2.0.0, 2.0.0-dev, 2.0.0-beta, 2.0.0-beta2 ...
+$spip_version_branche = "2.0.0 beta2";
 // version des signatures de fonctions PHP
 // (= numero SVN de leur derniere modif cassant la compatibilite et/ou necessitant un recalcul des squelettes)
 $spip_version_code = 12691;

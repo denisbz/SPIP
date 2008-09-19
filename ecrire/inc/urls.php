17078284e877fb4d9fe66d61bc407888da5d0102
esj <esj@rezo.net>
2008-09-19 06:31:00 +0000

    Appliquer la solution [12716] à [12689].
    git-svn-id: svn://trac.rezo.net/spip/spip@12730 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/urls.php
+++ ecrire/inc/urls.php
@@ -16,82 +16,89 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 // des items des tables SQL principales, selon le statut de publication
 
 // http://doc.spip.org/@generer_url_ecrire_article
-function generer_url_ecrire_article($id, $suite='', $ancre='', $statut='', $connect='') {
+function generer_url_ecrire_article($id, $args='', $ancre='', $statut='', $connect='') {
 	$a = "id_article=" . intval($id);
 	if (!$statut) {
 		$statut = sql_getfetsel('statut', 'spip_articles', $a,'','','','',$connect);
 	}
-	if ($suite) $a .= "&$suite";
-	if ($statut == 'publie') {
-		return url_absolue(suivre_lien(_DIR_RACINE, generer_url_entite($id, 'article', $suite, $ancre, true)));
-	} else	return generer_url_ecrire('articles', $a) . ($ancre ? "#$ancre" : '');
+	$h = ($statut == 'publie' OR $connect)
+	? generer_url_entite_absolue($id, 'article', $args, $ancre, $connect)
+	: (generer_url_ecrire('articles', $a . ($args ? "&$args" : ''))
+		. ($ancre ? "#$ancre" : ''));
+	return $h;
 }
 
 // http://doc.spip.org/@generer_url_ecrire_rubrique
-function generer_url_ecrire_rubrique($id, $suite='', $ancre='', $statut='', $connect='') {
+function generer_url_ecrire_rubrique($id, $args='', $ancre='', $statut='', $connect='') {
 	$a = "id_rubrique=" . intval($id);
 	if (!$statut) {
 		$statut = sql_getfetsel('statut', 'spip_rubriques', $a,'','','','',$connect);
 	}
-	if ($suite) $a .= "&$suite";
-	if ($statut == 'publie') {
-		return url_absolue(suivre_lien(_DIR_RACINE, generer_url_entite($id, 'rubrique', $suite, $ancre, true)));
-	} else	return generer_url_ecrire('naviguer',$a) . ($ancre ? "#$ancre" : '');
+	$h = ($statut == 'publie' OR $connect)
+	? generer_url_entite_absolue($id, 'rubrique', $args, $ancre, $connect)
+	: (generer_url_ecrire('naviguer',$a . ($args ? "&$args" : ''))
+		. ($ancre ? "#$ancre" : ''));
+	return $h;
 }
 
 // http://doc.spip.org/@generer_url_ecrire_breve
-function generer_url_ecrire_breve($id, $suite='', $ancre='', $statut='', $connect='') {
+function generer_url_ecrire_breve($id, $args='', $ancre='', $statut='', $connect='') {
 	$a = "id_breve=" . intval($id);
 	if (!$statut) {
 		$statut = sql_getfetsel('statut', 'spip_breves', $a,'','','','',$connect);
 	}
-	if ($suite) $a .= "&$suite";
-	if ($statut == 'publie') {
-		return url_absolue(suivre_lien(_DIR_RACINE, generer_url_entite($id, 'breve', $suite, $ancre, true)));
-	} else	return generer_url_ecrire('breves_voir',$a) . ($ancre ? "#$ancre" : '');
+	$h = ($statut == 'publie' OR $connect)
+	?  generer_url_entite_absolue($id, 'breve', $args, $ancre, $connect)
+	: (generer_url_ecrire('breves_voir',$a . ($args ? "&$args" : ''))
+		. ($ancre ? "#$ancre" : ''));
+	return $h;
 }
 
 // http://doc.spip.org/@generer_url_ecrire_mot
-function generer_url_ecrire_mot($id, $suite='', $ancre='', $statut='', $connect='') {
+function generer_url_ecrire_mot($id, $args='', $ancre='', $statut='', $connect='') {
 	$a = "id_mot=" . intval($id);
-	if ($suite) $a .= "&$suite";
-	if (!$statut) {
-		return url_absolue(suivre_lien(_DIR_RACINE, generer_url_entite($id, 'mot', $suite, $ancre, true)));
-	} else	return generer_url_ecrire('mots_edit',$a) . ($ancre ? "#$ancre" : '');
+	$h = (!$statut OR $connect)
+	?  generer_url_entite_absolue($id, 'mot', $args, $ancre, $connect)
+	: (generer_url_ecrire('mots_edit',$a . ($args ? "&$args" : ''))
+		. ($ancre ? "#$ancre" : ''));
+	return $h;
 }
 
 // http://doc.spip.org/@generer_url_ecrire_site
-function generer_url_ecrire_site($id, $suite='', $ancre='', $statut='', $connect='') {
+function generer_url_ecrire_site($id, $args='', $ancre='', $statut='', $connect='') {
 	$a = "id_syndic=" . intval($id);
-	if ($suite) $a .= "&$suite";
-	if (!$statut) {
-		return url_absolue(suivre_lien(_DIR_RACINE, generer_url_entite($id, 'site', $suite, $ancre, true)));
-	} else	return generer_url_ecrire('sites',$a) . ($ancre ? "#$ancre" : '');
+	$h = (!$statut OR $connect)
+	?  generer_url_entite_absolue($id, 'site', $args, $ancre, $connect)
+	: (generer_url_ecrire('sites',$a . ($args ? "&$args" : ''))
+		. ($ancre ? "#$ancre" : ''));
+	return $h;
 }
 
 // http://doc.spip.org/@generer_url_ecrire_auteur
-function generer_url_ecrire_auteur($id, $suite='', $ancre='', $statut='', $connect='') {
+function generer_url_ecrire_auteur($id, $args='', $ancre='', $statut='', $connect='') {
 	$a = "id_auteur=" . intval($id);
-	if ($suite) $a .= "&$suite";
-	if (!$statut) {
-		return url_absolue(suivre_lien(_DIR_RACINE, generer_url_entite($id, 'auteur', $suite, $ancre, true)));
-	} else	return generer_url_ecrire('auteur_infos',$a) . ($ancre ? "#$ancre" : '');
+	$h = (!$statut OR $connect)
+	?  generer_url_entite_absolue($id, 'auteur', $args, $ancre, $connect)
+	: (generer_url_ecrire('auteur_infos',$a . ($args ? "&$args" : ''))
+		. ($ancre ? "#$ancre" : ''));
+	return $h;
 }
 
 // http://doc.spip.org/@generer_url_ecrire_forum
-function generer_url_ecrire_forum($id, $suite='', $ancre='', $statut='', $connect='') {
+function generer_url_ecrire_forum($id, $args='', $ancre='', $statut='', $connect='') {
 	$a = "id_forum=" . intval($id);
 	if (!$statut) {
 		$statut = sql_getfetsel('statut', 'spip_forum', $a,'','','','',$connect);
 	}
-	if ($suite) $a .= "&$suite";
-	if ($statut == 'publie') {
-		return url_absolue(suivre_lien(_DIR_RACINE, generer_url_entite($id, 'forum', $suite, $ancre, true)));
-	}  else return generer_url_ecrire('controle_forum', "debut_id_forum=$id");
+	$h = ($statut == 'publie' OR $connect)
+	?  generer_url_entite_absolue($id, 'forum', $args, $ancre, $connect)
+	: (generer_url_ecrire('controle_forum', "debut_id_forum=$id" . ($args ? "&$args" : ''))
+		. ($ancre ? "#$ancre" : ''));
+	return $h;
 }
 
 // http://doc.spip.org/@generer_url_ecrire_document
-function generer_url_ecrire_document($id, $suite='', $ancre='', $statut='', $connect='') {
+function generer_url_ecrire_document($id, $args='', $ancre='', $statut='', $connect='') {
 	include_spip('inc/documents');
 	return generer_url_document_dist($id);
 }

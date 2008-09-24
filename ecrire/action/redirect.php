7a17710ee5d0c7de719c76d6b51541ecd5f14102
esj <esj@rezo.net>
2008-09-24 15:25:30 +0000

    Retour, en plus simple, de l'action Redirect, afin que l'on puisse prendre en compte le chgt de titre donc d'URL symbolique quand on clique sur le bouton 'Voir en ligne'.
    git-svn-id: svn://trac.rezo.net/spip/spip@12774 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

---
+++ ecrire/action/redirect.php
@@ -0,0 +1,29 @@
+<?php
+
+/***************************************************************************\
+ *  SPIP, Systeme de publication pour l'internet                           *
+ *                                                                         *
+ *  Copyright (c) 2001-2008                                                *
+ *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
+ *                                                                         *
+ *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
+ *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
+\***************************************************************************/
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+// Un script utile pour recalculer une URLs symbolique des son changement
+
+function action_redirect_dist()
+{
+	$type = _request('type');
+	if (!preg_match('/^\w+$/', $type)) return;
+	$h = generer_url_entite_absolue(intval(_request('id')),
+					$type,
+					"var_mode=" . _request('var_mode'),
+					'',
+					true);
+	redirige_par_entete(str_replace('&amp;', '&', $h));
+}
+
+?>

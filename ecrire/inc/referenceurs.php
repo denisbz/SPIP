9805740f7a824b49f8779c9cbf01a03a9903a7a4
romy <romy@rezo.net>
2008-09-30 07:30:51 +0000

    statistiques_visites : trop de gras nuisait a la lisibilite (4...)
    git-svn-id: svn://trac.rezo.net/spip/spip@12831 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/referenceurs.php
+++ ecrire/inc/referenceurs.php
@@ -75,9 +75,9 @@ function inc_referenceurs_dist ($script, $args, $select, $table, $where, $groupb
 				$ret .= "\n<a href=\"http://".$lesurls[$numero]."\"><img src=\"$source_vignettes".rawurlencode($lesurls[$numero])."\"\nstyle=\"float: $spip_lang_right; margin-bottom: 3px; margin-left: 3px;\" alt='' /></a>";
 
 			$bouton = "";
-			if ($visites > 5) $bouton .= "<span class='visites'>$visites "._T('info_visites')."</span> ";
-			else if ($visites > 1) $bouton .= "$visites "._T('info_visites')." ";
-			else $bouton .= "<span style='color: #999999'>$visites "._T('info_visite')."</span> ";
+			if ($visites > 5) $bouton .= "<span class='visites visites3'>$visites "._T('info_visites')."</span> ";
+			else if ($visites > 1) $bouton .= "<span class='visites visites2'>$visites "._T('info_visites')."</span> ";
+			else $bouton .= "<span class='visites visites1'>$visites "._T('info_visite')."</span> ";
 
 			if ($dom == "(email)") {
 				$aff .= $ret . $bouton . "<b>".$dom."</b>";

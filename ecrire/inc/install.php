8b5c95033e07c288d6dcb9fca5c778c6c910ff11
romy <romy@rezo.net>
2008-10-06 16:16:50 +0000

    Amélioration de la frise indiquant les etapes d'installation de SPIP : une seule couleur suffit, pour signaler l'etape en cours (sans les precedentes), indépendamment de son etat + amélioration du contraste pour meilleure lisibilité. Cf.: message http://article.gmane.org/gmane.comp.web.spip.devel/50140 et suivants.
    git-svn-id: svn://trac.rezo.net/spip/spip@12882 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/install.php
+++ ecrire/inc/install.php
@@ -196,7 +196,7 @@ function info_progression_etape($en_cours,$phase,$dir, $erreur = false){
 */
 		if ($debut < $last) {
 			if ($debut == $en_cours && $erreur) $class = "erreur";
-			else if ($debut == $en_cours) $class = "encours";
+			else if ($debut == $en_cours) $class = "on";
 			else if ($debut > $en_cours) $class = "prochains";
 			else $class = "valides";
 

3ae58dca1fa585158aee40ddf84ec163b98d5de2
cedric <cedric@yterium.com>
2008-10-07 17:15:59 +0000

    report de [12895]
    git-svn-id: svn://trac.rezo.net/spip/spip@12896 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc_version.php
+++ ecrire/inc_version.php
@@ -375,7 +375,7 @@ error_reporting(SPIP_ERREUR_REPORT);
 // ou a defini certaines des constantes que cette fonction doit definir
 // ===> on execute en neutralisant les messages d'erreur
 
-@spip_core_initialisation(
+@spip_initialisation_core(
 	(_DIR_RACINE  . _NOM_PERMANENTS_INACCESSIBLES),
 	(_DIR_RACINE  . _NOM_PERMANENTS_ACCESSIBLES),
 	(_DIR_RACINE  . _NOM_TEMPORAIRES_INACCESSIBLES),
@@ -392,7 +392,7 @@ if (@is_readable(_DIR_TMP."charger_plugins_options.php")){
 	// chargement optimise precompile
 	include_once(_DIR_TMP."charger_plugins_options.php");
 } else {
-	@spip_initialisation();
+	@spip_initialisation_suite();
 	include_spip('inc/plugin');
 	// generer les fichiers php precompiles
 	// de chargement des plugins et des pipelines
@@ -404,7 +404,7 @@ if (@is_readable(_DIR_TMP."charger_plugins_options.php")){
 	}
 }
 // Initialisations non critiques surchargeables par les plugins
-@spip_initialisation();
+@spip_initialisation_suite();
 
 if (!defined('_OUTILS_DEVELOPPEURS'))
 	define('_OUTILS_DEVELOPPEURS',false);

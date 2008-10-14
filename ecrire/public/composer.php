74274a4fe7d315896ba30ed6085c3c0026e8b7ed
cedric <cedric@yterium.com>
2008-10-14 06:41:48 +0000

    "[12935] a envoye par erreur un debut de datage systematique des url documents et logo. On y reviendra, mais pas en stable"
    git-svn-id: svn://trac.rezo.net/spip/branches/spip-2.0@12940 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/public/composer.php
+++ ecrire/public/composer.php
@@ -237,7 +237,7 @@ function calcule_logo($type, $onoff, $id, $id_rubrique, $flag_fichier) {
 				// on retourne une url du type IMG/artonXX?timestamp
 				// qui permet de distinguer le changement de logo
 				// et placer un expire sur le dossier IMG/
-				return array ($on[0].($on[4]?"?$on[4]":"") , ($off ? $off[0] . ($off[4]?"?$off[4]":"") : ''));
+				return array ($on[0] , ($off ? $off[0] : ''));
 			}
 		}
 		else if ($id_rubrique) {

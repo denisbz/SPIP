413cb6d8911624fa8f2deafb84f5348baf063cfa
cedric <cedric@yterium.com>
2008-10-16 20:17:02 +0000

    "puisque les couleurs ne sont pas visibles par tous, essayons d'etre plus explicites pour ceux qui naviguent sans vision"
    git-svn-id: svn://trac.rezo.net/spip/branches/spip-2.0@12973 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/install.php
+++ ecrire/inc/install.php
@@ -182,7 +182,7 @@ function info_progression_etape($en_cours,$phase,$dir, $erreur = false){
 
 //	$aff_etapes = "<span id='etapes'>";
 
-	$aff_etapes = "<ol id='infos_etapes'>";
+	$aff_etapes = "<ul id='infos_etapes'>";
 
 	foreach($liste as $etape=>$fichier){
 /*		if ($etape=="$phase$en_cours.php"){
@@ -195,18 +195,21 @@ function info_progression_etape($en_cours,$phase,$dir, $erreur = false){
 			$etat = 'todo';
 */
 		if ($debut < $last) {
-			if ($debut == $en_cours && $erreur) $class = "on erreur";
+			if ($debut == $en_cours && $erreur) $class = "erreur";
 			else if ($debut == $en_cours) $class = "on";
 			else if ($debut > $en_cours) $class = "prochains";
 			else $class = "valides";
 
 			$aff_etapes .= "<li class='$class'><div class='fond'>";
-			$aff_etapes .= "<span class='numero_etape'>$debut</span>".$intitule_etat["$phase"][$debut];
+			$aff_etapes .= ($debut == $en_cours)?"<strong>":'';
+			$aff_etapes .= "<em>"._T('etape')." </em><span class='numero_etape'>$debut</span><em>&nbsp;: </em>";
+			$aff_etapes .= $intitule_etat["$phase"][$debut];
+			$aff_etapes .= ($debut == $en_cours)?"</strong>":'';
 			$aff_etapes .= "</div></li>";
 		}
 		$debut++;
 	}
-	$aff_etapes .= "</ol>";
+	$aff_etapes .= "</ul>";
 	$aff_etapes .= "<br class='nettoyeur' /\n";
 	return $aff_etapes;
 }
@@ -325,7 +328,7 @@ function install_connexion_form($db, $login, $pass, $predef, $hidden, $etape)
 			// Passer l'avertissement SQLIte en  commentaire, on pourra facilement le supprimer par la suite sans changer les traductions.
 			. "<br /><small>(". _T('install_types_db_connus_avertissement') .')</small>'
 			.'</label>'		
-		. "\n<div><select name='server_db' id='sql_serveur_db' >"
+		. "\n<div style='text-align: center;'><select name='server_db' id='sql_serveur_db' >"
 		. ($mysql
 			? "\n<option value='mysql'>"._T('install_select_type_mysql')."</option>"
 			: '')

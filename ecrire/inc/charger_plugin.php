405657a66d495c134fa82a198b15a0c3dd36701d
marcimat <marcimat@rezo.net>
2008-10-18 21:55:18 +0000

    Report de [12989]
    git-svn-id: svn://trac.rezo.net/spip/spip@12991 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/charger_plugin.php
+++ ecrire/inc/charger_plugin.php
@@ -507,7 +507,8 @@ function bouton_telechargement_plugin($url, $rep) {
 			'',
 			'',
 			"<input type='hidden' name='url_zip_plugin' value='$url' />"
-			."<input type='submit' name='ok' value='"._T('bouton_download')."' />");
+			."<input type='submit' name='ok' value='"._T('bouton_download')."' />",
+			'class="noajax"');
 
 	return _T('plugin_info_telecharger',array('url'=>$url,'rep'=>$rep.'/')).$bouton;
 

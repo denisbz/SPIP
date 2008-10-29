6fe7ba0f0d22d82561c755696b29c7ceb83156b6
cedric <cedric@yterium.com>
2008-10-29 21:50:46 +0000

    enlever id_rubrique des valeurs saisies pour ne pas la perdre dans l'url, ce qui provoquait la perte des erreurs et saisie au premier submit (realet)
    git-svn-id: svn://trac.rezo.net/spip/spip@13067 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- prive/formulaires/editer_article.php
+++ prive/formulaires/editer_article.php
@@ -17,7 +17,12 @@ include_spip('inc/editer');
 
 // http://doc.spip.org/@inc_editer_article_dist
 function formulaires_editer_article_charger_dist($id_article='new', $id_rubrique=0, $retour='', $lier_trad=0, $config_fonc='articles_edit_config', $row=array(), $hidden=''){
-	return formulaires_editer_objet_charger('article',$id_article,$id_rubrique,$lier_trad,$retour,$config_fonc,$row,$hidden);
+	$valeurs = formulaires_editer_objet_charger('article',$id_article,$id_rubrique,$lier_trad,$retour,$config_fonc,$row,$hidden);
+	// il faut enlever l'id_rubrique car la saisie se fait sur id_parent
+	// et id_rubrique peut etre passe dans l'url comme rubrique parent initiale
+	// et sera perdue si elle est supposee saisie
+	unset($valeurs['id_rubrique']);
+	return $valeurs;
 }
 
 // Choix par defaut des options de presentation

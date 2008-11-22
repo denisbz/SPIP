87fc15d88cba6f71322624990426c1bc4135aead
esj <esj@rezo.net>
2008-11-22 18:08:05 +0000

    Le sélectionneur Ajax des auteurs ne faisait pas suivre le paramètre Type, et du coup ne donnait pas la même liste d'auteurs sélectionnable que la version avec balise Select.
    git-svn-id: svn://trac.rezo.net/spip/spip@13247 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/selectionner_auteur.php
+++ ecrire/inc/selectionner_auteur.php
@@ -20,15 +20,15 @@ include_spip('inc/selectionner');
 //
 
 // http://doc.spip.org/@inc_selectionner_auteur_dist
-function inc_selectionner_auteur_dist($id_article)
+function inc_selectionner_auteur_dist($id_article, $type='article')
 {
 	static $n = 0;
 	$idom = "selectionner_auteur_$n";
 
-    if (!$determiner_non_auteurs = charger_fonction('determiner_non_auteurs_article','inc',true))
-        $determiner_non_auteurs = 'determiner_non_auteurs';
+	if (!$determiner_non_auteurs = charger_fonction('determiner_non_auteurs_'.$type,'inc',true))
+		$determiner_non_auteurs = 'determiner_non_auteurs';
 
-	$futurs = selectionner_auteur_boucle($determiner_non_auteurs('article',$id_article), $idom);
+	$futurs = selectionner_auteur_boucle($determiner_non_auteurs($type, $id_article), $idom);
 
 	// url completee par la fonction JS onkeypress_rechercher
 	$url = generer_url_ecrire('rechercher_auteur', "idom=$idom&nom=");

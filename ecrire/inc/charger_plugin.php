cf51abf28861681f0a23b74237f52fac346760a7
cedric <cedric@yterium.com>
2008-12-03 11:55:54 +0000

    report de [12989] avec une difference de chaine de langue dont je ne retrouve ni l'origine ni la justification
    git-svn-id: svn://trac.rezo.net/spip/spip@13344 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/charger_plugin.php
+++ ecrire/inc/charger_plugin.php
@@ -518,7 +518,7 @@ function bouton_telechargement_plugin($url, $rep) {
 			'',
 			'',
 			"<input type='hidden' name='url_zip_plugin' value='$url' />"
-			."<input type='submit' name='ok' value='"._T('bouton_download')."' />",
+			."<input type='submit' name='ok' value='"._T('bouton_telecharger')."' />",
 			'class="noajax"');
 
 	return _T('plugin_info_telecharger',array('url'=>$url,'rep'=>$rep.'/')).$bouton;

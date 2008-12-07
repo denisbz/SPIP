242165d9638bc8a112ef62e7adb052b49b9f5f31
cedric <cedric@yterium.com>
2008-12-07 19:24:15 +0000

    Sur certains hebergeurs, le test de detection de taille maxi de manipulation d'image en GD2 provoque un plantage SQL Il faudrait le reecrire pour eviter cela, mais en attendant on permet de le desactiver en declarant soi meme la taille maxi permise pas GD2 : define('_IMG_GD_MAX_PIXELS',5000000); cette definition (manuelle ou par calcul auto) est obligatoire depuis que l'espace prive utilise image_reduire() sur toutes les pages article dans l'espace prive
    git-svn-id: svn://trac.rezo.net/spip/branches/spip-2.0@13411 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/configuration/reducteur.php
+++ ecrire/configuration/reducteur.php
@@ -74,47 +74,49 @@ function configuration_reducteur_dist()
 		
 	$test_out = "";
 	if ($GLOBALS['meta']['image_process']=='gd1' OR $GLOBALS['meta']['image_process']=='gd2') {
-	  $test_out .= "<p>"._T('info_taille_maximale_images')."</p>";
-		
-		$test_out .= "<div dir='ltr' id='teste_memory_size_gd' style='text-align:left;float:$spip_lang_right;width:196px;background:url(". chemin_image('jauge-test-gd.gif') . ") no-repeat top left;'>";
 		$max_size = isset($GLOBALS['meta']['max_taille_vignettes'])?$GLOBALS['meta']['max_taille_vignettes']:(500*500);
 		$max_size_echec = isset($GLOBALS['meta']['max_taille_vignettes_echec'])?$GLOBALS['meta']['max_taille_vignettes_echec']:0;
 		$max_size_test = isset($GLOBALS['meta']['max_taille_vignettes_test'])?$GLOBALS['meta']['max_taille_vignettes_test']:0;
 		if ($max_size_test<$max_size_echec OR  ($max_size_test AND !$max_size_echec)){
 			ecrire_meta('max_taille_vignettes_echec',$max_size_echec = $max_size_test,'non');
 		}
-		$maxtest = 1740; // 3MPixels
-		$test = array();
-		$time = time();
-		if ($max_size >= ($maxtest-20)*($maxtest-20)) $maxtest = 2380; // 6MPixels
-		$top = 16;
-		for ($j = 320;$j>=20;$j = $j/2){
-			$test_out .= "<div style='position:relative;top:{$top}px;$spip_lang_left:0px;font-size:1px;height:8px;'>";
-			$l = round($j/10);
-			$lok = 0; $lbad =0;
-			$margin_left = 0;
-			$top -= 8;
-			for ($i = 480;$i*$i<$max_size && $i<=$maxtest;$i+=$j) $lok += $l;
-			if ($lok-$l+2>0) 
-				$test_out .= "<img src='". chemin_image('jauge-vert.gif') . "' width='".($lok-$l+2)."' style='margin-right:".($l-2)."px;' height='8' alt='' />";
-			for (;(!$max_size_echec OR $i*$i<$max_size_echec) && $i<=$maxtest;$i+=$j){
-				if (!isset($test[$i])){
-					$url = generer_url_action("tester_taille", "arg=$i&time=$time");
-					$test_out .= "<img src='$url' width='2' style='margin-left:{$margin_left}px;margin-right:".($l-2)."px;' height='8' alt='' />";
-					$test[$i] = 1;
-					$margin_left = 0;
+		if (_IMG_GD_MAX_PIXELS==0 OR _IMG_GD_MAX_PIXELS==$GLOBALS['meta']['max_taille_vignettes']){
+		  $test_out .= "<p>"._T('info_taille_maximale_images')."</p>";
+			
+			$test_out .= "<div dir='ltr' id='teste_memory_size_gd' style='text-align:left;float:$spip_lang_right;width:196px;background:url(". chemin_image('jauge-test-gd.gif') . ") no-repeat top left;'>";
+			$maxtest = 1740; // 3MPixels
+			$test = array();
+			$time = time();
+			if ($max_size >= ($maxtest-20)*($maxtest-20)) $maxtest = 2380; // 6MPixels
+			$top = 16;
+			for ($j = 320;$j>=20;$j = $j/2){
+				$test_out .= "<div style='position:relative;top:{$top}px;$spip_lang_left:0px;font-size:1px;height:8px;'>";
+				$l = round($j/10);
+				$lok = 0; $lbad =0;
+				$margin_left = 0;
+				$top -= 8;
+				for ($i = 480;$i*$i<$max_size && $i<=$maxtest;$i+=$j) $lok += $l;
+				if ($lok-$l+2>0) 
+					$test_out .= "<img src='". chemin_image('jauge-vert.gif') . "' width='".($lok-$l+2)."' style='margin-right:".($l-2)."px;' height='8' alt='' />";
+				for (;(!$max_size_echec OR $i*$i<$max_size_echec) && $i<=$maxtest;$i+=$j){
+					if (!isset($test[$i])){
+						$url = generer_url_action("tester_taille", "arg=$i&time=$time");
+						$test_out .= "<img src='$url' width='2' style='margin-left:{$margin_left}px;margin-right:".($l-2)."px;' height='8' alt='' />";
+						$test[$i] = 1;
+						$margin_left = 0;
+					}
+					else $margin_left += $l;
 				}
-				else $margin_left += $l;
+				for (;$i<=$maxtest;$i+=$j) $lbad += $l;
+				if ($lbad) $test_out .= "<img src='" . chemin_image('jauge-rouge.gif') . "' width='$lbad' height='8' style='margin-left:{$margin_left}px;' alt='' />";
+				$test_out .= "</div>";
 			}
-			for (;$i<=$maxtest;$i+=$j) $lbad += $l;
-			if ($lbad) $test_out .= "<img src='" . chemin_image('jauge-rouge.gif') . "' width='$lbad' height='8' style='margin-left:{$margin_left}px;' alt='' />";
-			$test_out .= "</div>";
+			$test_out .= "</div><br style='clear:both;' />";
 		}
-		$test_out .= "</div><br style='clear:both;' />";
 	} else {
-			effacer_meta('max_taille_vignettes');
-			effacer_meta('max_taille_vignettes_echec');
-			effacer_meta('max_taille_vignettes_test');
+		effacer_meta('max_taille_vignettes');
+		effacer_meta('max_taille_vignettes_echec');
+		effacer_meta('max_taille_vignettes_test');
 	}
 
 	return ajax_action_greffe("configurer-reducteur", '', 

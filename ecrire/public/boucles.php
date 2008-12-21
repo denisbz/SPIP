79319ad7a0dd516a4d5fe76414493b05d924396b
esj <esj@rezo.net>
2008-12-21 18:02:12 +0000

    Risquaient pas de montrer les doc attachés aux messages de forums  les boucles documents du squelette inc-forum, vu comment le compilateur préparait cette boucle.
    git-svn-id: svn://trac.rezo.net/spip/spip@13511 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/public/boucles.php
+++ ecrire/public/boucles.php
@@ -192,16 +192,18 @@ function boucle_DOCUMENTS_dist($id_boucle, &$boucles) {
 				ON (l.id_objet=bb.id_breve AND l.objet=\"breve\")
 			LEFT JOIN spip_rubriques AS rr
 				ON (l.id_objet=rr.id_rubrique AND l.objet=\"rubrique\")
+			LEFT JOIN spip_forum AS ff
+				ON (l.id_objet=ff.id_forum AND l.objet=\"forum\")
 		";
 		$boucle->group[] = "$id_table.id_document";
 		
 		if ($GLOBALS['var_preview']) {
-			array_unshift($boucle->where,"'(aa.statut IN (\"publie\",\"prop\") OR bb.statut  IN (\"publie\",\"prop\") OR rr.statut IN (\"publie\",\"prive\"))'");
+			array_unshift($boucle->where,"'(aa.statut IN (\"publie\",\"prop\") OR bb.statut  IN (\"publie\",\"prop\") OR rr.statut IN (\"publie\",\"prive\") OR ff.statut IN (\"publie\",\"prop\"))'");
 		} else {
 			$postdates = ($GLOBALS['meta']['post_dates'] == 'non')
 				? ' AND aa.date<=\'.sql_quote(quete_date_postdates()).\''
 				: '';
-			array_unshift($boucle->where,"'((aa.statut = \"publie\"$postdates) OR bb.statut = \"publie\" OR rr.statut = \"publie\")'");
+			array_unshift($boucle->where,"'((aa.statut = \"publie\"$postdates) OR bb.statut = \"publie\" OR rr.statut = \"publie\" OR ff.statut=\"publie\")'");
 		}
 	}
 

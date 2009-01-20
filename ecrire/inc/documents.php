dc0691b6a75355cdafa9ebfb288a214660faeb21
marcimat <marcimat@rezo.net>
2009-01-20 22:49:06 +0000

    - Les documents s'ajoutent sur les textarea de nom texte... - code mort js (il doit y en avoir d'autre dans ce fichier spip_barre.js)
    git-svn-id: svn://trac.rezo.net/spip/spip@13641 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/documents.php
+++ ecrire/inc/documents.php
@@ -353,7 +353,7 @@ function affiche_raccourci_doc($doc, $id, $align) {
 		$pipe = "|$align";
 
 		if ($GLOBALS['browser_barre'])
-			$onclick = "\nondblclick=\"barre_inserer('\\x3C$doc$id$pipe&gt;', $('.barre_inserer')[0]);\"\ntitle=\"". str_replace('&amp;', '&', entites_html(_T('double_clic_inserer_doc')))."\"";
+			$onclick = "\nondblclick=\"barre_inserer('\\x3C$doc$id$pipe&gt;', $('textarea[name=texte]')[0]);\"\ntitle=\"". str_replace('&amp;', '&', entites_html(_T('double_clic_inserer_doc')))."\"";
 	} else {
 		$align='center';
 	}
--- prive/javascript/spip_barre.js
+++ prive/javascript/spip_barre.js
@@ -20,56 +20,6 @@ var is_win = ((clientPC.indexOf("win")!=-1) || (clientPC.indexOf("16bit") != -1)
 var is_mac = (clientPC.indexOf("mac")!=-1);
 
 
-function barre_raccourci(debut,fin,champ) {
-	var txtarea = champ;
-
-	txtarea.focus();
-	donotinsert = false;
-	theSelection = false;
-	bblast = 0;
-
-	if ((clientVer >= 4) && is_ie && is_win)
-	{
-		theSelection = document.selection.createRange().text; // Get text selection
-		if (theSelection) {
-
-			while (theSelection.substring(theSelection.length-1, theSelection.length) == ' ')
-			{
-				theSelection = theSelection.substring(0, theSelection.length-1);
-				fin = fin + " ";
-			}
-			if (theSelection.substring(0,1) == '{' && debut.substring(0,1) == '{')
-			{
-				debut = debut + " ";
-			}
-			if (theSelection.substring(theSelection.length-1, theSelection.length) == '}' && fin.substring(0,1) == '}')
-			{
-				fin = " " + fin;
-			}
-
-			// Add tags around selection
-			document.selection.createRange().text = debut + theSelection + fin;
-			txtarea.focus();
-			theSelection = '';
-			return;
-		}
-	}
-	else if (txtarea.selectionEnd && (txtarea.selectionEnd - txtarea.selectionStart > 0))
-	{
-		mozWrap(txtarea, debut, fin);
-		return;
-	}
-}
-
-function barre_demande(debut,milieu,fin,affich,champ) {
-	var inserer = prompt(affich);
-
-	if (inserer != null) {
-		if (inserer == "") {inserer = "xxx"; }
-
-		barre_raccourci(debut, milieu+inserer+fin, champ);
-	}
-}
 
 function barre_inserer(text,champ) {
 	var txtarea = champ;

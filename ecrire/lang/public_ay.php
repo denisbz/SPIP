950dff6f7f1c1fd63a38cfb7feba660b2a79e9f4
fil <fil@rezo.net>
2009-01-22 14:35:48 +0000

    version aymara (Dani Che, y viva Bolivia)
    git-svn-id: svn://trac.rezo.net/spip/spip@13644 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

---
+++ ecrire/lang/public_ay.php
@@ -0,0 +1,129 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP nommé  genere le NOW()
+// langue / language = ay
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+
+// A
+'accueil_site' => 'U&ntilde;naq qamawita',
+'articles' => 'Qillqatanaka',
+'articles_auteur' => 'Qillqirin aka qillqatanakapa',
+'articles_populaires' => 'U&ntilde;t\'at qillqatanaka',
+'articles_recents' => 'Jichha yatxatawinaka',
+'articles_rubrique' => 'Aka t\'aqat qillqatanaka',
+'aucun_article' => 'Aka utxawinx, ni kuna qillqatas utjkiti',
+'aucun_auteur' => 'Aka utxawinx janiw qillqirix utjkiti',
+'aucun_site' => 'Aka utxawinx ni kuna qamawins utjkiti',
+'aucune_breve' => 'Aka utxawinx ni kuna jisk\'aptatas utjkiti',
+'aucune_rubrique' => 'Aka utxawinx ni kuna t\'aqas utjkiti',
+'autres_breves' => 'Yaqha jisk\'aptatanaka',
+'autres_groupes_mots_clefs' => 'Imt\'aw arunakata yaqha tamanaka',
+'autres_sites' => 'Yaqha qamawinaka',
+
+
+// B
+'bonjour' => 'Kamisaki',
+'breves' => 'Jisk\'aptatanaka',
+
+
+// C
+'commenter_site' => 'Aka qamawxat aruskipt\'a&ntilde;a',
+
+
+// D
+'date' => 'Uru',
+'dernier_ajout' => 'Qhipha jichhapxtawi',
+'dernieres_breves' => 'Qhipa jisk\'aptatanaka',
+'derniers_articles' => 'Qhiphankir qillqatanaka',
+'derniers_commentaires' => 'Qhipha aruskipt\'awinaka',
+'derniers_messages_forum' => 'Qhipha yatiyawinak aruskipawinakan yatiyata',
+
+
+// E
+'edition_mode_texte' => 'Sawutjama lurawi',
+'en_reponse' => 'Ukar jaysaya&ntilde;a',
+'en_resume' => 'Jisk\'aptasina',
+'envoyer_message' => 'Ma yatiyaw apaya&ntilde;a',
+'espace_prive' => 'Ch\'usawx jark\'ata',
+
+
+// F
+'forum' => 'Aruskipa&ntilde;a',
+
+
+// H
+'hierarchie_site' => 'Qamawita kamachi&ntilde;awi',
+
+
+// J
+'jours' => 'Urunaka',
+
+
+// M
+'meme_auteur' => 'Pachpa qillqirita',
+'meme_rubrique' => 'Pachpa t\'aqana',
+'memes_auteurs' => 'Pachpa qillqirinakata',
+'message' => 'Yatiyawi',
+'messages_forum' => 'Aruskipawit yatiyawinaka',
+'messages_recents' => 'Amuyt\'awit jichhak amuyt\'awinaka',
+'mots_clefs' => 'Imt\'at arunaka',
+'mots_clefs_meme_groupe' => 'Pachpa taman imt\'at arunaka',
+
+
+// N
+'navigation' => 'Tuyuriwi',
+'nom' => 'Suti',
+'nouveautes' => 'Machaq yatiyawinaka',
+'nouveautes_web' => 'Web ukan machaq yatiyawinaka',
+'nouveaux_articles' => 'Machaq qillqatanaka',
+'nouvelles_breves' => 'Machaq jisk\'aptatanaka',
+
+
+// P
+'page_precedente' => 'Nayrir laphi u&ntilde;naqa',
+'page_suivante' => 'Jutir laphi u&ntilde;naqa',
+'par_auteur' => 'Lurata',
+'participer_site' => 'Kunanaks lurasmaw aka jakawi qamawin ukhamarak qillqatanak lurasma akjaru qillqasisina.Ukat m&auml;kiw m&auml; correo electr&oacute;nico qatuqata mantaw imt\'atanakamampi',
+'plan_site' => 'Qamawt irpiri',
+'popularite' => 'Juk\'amp u&ntilde;jata',
+'poster_message' => 'Ma yatiyawi qillqa&ntilde;a',
+'proposer_site' => 'Aka t\'aqan usku&ntilde;atakix ma qamaw amuyt\'am:',
+
+
+// R
+'repondre_article' => 'Aka qillqat amuykipa&ntilde;a',
+'repondre_breve' => 'Aka jisk\'aptata amuykipa&ntilde;a',
+'repondre_message' => 'Aka yatiyawir jaysa&ntilde;a',
+'resultats_recherche' => 'Thaqhata u&ntilde;stayi',
+'retour_debut_forums' => 'Aruskipawi qalltar kuti&ntilde;a',
+'rubrique' => 'T\'aqa',
+'rubriques' => 'T\'aqanaka',
+
+
+// S
+'signatures_petition' => 'Firmas',
+'site_realise_avec_spip' => 'Qamaw SPIP ukamp lurata',
+'sites_web' => 'web qamawinaka',
+'sous_rubriques' => 'Jutir t\'aqa',
+'suite' => 'Sarantaskaki&ntilde;a',
+'sur_web' => 'Web ukata',
+'syndiquer_rubrique' => 'Aka t\'aqa apxasi&ntilde;a',
+'syndiquer_site' => 't\'aqat qamaw apxasi&ntilde;a',
+
+
+// T
+'texte_lettre_information' => 'Akax jisk\'a qillqat yatiyawin qamawipawa',
+'texte_lettre_information_2' => 'Aka jisk\'a qillqatxa qillqatanak ukhamaraki jisk\'aptat yatiyatanak jisk\'ayi',
+
+
+// V
+'ver_imprimer' => 'U&ntilde;jat liq\'su&ntilde;ataki',
+'voir_en_ligne' => 'Naktata u&ntilde;ja&ntilde;a',
+'voir_squelette' => 'Aka laphi u&ntilde;naqan ch\'aka ch\'aka u&ntilde;ja&ntilde;a'
+
+
+);
+
+?>

f253ff83100528a901d112228c8d5d04180e86c8
cedric <cedric@yterium.com>
2009-01-23 16:58:41 +0000

    "4 filtres issus de bonux avec un nommage plus clair : - un filtre_icone pour utiliser selon la syntaxe [(#URL|icone{texte,fond,align,fonction,class})] et qui permet de faire des icones de l'interface privee dans les squelettes
    - un affiche_un_ou_plusieurs a utiliser avec :
    [(#TOTAL_BOUCLE|affiche_un_ou_plusieurs{xxx:chaine_un,xxx:chaine_plusieurs})]
    
    - un filtre balise_img pour faire simplement
    [(#CHEMIN{monimage.png}|balise_img)]
    peut prendre alt et class en arguments
    
    - lien_ou_pasclicable : pour faire un lien ou un <strong class='on'> selon l'etat du lien :
    [(#URL|lien_ou_pasclicable{texte,condition selection,class,title,rel})]
    class title et rel sont facultatives"
    
    git-svn-id: svn://trac.rezo.net/spip/spip@13651 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/filtres.php
+++ ecrire/inc/filtres.php
@@ -2266,4 +2266,143 @@ function _xor($message, $key=null){
 function url_reponse_forum($texte){return $texte;}
 function url_rss_forum($texte){return $texte;}
 
+
+/**
+ * une fonction pour generer des menus avec liens
+ * ou un <strong class='on'> non clicable lorsque l'item est selectionne
+ *
+ * @param string $url
+ * @param string $libelle
+ * @param bool $on
+ * @param string $class
+ * @param string $title
+ * @return string
+ */
+function lien_ou_pasclicable($url,$libelle,$on=false,$class="",$title="",$rel=""){
+	return 
+	($on ?"<strong class='on'>":
+		"<a href='$url'"
+	  	.($title?" title='".attribut_html($title)."'":'')
+	  	.($class?" class='".attribut_html($class)."'":'')
+	  	.($rel?" rel='".attribut_html($rel)."'":'')
+	  	.">"
+	)
+	. $libelle
+	. ($on ? "</strong>":"</a>");
+}
+
+
+/**
+ * une fonction pour generer une balise img a partir d'un nom de fichier
+ *
+ * @param string $img
+ * @param string $alt
+ * @param string $class
+ * @return string
+ */
+function balise_img($img,$alt="",$class=""){
+	$taille = taille_image($img);
+	list($hauteur,$largeur) = $taille;
+	if (!$hauteur OR !$largeur)
+		return "";
+	return 
+	"<img src='$img' width='$largeur' height='$hauteur'"
+	  ." alt='".attribut_html($alt)."'"
+	  .($class?" class='".attribut_html($class)."'":'')
+	  .' />';
+}
+
+
+/**
+ * Afficher un message "un truc"/"N trucs"
+ *
+ * @param int $nb
+ * @return string
+ */
+function affiche_un_ou_plusieurs($nb,$chaine_un,$chaine_plusieurs,$var='nb'){
+	if (!$nb=intval($nb)) return "";
+	if ($nb>1) return _T($chaine_plusieurs, array($var => $nb));
+	else return _T($chaine_un);
+}
+
+
+/**
+ * un filtre icone mappe sur icone_inline, qui cree une icone a gauche par defaut
+ * le code de icone_inline est grandement reproduit ici car les liens ajax portent simplement une class ajax
+ * lorsque les interfaces sont en squelette, alors que l'implementation d'ajax de des scripts php
+ * est plus complexe
+ *
+ * @param string $lien
+ * @param string $texte
+ * @param string $fond
+ * @param string $align
+ * @param string $fonction
+ * @return string
+ */
+function filtre_icone($lien, $texte, $fond, $align="", $fonction="", $class=""){
+	$align = $align?$align:$GLOBALS['spip_lang_left'];
+	global $spip_display;
+
+	if ($fonction == "supprimer.gif") {
+		$style = 'icone36 danger';
+	} else {
+		$style = 'icone36';
+		if (strlen($fonction) < 3) $fonction = "rien.gif";
+	}
+	$style .= " " . substr(basename($fond),0,-4);
+
+	if ($spip_display == 1){
+		$hauteur = 20;
+		$largeur = 100;
+		$title = $alt = "";
+	}
+	else if ($spip_display == 3){
+		$hauteur = 30;
+		$largeur = 30;
+		$title = "\ntitle=\"$texte\"";
+		$alt = $texte;
+	}
+	else {
+		$hauteur = 70;
+		$largeur = 100;
+		$title = '';
+		$alt = $texte;
+	}
+
+	$size = 24;
+	if (preg_match("/-([0-9]{1,3})[.](gif|png)$/i",$fond,$match))
+		$size = $match[1];
+	if ($spip_display != 1 AND $spip_display != 4){
+		if ($fonction != "rien.gif"){
+		  $icone = http_img_pack($fonction, $alt, "$title width='$size' height='$size'\n" .
+					  http_style_background($fond, "no-repeat center center"));
+		}
+		else {
+			$icone = http_img_pack($fond, $alt, "$title width='$size' height='$size'");
+		}
+	} else $icone = '';
+
+	// cas d'ajax_action_auteur: faut defaire le boulot
+	// (il faudrait fusionner avec le cas $javascript)
+	if (preg_match(",^<a\shref='([^']*)'([^>]*)>(.*)</a>$,i",$lien,$r))
+		list($x,$lien,$atts,$texte)= $r;
+	else $atts = '';
+
+	if ($align && $align!='center') $align = "float: $align; ";
+
+	$icone = "<a style='$align' class='$style $class'"
+	. $atts
+	. $javascript
+	. "\nhref='"
+	. $lien
+	. "'>"
+	. $icone
+	. (($spip_display == 3)	? '' : "<span>$texte</span>")
+	  . "</a>\n";
+
+	if ($align <> 'center') return $icone;
+	$style = " style='text-align:center;'";
+	return "<div$style>$icone</div>";
+}
+
 ?>

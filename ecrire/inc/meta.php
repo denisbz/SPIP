440c63ee338cf4848accd4e68aef6cdbcd39dde6
esj <esj@rezo.net>
2009-01-28 22:22:17 +0000

    Compris pourquoi des mises à jours (du moins certaines d'entre elles) renvoeint sur le login immédiatement après avoir été lancées. Il s'agit des versions < [8097] qui n'ont pas de champ "impt" dans la table des meta. Un hash reposant sur l'alea éphémère est construit, mais si cet aléa est trop vieux il est renouvelé au niveau PHP, tandis que son enregistrement par SQL échoue car la colonne "impt" est inconnue de la version installée. Du coup le hash comparé est faux et SPIP sort l'apparent faussaire.
    Message plus clair: si vous avez une version < [8097] vous ne pourrez pas passer en 2.0.3 (et en pas grand chose d'ailleurs).
    
    
    git-svn-id: svn://trac.rezo.net/spip/branches/spip-2.0@13677 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/meta.php
+++ ecrire/inc/meta.php
@@ -36,13 +36,13 @@ function inc_meta_dist()
 	// renouveller l'alea au besoin
 	if ((test_espace_prive() || isset($_GET['renouvelle_alea']))
 	AND $GLOBALS['meta']
+	AND ($GLOBALS['exec'] === 'upgrade')
 	AND (time() > _RENOUVELLE_ALEA + @$GLOBALS['meta']['alea_ephemere_date'])) {
 		// si on n'a pas l'acces en ecriture sur le cache,
 		// ne pas renouveller l'alea sinon le cache devient faux
 		if (supprimer_fichier(_FILE_META)) {
 			include_spip('inc/acces');
 			renouvelle_alea();
-spip_log('renouvelle_alea');
 			$new = false; 
 		} else spip_log("impossible d'ecrire dans " . _FILE_META);
 	}

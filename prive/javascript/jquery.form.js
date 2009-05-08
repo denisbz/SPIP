c9c5cf1d1c442cf1c1aac2ad7d828fe648daddcf
cedric <cedric@yterium.com>
2009-05-08 15:44:52 +0000

    correction pour les formulaires forum donc action='#forum' ce qui fait echouer FF 3.1 A tester sous ie8 ! complement : rester aussi pres que possible du js original
    git-svn-id: svn://trac.rezo.net/spip/branches/spip-2.0@13967 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- prive/javascript/jquery.form.js
+++ prive/javascript/jquery.form.js
@@ -54,11 +54,11 @@ $.fn.ajaxSubmit = function(options) {
         options = { success: options };
 
     // clean url (don't include hash vaue)
-    var url = this.attr('action') || window.location.toString();
+    var url = this.attr('action') || window.location.href;
     url = (url.match(/^([^#]+)/)||[])[1];
 		// if action is only an anchor and remains empty, ff fail
 		// try again with window.location in this case
-    url = url || window.location.toString();
+    url = url || window.location.href;
     url = (url.match(/^([^#]+)/)||[])[1];
     url = url || '';
 

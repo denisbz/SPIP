ecf122b3bca23611f72a3fd20f0e87ce36dead65
cedric <cedric@yterium.com>
2009-05-10 15:45:29 +0000

    l'optimisation de la pagination avec sql_seek ne plante pas si sql_seek n'est pas disponible
    git-svn-id: svn://trac.rezo.net/spip/spip@13975 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/public/compiler.php
+++ ecrire/public/compiler.php
@@ -326,7 +326,7 @@ function calculer_boucle_nonrec($id_boucle, &$boucles) {
 	return  $count .
 		(!$flag_cpt  ? "" :
 			"\n\t\$Numrows['$id_boucle']['compteur_boucle'] = 0;"
-		 .(($boucle->mode_partie)?"\n\tif (isset(\$debut_boucle) AND \$debut_boucle) sql_seek(\$result,\$Numrows['$id_boucle']['compteur_boucle']=\$debut_boucle);":"")
+		 .(($boucle->mode_partie)?"\n\tif (isset(\$debut_boucle) AND \$debut_boucle AND sql_seek(\$result,\$debut_boucle,"._q($boucle->sql_serveur).",'continue')) \$Numrows['$id_boucle']['compteur_boucle']=\$debut_boucle;":"")
 			)
 		. '
 	$t0 = "";' .

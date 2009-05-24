f4a86b21dc90b3a97698bd74dbd4fbeac079bf8b
esj <esj@rezo.net>
2009-05-24 05:24:15 +0000

    Le Title de l'agenda indiquait toujours la date courante.
    git-svn-id: svn://trac.rezo.net/spip/spip@14039 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/exec/calendrier.php
+++ ecrire/exec/calendrier.php
@@ -17,47 +17,50 @@ include_spip('inc/presentation');
 // http://doc.spip.org/@exec_calendrier_dist
 function exec_calendrier_dist()
 {
-  $type = _request('type');
+	if ($date = _request('date')){
+		$time = explode('-', $date);
+		if (count($time) <= 2) $time[]=1;
+		if (count($time) <= 2) $time[]=1;
+		$time = array_reverse($time);
+	} else {
+		$time = array(_request('jour'), _request('mois'), _request('annee'));
+		if ($time[1] AND $time[0] AND $time[2])
+			$date = mktime(1,1,1, $time[1], $time[0], $time[2]);
+		else 	$date = date("Y-m-d", $timem = time()); 
+	}
 
-  $date = date("Y-m-d", time()); 
-  if ($type == 'semaine') {
+	$type = _request('type');
 
-	$GLOBALS['afficher_bandeau_calendrier_semaine'] = true;
+	if ($type == 'semaine') {
 
-	$titre = _T('titre_page_calendrier',
+		$GLOBALS['afficher_bandeau_calendrier_semaine'] = true;
+
+		$titre = _T('titre_page_calendrier',
 		    array('nom_mois' => nom_mois($date), 'annee' => annee($date)));
 	  }
-  elseif ($type == 'jour') {
-	$titre = nom_jour($date)." ". affdate_jourcourt($date);
- }
-  else {
-	$titre = _T('titre_page_calendrier',
+	elseif ($type == 'jour') {
+		$titre = nom_jour($date)." ". affdate_jourcourt($date);
+	}  else {
+		$titre = _T('titre_page_calendrier',
 		    array('nom_mois' => nom_mois($date), 'annee' => annee($date)));
-	$type = 'mois';
-  }
-  $ancre = 'calendrier-1';
-  if (_request('date')){
-	  $time = explode('-',_request('date'));
-	  $time = array_reverse($time);
-  }
-  else
-	  $time = array(_request('jour'), _request('mois'), _request('annee'));
+		$type = 'mois';
+	}
+	$ancre = 'calendrier-1';
+	$r = generer_url_ecrire('calendrier', "type=$type") . "#$ancre";
+	$r = http_calendrier_init($time, $type, _request('echelle'), _request('partie_cal'), $r);
 
-  $r = generer_url_ecrire('calendrier', "type=$type") . "#$ancre";
-  $r = http_calendrier_init($time, $type, _request('echelle'), _request('partie_cal'), $r);
-
-  if (_AJAX) {
-    ajax_retour($r);
-  } else {
-	$commencer_page = charger_fonction('commencer_page', 'inc');
-	echo $commencer_page($titre, "accueil", "calendrier");
-  // ne produit rien par defaut, mais est utilisee par le plugin agenda
-	echo barre_onglets("calendrier", "calendrier"); 
-	echo debut_grand_cadre(true);
-	echo "\n<div>&nbsp;</div>\n<div id='", $ancre, "'>",$r,'</div>';
-	echo fin_grand_cadre(true);
-	echo fin_page();
-  }
+	if (_AJAX) {
+		ajax_retour($r);
+	} else {
+		$commencer_page = charger_fonction('commencer_page', 'inc');
+		echo $commencer_page($titre, "accueil", "calendrier");
+// ne produit rien par defaut, mais est utilisee par le plugin agenda
+		echo barre_onglets("calendrier", "calendrier"); 
+		echo debut_grand_cadre(true);
+		echo "\n<div>&nbsp;</div>\n<div id='", $ancre, "'>",$r,'</div>';
+		echo fin_grand_cadre(true);
+		echo fin_page();
+	}
 }
 
 ?>

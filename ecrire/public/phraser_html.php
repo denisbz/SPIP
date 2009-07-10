1d7e66c1b748e76a3ad8935feb7331ce71a800e3
esj <esj@rezo.net>
2009-07-10 11:04:44 +0000

    Oups [14172]. Ce que c'est que de faire 36 choses en parallèle.
    git-svn-id: svn://trac.rezo.net/spip/branches/spip-2.0@14189 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/public/phraser_html.php
+++ ecrire/public/phraser_html.php
@@ -152,7 +152,7 @@ function phraser_champs($texte,$ligne,$result) {
 		if ($suite[0] == '{') {
 			phraser_arg($suite, '', array(), $champ);
 			$texte = ltrim($suite);
-		}
+		} else $texte = $suite;
 		$result[] = $champ;
 	  } else {
 	    // faux champ

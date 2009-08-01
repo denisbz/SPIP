8de04aad5943366914164352ba8db6d96be26c3e
cedric <cedric@yterium.com>
2009-08-01 12:38:55 +0000

    les variables id_xx peuvent etre des tableaux d'entier, et utiliser strncmp 20% plus rapide que substr
    git-svn-id: svn://trac.rezo.net/spip/branches/spip-2.0@14324 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- config/ecran_securite.php
+++ config/ecran_securite.php
@@ -50,14 +50,14 @@ define('_ECRAN_SECURITE', '0.6'); // 27 juillet 2009
  *       (sauf pour id_table, qui n'est pas numerique jusqu'a [5743])
  */
 foreach ($_GET as $var => $val)
-	if (substr($var, 0,3) == 'id_' AND $var!='id_table')
-		$_GET[$var] = intval($_GET[$var]);
+	if (strncmp($var,"id_",3)==0 AND $var!='id_table')
+		$_GET[$var] = is_array($_GET[$var])?array_map('intval',$_GET[$var]):intval($_GET[$var]);
 foreach ($_POST as $var => $val)
-	if (substr($var, 0,3) == 'id_' AND $var!='id_table')
-		$_POST[$var] = intval($_POST[$var]);
+	if (strncmp($var,"id_",3)==0 AND $var!='id_table')
+		$_POST[$var] = is_array($_POST[$var])?array_map('intval',$_POST[$var]):intval($_POST[$var]);
 foreach ($GLOBALS as $var => $val)
-	if (substr($var, 0,3) == 'id_' AND $var!='id_table')
-		$GLOBALS[$var] = intval($GLOBALS[$var]);
+	if (strncmp($var,"id_",3)==0 AND $var!='id_table')
+		$GLOBALS[$var] = is_array($GLOBALS[$var])?array_map('intval',$GLOBALS[$var]):intval($GLOBALS[$var]);
 
 
 /*     - interdit la variable $cjpeg_command, qui etait utilisee sans

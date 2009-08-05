5889ce042573e6002605a1222a7e340d2a32841a
esj <esj@rezo.net>
2009-08-05 05:36:39 +0000

    [14347] en mieux.
    git-svn-id: svn://trac.rezo.net/spip/branches/spip-2.0@14348 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/exec/install.php
+++ ecrire/exec/install.php
@@ -23,7 +23,7 @@ define('_FILE_TMP', '_install');
 function exec_install_dist()
 {
 	$etape = _request('etape');
-	if ((_FILE_CONNECT AND (!in_array($etape, array('chmod', 'sup1', 'sup2')))) OR !autoriser('configurer')) {
+	if (_FILE_CONNECT  AND (!in_array($etape, array('chmod', 'sup1', 'sup2')) OR !autoriser('configurer'))) {
   // L'etape chmod peut etre reexecutee n'importe quand apres l'install,
   // pour verification des chmod. Sinon, install deja faite => refus.
 		echo minipres();

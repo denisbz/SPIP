564ca22b8adb188d282d48ac740fbb3047dd0730
esj <esj@rezo.net>
2009-08-05 05:28:00 +0000

    Sécurité: un simple rédacteur avait le droit d'appliquer chmod et tenter  de déclarer une autre base. Je n'ai pas encore approfondi le degré de gravité de la chose, mais c'est de toutes façons anormal de pouvoir arriver là.
    git-svn-id: svn://trac.rezo.net/spip/branches/spip-2.0@14347 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/exec/install.php
+++ ecrire/exec/install.php
@@ -14,6 +14,7 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 include_spip('inc/minipres');
 include_spip('inc/install');
+include_spip('inc/autoriser');
 
 define("_ECRIRE_INSTALL", "1");
 define('_FILE_TMP', '_install');
@@ -22,7 +23,7 @@ define('_FILE_TMP', '_install');
 function exec_install_dist()
 {
 	$etape = _request('etape');
-	if (_FILE_CONNECT AND (!in_array($etape, array('chmod', 'sup1', 'sup2')))) {
+	if ((_FILE_CONNECT AND (!in_array($etape, array('chmod', 'sup1', 'sup2')))) OR !autoriser('configurer')) {
   // L'etape chmod peut etre reexecutee n'importe quand apres l'install,
   // pour verification des chmod. Sinon, install deja faite => refus.
 		echo minipres();

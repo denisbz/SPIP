f58de7d8debf1f69236419baddcfc4b87649d5fe
esj <esj@rezo.net>
2009-08-15 11:15:43 +0000

    La fonction {{{spip_log}}} se réduit à présent à appeller la fonction surchargeable {{{inc_log_dist}}}, qui fait la même chose qu'avant  en standard.
    git-svn-id: svn://trac.rezo.net/spip/spip@14396 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/utils.php
+++ ecrire/inc/utils.php
@@ -149,64 +149,8 @@ function pipeline($action, $val=null) {
 //
 // http://doc.spip.org/@spip_log
 function spip_log($message, $logname=NULL, $logdir=NULL, $logsuf=NULL) {
-	static $compteur = array();
-	global $nombre_de_logs, $taille_des_logs;
-
-	if (is_null($logname))
-		$logname = defined('_FILE_LOG') ? _FILE_LOG : 'spip';
-	if (!isset($compteur[$logname])) $compteur[$logname] = 0;
-	if ($logname != 'maj'
-	AND defined('_MAX_LOG')
-	AND (
-		$compteur[$logname]++ > _MAX_LOG
-		OR !$nombre_de_logs
-		OR !$taille_des_logs
-	))
-		return;
-
-	$logfile = ($logdir===NULL ? _DIR_LOG : $logdir)
-	  . (test_espace_prive()?'prive_':'') //distinguer les logs prives et publics
-	  . ($logname)
-	  . ($logsuf===NULL ? _FILE_LOG_SUFFIX : $logsuf);
-
-	// si spip_log() dans mes_options, poser dans spip.log
-	if (!defined('_DIR_LOG'))
-		$logfile = _DIR_RACINE._NOM_TEMPORAIRES_INACCESSIBLES.$logname.'.log';
-
-	$rotate = 0;
-	$pid = '(pid '.@getmypid().')';
-
-	// accepter spip_log( Array )
-	if (!is_string($message)) $message = var_export($message, true);
-
-	$m = date("M d H:i:s").' '.$GLOBALS['ip'].' '.$pid.' '
-		.preg_replace("/\n*$/", "\n", $message);
-
-
-	if (@is_readable($logfile)
-	AND (!$s = @filesize($logfile) OR $s > $taille_des_logs * 1024)) {
-		$rotate = $nombre_de_logs;
-		$m .= "[-- rotate --]\n";
-	}
-
-	$f = @fopen($logfile, "ab");
-	if ($f) {
-		fputs($f, ($logname!==NULL) ? $m : str_replace('<','&lt;',$m));
-		fclose($f);
-	}
-
-	if ($rotate-- > 0
-	AND function_exists('spip_unlink')) {
-		spip_unlink($logfile . '.' . $rotate);
-		while ($rotate--) {
-			@rename($logfile . ($rotate ? '.' . $rotate : ''), $logfile . '.' . ($rotate + 1));
-		}
-	}
-
-	// Dupliquer les erreurs specifiques dans le log general
-	if ($logname !== _FILE_LOG
-	AND defined('_FILE_LOG'))
-		spip_log($logname=='maj' ? 'cf maj.log' : $message);
+	$log = charger_fonction('log', 'inc');
+	$log( $message, $logname, $logdir, $logsuf);
 }
 
 // Renvoie le _GET ou le _POST emis par l'utilisateur

17cc84d728e06e88da4e79a8822197ae05665d2c
salvatore <salvatore@rezo.net>
2009-09-22 20:04:58 +0000

    langues (public)
    git-svn-id: svn://trac.rezo.net/spip/spip@14525 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/lang/public_co.php
+++ ecrire/lang/public_co.php
@@ -1,6 +1,8 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://trac.rezo.net/spip/spip/ecrire/lang/
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(

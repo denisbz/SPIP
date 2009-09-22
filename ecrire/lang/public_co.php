1185b24293176a0209053d3ac2d75397bcb6fb35
fil <fil@rezo.net>
2009-09-22 19:53:15 +0000

    ajout du corsu (auteur: tavamig)
    git-svn-id: svn://trac.rezo.net/spip/spip@14522 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

---
+++ ecrire/lang/public_co.php
@@ -0,0 +1,120 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://trac.rezo.net/spip/spip/ecrire/lang/
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// A
+'accueil_site' => 'Accogliu di u situ',
+'articles' => 'Art&igrave;culi',
+'articles_auteur' => 'Art&igrave;culi di st\'autore',
+'articles_populaires' => 'Art&igrave;culi i pi&ugrave; pupulari',
+'articles_rubrique' => 'Art&igrave;culi di sta r&ugrave;bbrica',
+'aucun_article' => '&Ugrave;n ci h&egrave; art&igrave;culu &agrave; st\'indirizzu',
+'aucun_auteur' => '&Ugrave;n ci h&egrave; autore &agrave; st\'indirizzu',
+'aucun_site' => '&Ugrave;n ci h&egrave; situ &agrave; st\'indirizzu',
+'aucune_breve' => '&Ugrave;n ci h&egrave; dispacciu &agrave; st\'indirizzu',
+'aucune_rubrique' => '&Ugrave;n ci h&egrave; r&ugrave;bbrica &agrave; st\'indirizzu',
+'autres' => 'Altri',
+'autres_breves' => 'Altri dispacci',
+'autres_groupes_mots_clefs' => 'Altri gruppi di parulle chjave',
+'autres_sites' => 'Alri siti',
+
+// B
+'bonjour' => 'Salute',
+'breves' => 'Dispacci',
+
+// C
+'commenter_site' => 'Cumment&agrave; stu situ',
+'copie_document_impossible' => 'Impuss&igrave;bule di cupi&agrave; u ducumentu',
+
+// D
+'date' => 'Data',
+'dernier_ajout' => '&Ugrave;ltimu aghjuntu',
+'dernieres_breves' => '&Ugrave;ltimi dispacci',
+'derniers_articles' => '&Ugrave;ltimi art&igrave;culi',
+'derniers_commentaires' => '&Ugrave;ltimi cummenti',
+'derniers_messages_forum' => '&Ugrave;ltimi messagi pubblicati in i fori',
+'documents_interdits_forum' => 'Ducumenti interdetti in u foru',
+
+// E
+'edition_mode_texte' => 'Edizione in modu testu di',
+'en_reponse' => 'Da risposta &agrave;&nbsp;:',
+'en_resume' => 'Da riassuntu',
+'envoyer_message' => 'Mand&agrave; u messagiu',
+'espace_prive' => 'Spaziu privatu',
+
+// F
+'formats_acceptes' => 'Furmati accettati : @formats@.',
+'forum' => 'Foru',
+
+// H
+'hierarchie_site' => 'Ierarchia di u situ',
+
+// J
+'jours' => 'ghjorni',
+
+// L
+'lien_connecter' => 'Cunnett&agrave; si',
+
+// M
+'meme_auteur' => 'Da listessu autore',
+'meme_rubrique' => 'In a listessa r&ugrave;bbrica',
+'memes_auteurs' => 'Da listessi autori',
+'message' => 'Messagiu',
+'messages_forum' => 'Messagi di foru',
+'messages_recents' => 'Messagi di fori i pi&ugrave; recenti',
+'mots_clefs' => 'Parulle chjave',
+'mots_clefs_meme_groupe' => 'Parulle chjave in u listessu gruppu',
+
+// N
+'navigation' => 'Navigazione',
+'nom' => 'Nome',
+'nouveautes' => 'E nuvit&agrave;',
+'nouveautes_web' => 'Nuvit&agrave; nant\'&agrave; u web',
+'nouveaux_articles' => 'Nuvelli art&igrave;culi',
+'nouvelles_breves' => 'Nuvelli dispacci',
+
+// P
+'page_precedente' => 'p&agrave;gina prima',
+'page_suivante' => 'p&agrave;gina dopu',
+'par_auteur' => 'da ',
+'participer_site' => 'Pudete puru particip&agrave; &agrave; a vita di stu situ &egrave; prupone i vostri art&igrave;culi iscrivendu vi qu&igrave; sottu. 
+	Riceverete s&ugrave;bbitu un email c&ugrave; i vostri c&ograve;dici d\'accessu &agrave; u spaziu privatu di u situ.',
+'plan_site' => 'Pianu di u situ',
+'popularite' => 'Pupularit&agrave;',
+'poster_message' => 'Pust&agrave; un messagiu',
+'proposer_site' => 'Pudete puru prupone un situ da aghjunghje in sta r&ugrave;bbrica&nbsp;:',
+
+// R
+'repondre_article' => 'Risponde &agrave; st\'art&igrave;culu',
+'repondre_breve' => 'Risponde &agrave; stu dispacciu',
+'repondre_message' => 'Risponde &agrave; stu messagiu',
+'resultats_recherche' => 'Risultati di a ricerca',
+'retour_debut_forums' => 'Ritornu &agrave; u principiu di i fori',
+'rubrique' => 'R&ugrave;bbrica',
+'rubriques' => 'R&ugrave;bbriche',
+
+// S
+'signatures_petition' => 'Zifre',
+'site_realise_avec_spip' => 'Site fattu c&ugrave; SPIP',
+'sites_web' => 'Siti Web',
+'sous_rubriques' => 'Sottur&ugrave;bbriche',
+'spam' => 'Spam',
+'suite' => 's&egrave;guita',
+'sur_web' => 'Nant\'&agrave; u Web',
+'syndiquer_rubrique' => 'Sindic&agrave; sta r&ugrave;bbrica',
+'syndiquer_site' => 'Sindic&agrave; u situ sanu',
+
+// T
+'texte_lettre_information' => 'Eccu a l&egrave;ttera d\'infurmazione di u situ',
+'texte_lettre_information_2' => 'Sta l&egrave;ttera face u censu di l\'art&igrave;culi &egrave; i dispacci dipoi',
+
+// V
+'ver_imprimer' => 'Versione da stamp&agrave;',
+'voir_en_ligne' => 'Vede in ligna',
+'voir_squelette' => 'vede u mudellu di sta p&agrave;gina'
+);
+
+?>

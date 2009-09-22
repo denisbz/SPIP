9c2b41b24053588653cfa81866c440302b4372ae
esj <esj@rezo.net>
2009-09-22 16:21:01 +0000

    Erreurs SQL en multi-serveurs, suite: [14492] corrigeait dans la branche dev pendant que [14484] et [14489] corrigeaient dans la branche stable. Avec ça, la stable est bien corrigée pour MySQL et PG, mais dans la dev il y a de toutes façons le chantier en cours de l'abstraction des erreurs SQL, c'est encore incomplet.
    git-svn-id: svn://trac.rezo.net/spip/branches/spip-2.0@14521 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/req/pg.php
+++ ecrire/req/pg.php
@@ -113,8 +113,8 @@ function spip_pg_trace_query($query, $serveur='')
 	$t = !isset($_GET['var_profile']) ? 0 : trace_query_start();
 	$r = spip_pg_query_simple($link, $query);
 
-	if ($e = spip_pg_errno())	// Log de l'erreur eventuelle
-		$e .= spip_pg_error($query); // et du fautif
+	if ($e = spip_pg_errno($serveur))	// Log de l'erreur eventuelle
+	  $e .= spip_pg_error($query, $serveur); // et du fautif
 	return $t ? trace_query_end($query, $t, $r, $e, $serveur) : $r;
 }
 
@@ -776,8 +776,8 @@ function spip_pg_insert($table, $champs, $valeurs, $desc=array(), $serveur='',$r
 		if ($r2 = pg_fetch_array($r, NULL, PGSQL_NUM))
 			return $r2[0];
 	}
-	$n = spip_pg_errno();
-	$m = spip_pg_error($q);
+	$n = spip_pg_errno($serveur);
+	$m = spip_pg_error($q, $serveur);
 	spip_log("$n $m $q '$r' '$r2'", 'pg'); // trace a minima
 	return -1;
 }
@@ -912,8 +912,8 @@ function spip_pg_replace($table, $values, $desc, $serveur='',$requeter=true) {
 	  $couples = spip_pg_query_simple($link, $q = "UPDATE $table SET $couples WHERE $where");
 #	  spip_log($q);
 	  if (!$couples) {
-	    $n = spip_pg_errno();
-	    $m = spip_pg_error($q);
+	    $n = spip_pg_errno($serveur);
+	    $m = spip_pg_error($q, $serveur);
 	  } else {
 	    $couples = pg_affected_rows($couples);
 	  }
@@ -924,8 +924,8 @@ function spip_pg_replace($table, $values, $desc, $serveur='',$requeter=true) {
 
 		$couples = spip_pg_query_simple($link, $q = "INSERT INTO $table (" . join(',',array_keys($values)) . ') VALUES (' .join(',', $values) . ")$ret");
 	    if (!$couples) {
-	      $n = spip_pg_errno();
-	      $m = spip_pg_error($q);
+		$n = spip_pg_errno($serveur);
+		$m = spip_pg_error($q, $serveur);
 	    } elseif ($ret) {
 	      $r = pg_fetch_array($couples, NULL, PGSQL_NUM);
 	      if ($r[0]) {
@@ -1034,14 +1034,19 @@ function spip_pg_in($val, $valeurs, $not='', $serveur) {
 
 // http://doc.spip.org/@spip_pg_error
 function spip_pg_error($query, $serveur='',$requeter=true) {
-	$s = str_replace('ERROR', 'errcode: 1000 ', pg_last_error());
+	$connexion = $GLOBALS['connexions'][$serveur ? $serveur : 0];
+	$link = $connexion['link'];
+	$s = $link ? pg_last_error($link) : pg_last_error();
+	$s = str_replace('ERROR', 'errcode: 1000 ', $s);
 	if ($s) spip_log("$s - $query", 'pg');
 	return $s;
 }
 
 // http://doc.spip.org/@spip_pg_errno
 function spip_pg_errno($serveur='',$requeter=true) {
-	$s = pg_last_error(); 
+	$connexion = $GLOBALS['connexions'][$serveur ? $serveur : 0];
+	$link = $connexion['link'];
+	$s = $link ? pg_last_error($link) : pg_last_error();
 	if ($s) spip_log("Erreur PG $s");
 	return $s ? 1 : 0;
 }

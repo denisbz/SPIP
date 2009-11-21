efbeffaee0d8b0824b782bf4712beac782f42c70
cedric <cedric@yterium.com>
2009-11-21 11:47:09 +0000

    Accelerer SPIP gestion du cache : plutot que d'y reprendre a 3 fois pour lire un fichier cache (file_exists, filemtime puis lire_contenu), multipliant d'autant les acces disques on optimise : - en stockant dans le fichier cache sa date de production, plutot que se reposer sur filemtime, la lecture du contenu etant de toute facon necessaire pour connaitre sa duree de validite - en lisant directement le contenu de ce cache, ce qui se traduira de toute facon par un retour null si le cache n'existe pas On passe de 3 acces a un seul sur le meme fichier (la lecutre du contenu ayant par ailleurs ete optimisee)
    git-svn-id: svn://trac.rezo.net/spip/branches/spip-2.0@14755 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/public/cacher.php
+++ ecrire/public/cacher.php
@@ -50,10 +50,7 @@ function generer_nom_fichier_cache($contexte, $page) {
 	// Sous-repertoires 0...9a..f ; ne pas prendre la base _DIR_CACHE
 	$rep = _DIR_CACHE;
 
-	if(!@file_exists($rep)) {
-		$rep = preg_replace(','._DIR_TMP.',', '', $rep);
-		$rep = sous_repertoire(_DIR_TMP, $rep, false,true);
-	}
+	$rep = sous_repertoire($rep, '', false,true);
 	$subdir = sous_repertoire($rep, substr($md_cache, 0, 1), true,true);
 	return $subdir.$cache;
 }
@@ -150,6 +147,11 @@ function creer_cache(&$page, &$chemin_cache) {
 		$chemin_cache .= '-'.$page['invalideurs']['session'];
 	}
 
+	// ajouter la date de production dans le cache lui meme
+	// (qui contient deja sa duree de validite)
+	$page['lastmodified'] = time();
+
+
 	// l'enregistrer, compresse ou non...
 	$ok = ecrire_fichier(_DIR_CACHE . $chemin_cache,
 		serialize(gzip_page($page)));
@@ -220,12 +222,9 @@ function public_cacher_dist($contexte, &$use_cache, &$chemin_cache, &$page, &$la
 	// quatre versions possibles : gzip ou non, session ou non
 	$chemin_cache = generer_nom_fichier_cache($contexte, $page);
 
-	if (@file_exists(_DIR_CACHE . ($f = $g = $chemin_cache))
-	OR (@file_exists(_DIR_CACHE . ($f = $chemin_cache.'-'.spip_session())))
-	) {
-		$lastmodified = @filemtime(_DIR_CACHE . $f);
-	} else
-		$lastmodified = 0;
+	$lastmodified = 0;
+	if (!lire_fichier(_DIR_CACHE . ($f = $g = $chemin_cache), $page))
+		lire_fichier(_DIR_CACHE . ($f = $chemin_cache.'-'.spip_session()), $page);
 
 	// HEAD : cas sans jamais de calcul pour raisons de performance
 	if ($_SERVER['REQUEST_METHOD'] == 'HEAD') {
@@ -261,11 +260,10 @@ function public_cacher_dist($contexte, &$use_cache, &$chemin_cache, &$page, &$la
 		$GLOBALS['delais'] = _DUREE_CACHE_DEFAUT;
 	}
 
-	// Lire le fichier cache et determiner sa validite
-	if ($lastmodified
-	AND lire_fichier(_DIR_CACHE . $f, $page)) {
-		$page = @unserialize($page);
-		$use_cache = cache_valide($page, $lastmodified);
+	// decoder le cache et determiner sa validite
+	if (strlen($page) AND
+		$page = @unserialize($page)) {
+		$use_cache = cache_valide($page, $page['lastmodified']);
 		if (!$use_cache) {
 			// $page est un cache utilisable
 			gunzip_page($page);
@@ -278,7 +276,7 @@ function public_cacher_dist($contexte, &$use_cache, &$chemin_cache, &$page, &$la
 
 	// Si pas valide mais pas de connexion a la base, le garder quand meme
 	if (!spip_connect()) {
-		if ($lastmodified)
+		if ($page)
 			$use_cache = 0;
 		else {
 			spip_log("Erreur base de donnees, impossible utiliser $chemin_cache");

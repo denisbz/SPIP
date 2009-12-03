8fa5b45eda3462d9b2e9feb8703ebfaa534b6ca0
esj <esj@rezo.net>
2009-12-03 17:33:59 +0000

    Autres ereg/split très gênants en 1.9.2 sur PHP 5.3
    git-svn-id: svn://trac.rezo.net/spip/branches/spip-1.9.2@14775 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/auteur_infos.php
+++ ecrire/inc/auteur_infos.php
@@ -53,7 +53,7 @@ function inc_auteur_infos_dist($auteur, $redirect) {
 
 // http://doc.spip.org/@afficher_erreurs_auteur
 function afficher_erreurs_auteur($echec) {
-	foreach (split('@@@',$echec) as $e)
+	foreach (explode('@@@',$echec) as $e)
 		$corps .= '<p>' . _T($e) . "</p>\n";
 
 	$corps = debut_cadre_relief('', true)

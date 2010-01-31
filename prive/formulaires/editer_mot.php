0cafd0a997eb7dc2a5a31b1ee1f4f3035da33733
cedric <cedric@yterium.com>
2010-01-31 11:47:59 +0000

    #1424 : avertir quand on essaye de creer un mot qui ressemble (meme titre au numero pres) a un mot deja existant dans le meme groupe + bugs sur le retour du formulaire de creation de mot + bug dans la css prive lorsqu'on met un label dans un message d'erreur
    git-svn-id: svn://trac.rezo.net/spip/branches/spip-2.1@15079 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- prive/formulaires/editer_mot.php
+++ prive/formulaires/editer_mot.php
@@ -47,6 +47,17 @@ function mots_edit_config($row)
 function formulaires_editer_mot_verifier_dist($id_mot='new', $id_groupe=0, $retour='', $ajouter_id_article=0, $table='', $table_id=0, $config_fonc='mots_edit_config', $row=array(), $hidden=''){
 
 	$erreurs = formulaires_editer_objet_verifier('mot',$id_mot,array('titre'));
+	// verifier qu'un mot du meme groupe n'existe pas avec le meme titre
+	// la comparaison accepte un numero absent ou different
+	// sinon avertir
+	if (!count($erreurs) AND !_request('confirm_titre_mot')){
+		if (sql_countsel("spip_mots", "id_groupe=".intval(_request('id_groupe'))
+						." AND titre REGEXP ".sql_quote("^([0-9]+[.] )?".preg_quote(supprimer_numero(_request('titre')))."$")
+						." AND id_mot<>".intval($id_mot)))
+			$erreurs['titre'] =
+						_L('Un mot existe deja avec ce titre dans ce groupe. &Ecirc;tes vous s&ucirc;r de vouloir cr&eacute;er le m&ecirc;meme ?')
+						." <input type='hidden' name='confirm_titre_mot' value='1' />";
+	}
 	return $erreurs;
 }
 

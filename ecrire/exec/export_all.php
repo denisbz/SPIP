41ba884b96f253ecef7a2e59090c89eaaf59e02b
cedric <cedric@yterium.com>
2010-02-01 22:17:08 +0000

    un peu d'unification dans base/dump et un peu d'explication
    git-svn-id: svn://trac.rezo.net/spip/branches/spip-2.1@15119 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/exec/export_all.php
+++ ecrire/exec/export_all.php
@@ -12,48 +12,20 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-define('_VERSION_ARCHIVE', '1.3');
+/**
+ * 
+ * On arrive ici depuis exec=admin_tech
+ * - le premier coup on initialise et on renvoie sur action=export_all avec un start
+ * - action=export_all ouvre le fichier et ecrit son entete, et renvoie ici
+ * - les autres coups on lance inc/export, qui remplit le dump et renvoie ici a chaque timeout
+ * - a chaque coup on relance inc/export
+ * - lorsque inc/export a fini, il renvoie vers action=export_all avec un end
+ * - action=export_all clos le fichier et affiche le resume
+ * 
+ */
 
-include_spip('base/serial');
-include_spip('base/auxiliaires');
 include_spip('inc/presentation');
-include_spip('public/interfaces'); // pour table_jointures
-
-// NB: Ce fichier peut ajouter des tables (old-style)
-// donc il faut l'inclure "en globals"
-if ($f = find_in_path('mes_fonctions.php')) {
-	global $dossier_squelettes;
-	@include_once ($f); 
-}
-
-if (@is_readable(_CACHE_PLUGINS_FCT)){
-	// chargement optimise precompile
-	include_once(_CACHE_PLUGINS_FCT);
-}
-
-// par defaut tout est exporte sauf les tables ci-dessous
-
-global $EXPORT_tables_noexport;
-
-$EXPORT_tables_noexport= array(
-	'spip_caches', // plugin invalideur
-	'spip_resultats', // resultats de recherche ... c'est un cache !
-	'spip_referers',
-	'spip_referers_articles',
-	'spip_visites',
-	'spip_visites_articles',
-	'spip_versions', // le dump des fragments n'est pas robuste
-	'spip_versions_fragments' // le dump des fragments n'est pas robuste
-	);
-
-if (!$GLOBALS['connect_toutes_rubriques']){
-	$EXPORT_tables_noexport[]='spip_messages';
-	$EXPORT_tables_noexport[]='spip_auteurs_messages';
-}
-
-//var_dump($EXPORT_tables_noexport);
-$EXPORT_tables_noexport = pipeline('lister_tables_noexport',$EXPORT_tables_noexport);
-
+include_spip('base/dump');
 
 // http://doc.spip.org/@exec_export_all_dist
 function exec_export_all_dist()
@@ -66,7 +38,7 @@ function exec_export_all_dist()
 	else {
 		$export = charger_fonction('export', 'inc');
 		$export($meta);
-	} 	
+	}
 }
 
 // L'en tete du fichier doit etre cree a partir de l'espace public
@@ -81,7 +53,7 @@ function exec_export_all_args($rub, $gz)
 	:  _request('nom_sauvegarde');
 	if (!preg_match(',^[\w_][\w_.]*$,', $nom)) $nom = 'dump';
 	$archive = $nom . '.xml' . $gz;
-	list($tables,) = export_all_list_tables();
+	list($tables,) = base_liste_table_for_dump($GLOBALS['EXPORT_tables_noexport']);
 	$clic =  _T('bouton_valider');
 	$plie = _T('install_tables_base');
 	$res = controle_tables_en_base('export', $tables, $rub);
@@ -110,69 +82,7 @@ function exec_export_all_args($rub, $gz)
 	return str_replace('</head>', $r . '</head>', $res);
 }
 
-// construction de la liste des tables pour le dump :
-// toutes les tables principales
-// + toutes les tables auxiliaires hors relations
-// + les tables relations dont les deux tables liees sont dans la liste
-
-// http://doc.spip.org/@export_all_list_tables
-function export_all_list_tables()
-{
-	$tables_for_dump = array();
-	$tables_pointees = array();
-	global $EXPORT_tables_noexport;
-	global $tables_principales;
-	global $tables_auxiliaires;
-	global $tables_jointures;
-
-// on construit un index des tables de liens
-// pour les ajouter SI les deux tables qu'ils connectent sont sauvegardees
-	$tables_for_link = array();
-	foreach($tables_jointures as $table => $liste_relations)
-		if (is_array($liste_relations))
-		{
-			$nom = $table;
-			if (!isset($tables_auxiliaires[$nom])&&!isset($tables_principales[$nom]))
-				$nom = "spip_$table";
-			if (isset($tables_auxiliaires[$nom])||isset($tables_principales[$nom])){
-				foreach($liste_relations as $link_table){
-					if (isset($tables_auxiliaires[$link_table])/*||isset($tables_principales[$link_table])*/){
-						$tables_for_link[$link_table][] = $nom;
-					}
-					else if (isset($tables_auxiliaires["spip_$link_table"])/*||isset($tables_principales["spip_$link_table"])*/){
-						$tables_for_link["spip_$link_table"][] = $nom;
-					}
-				}
-			}
-		}
-	
-	$liste_tables = array_merge(array_keys($tables_principales),array_keys($tables_auxiliaires));
-	foreach($liste_tables as $table){
-	  //		$name = preg_replace("{^spip_}","",$table);
-	  if (		!isset($tables_pointees[$table]) 
-	  		&&	!in_array($table,$EXPORT_tables_noexport) 
-	  		&&	!isset($tables_for_link[$table])){
-			$tables_for_dump[] = $table;
-			$tables_pointees[$table] = 1;
-		}
-	}
-	foreach ($tables_for_link as $link_table =>$liste){
-		$connecte = true;
-		foreach($liste as $connect_table)
-			if (!in_array($connect_table,$tables_for_dump))
-				$connecte = false;
-		if ($connecte)
-			# on ajoute les liaisons en premier
-			# si une restauration est interrompue,
-			# cela se verra mieux si il manque des objets
-			# que des liens
-			array_unshift($tables_for_dump,$link_table);
-	}
-	return array($tables_for_dump, $tables_for_link);
-}
-
 // Fabrique la liste a cocher des tables presentes
-
 function controle_tables_en_base($name, $check, $rub)
 {
 	$p = '/^' . $GLOBALS['table_prefix'] . '/';

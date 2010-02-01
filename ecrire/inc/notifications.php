094feb1d1fb52997339f797259cfc0c0de2cf7e5
cedric <cedric@yterium.com>
2010-02-01 23:49:02 +0000

    report de [15118] : notifications api
    git-svn-id: svn://trac.rezo.net/spip/spip@15123 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/notifications.php
+++ ecrire/inc/notifications.php
@@ -16,135 +16,215 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 // La fonction de notification de base, qui dispatche le travail
 // http://doc.spip.org/@inc_notifications_dist
 function inc_notifications_dist($quoi, $id=0, $options=array()) {
-	
+
 	// charger les fichiers qui veulent ajouter des definitions
 	// ou faire des trucs aussi dans le pipeline, ca fait deux api pour le prix d'une ...
 	pipeline('notifications',array('args'=>array('quoi'=>$quoi,'id'=>$id,'options'=>$options)));
 
-	if (function_exists($f = 'notifications_'.$quoi)
-	OR function_exists($f = $f.'_dist')) {
-		spip_log("$f($quoi,$id"
+	if ($notification = charger_fonction($quoi,'notifications',true)) {
+		spip_log("$notification($quoi,$id"
 			.($options?",".serialize($options):"")
-			.")");
-		$f($quoi, $id, $options);
+			.")",'notifications');
+		$notification($quoi, $id, $options);
 	}
 }
 
-// Fonction appelee par divers pipelines
-// http://doc.spip.org/@notifications_instituerarticle_dist
-function notifications_instituerarticle_dist($quoi, $id_article, $options) {
+/**
+ * Nettoyage des emails avant un envoi
+ * on passe par reference pour la perf
+ *
+ * les emails liste par $eclure seront exclus de la liste
+ *
+ * @param array $emails
+ * @param array $exclure
+ */
+function notifications_nettoyer_emails(&$emails, $exclure = array()){
+	// filtrer et unifier
+	$emails = array_unique(array_filter(array_map('email_valide',array_map('trim', $emails))));
+	if ($exclure AND count($exclure)){
+		// nettoyer les exclusions d'abord
+		notifications_nettoyer_emails($exclure);
+		// faire un diff
+		$emails = array_diff($emails,$exclure);
+	}
+}
 
-	// ne devrait jamais se produire
-	if ($options['statut'] == $options['statut_ancien']) {
-		spip_log("statut inchange");
+/**
+ * Envoyer un email de notification
+ * Le sujet peut etre vide, dans ce cas il reprendra la premiere ligne non vide du texte
+ *
+ * @param array/string $emails
+ * @param string $texte
+ * @param string $sujet
+ */
+function notifications_envoyer_mails($emails,$texte,$sujet=""){
+	// rien a faire si pas de texte !
+	if (!strlen($texte))
 		return;
+
+	// si on ne specifie qu'un email, le mettre dans un tableau
+	if (!is_array($emails))
+		$emails = explode(',',$emails);
+
+	notifications_nettoyer_emails($emails);
+
+	// si le sujet est vide, extraire la premiere ligne du corps
+	if (!strlen($sujet)){
+		// nettoyer un peu les retours chariots
+		$texte = str_replace("\r\n", "\r", $texte);
+		$texte = str_replace("\r", "\n", $texte);
+		// decouper
+		$texte = explode("\n",trim($texte));
+		// extraire la premiere ligne
+		$sujet = array_shift($texte);
+		$texte = trim(implode("\n",$texte));
 	}
 
-	include_spip('inc/texte');
-
-	if ($options['statut'] == 'publie')
-		notifier_publication_article($id_article);
-
-	if ($options['statut'] == 'prop' AND $options['statut_ancien'] != 'publie')
-		notifier_proposition_article($id_article);
-}
-
-
-// http://doc.spip.org/@extrait_article
-function extrait_article($row) {
-	include_spip('inc/texte');
-	
 	$envoyer_mail = charger_fonction('envoyer_mail','inc');
-	$titre = nettoyer_titre_email($row['titre']);
-	$id_article = $row['id_article'];
-	$chapo = $row['chapo'];
-	$texte = $row['texte'];
-	$date = $row['date'];
-	$statut = $row['statut'];
-
-	$les_auteurs = "";
-	$result_auteurs = sql_select("nom", "spip_auteurs AS A, spip_auteurs_articles AS L", "L.id_article=$id_article AND A.id_auteur=L.id_auteur");
-
-	while ($row = sql_fetch($result_auteurs)) {
-		if ($les_auteurs) $les_auteurs .= ', ';
-		$les_auteurs .= trim(supprimer_tags(typo($row['nom'])));
+	foreach($emails as $email){
+		// passer dans un pipeline qui permet un ajout eventuel
+		// (url de suivi des notifications par exemple)
+		$envoi = pipeline('notifications_envoyer_mails',array('email'=>$email,'sujet'=>$sujet,'texte'=>$texte));
+		$email = $envoi['email'];
+		$sujet = $envoi['sujet'];
+		$texte = $envoi['texte'];
+		if (function_exists('job_queue_add'))
+			job_queue_add('envoyer_mail', ">$email : $sujet", array($email, $sujet, $texte), 'inc/');
+		else
+			$envoyer_mail($email, $sujet, $texte);
 	}
 
-	$extrait = "** $titre **\n";
-	if ($les_auteurs) $extrait .= _T('info_les_auteurs_1', array('les_auteurs' => $les_auteurs));
-	if ($statut == 'publie') $extrait .= " "._T('date_fmt_nomjour_date', array('nomjour'=>nom_jour($date), 'date'=>affdate($date)));
-	$extrait .= "\n\n".textebrut(propre(couper("$chapo<p>$texte ", 700)))."\n\n";
-	return $extrait;
 }
 
+/**
+ * Notifier un evenement sur un article
+ * recupere le fond designe dans $modele,
+ * prend la premiere ligne comme sujet
+ * et l'interprete pour envoyer l'email
+ *
+ * @param int $id_article
+ * @param string $modele
+ */
+function email_notification_article($id_article, $modele) {
+	$envoyer_mail = charger_fonction('envoyer_mail','inc'); // pour nettoyer_titre_email
 
+	return recuperer_fond($modele,array('id_article'=>$id_article));
+}
+
+// Compatibilite, ne plus utiliser
 // http://doc.spip.org/@notifier_publication_article
 function notifier_publication_article($id_article) {
-
-	$envoyer_mail = charger_fonction('envoyer_mail','inc');
-	$adresse_suivi = $GLOBALS['meta']["adresse_suivi"];
-	$nom_site_spip = nettoyer_titre_email($GLOBALS['meta']["nom_site"]);
-	$suivi_edito = $GLOBALS['meta']["suivi_edito"];
-
-	if ($suivi_edito == "oui") {
-		$row = sql_fetsel("*", "spip_articles", "id_article = $id_article");
-		if ($row) {
-
-			$l = lang_select($row['lang']);
-
-			$url = generer_url_entite_absolue($id_article, 'article');
-
-			$titre = nettoyer_titre_email($row['titre']);
-
-			$sujet = _T('info_publie_1', array('nom_site_spip' => $nom_site_spip, 'titre' => $titre));
-			$courr = _T('info_publie_2')."\n\n";
-
-			$nom = $GLOBALS['visiteur_session']['nom'];
-			$nom = trim(supprimer_tags(typo($nom)));
-			$courr .= _T('info_publie_01', array('titre' => $titre, 'connect_nom' => $nom))
-				. "\n\n"
-				. extrait_article($row)
-				. "-> " . $url
-				. "\n";
-			$envoyer_mail($adresse_suivi, $sujet, $courr);
-			if ($l) lang_select();
-		}
+	if ($GLOBALS['meta']["suivi_edito"] == "oui") {
+		$adresse_suivi = $GLOBALS['meta']["adresse_suivi"];
+		$texte = email_notification_article($id_article, "notifications/article_publie");
+		notifications_envoyer_mails($adresse_suivi, $texte);
 	}
 }
 
+// Compatibilite, ne plus utiliser
 // http://doc.spip.org/@notifier_proposition_article
 function notifier_proposition_article($id_article) {
-	$envoyer_mail = charger_fonction('envoyer_mail','inc');
-	$adresse_suivi = $GLOBALS['meta']["adresse_suivi"];
-	$nom_site_spip = nettoyer_titre_email($GLOBALS['meta']["nom_site"]);
-	$suivi_edito = $GLOBALS['meta']["suivi_edito"];
-
-	if ($suivi_edito == "oui") {
-		$row = sql_fetsel("*", "spip_articles", "id_article = $id_article");
-		if ($row) {
-
-			if ($l = $row['lang']) $l = lang_select($l);
-
-			$titre = nettoyer_titre_email($row['titre']);
-
-			$sujet = _T('info_propose_1', array('nom_site_spip' => $nom_site_spip, 'titre' => $titre));
-			$envoyer_mail($adresse_suivi,
-				$sujet,
-				_T('info_propose_2')
-				."\n\n" 
-				. _T('info_propose_3', array('titre' => $titre))
-				."\n" 
-				. _T('info_propose_4')
-				."\n" 
-				. _T('info_propose_5')
-				."\n" 
-				. generer_url_ecrire("articles", "id_article=$id_article", true)
-				. "\n\n\n" 
-				. extrait_article($row)
-			);
-			if ($l) lang_select();
-		}
+	if ($GLOBALS['meta']["suivi_edito"] == "oui") {
+		$adresse_suivi = $GLOBALS['meta']["adresse_suivi"];
+		$texte = email_notification_article($id_article, "notifications/article_propose");
+		notifications_envoyer_mails($adresse_suivi, $texte);
 	}
 }
+
+/**
+ * Construitre l'email personalise de notification d'un forum
+ *
+ * @param array $t
+ * @param string $email
+ * @return string
+ */
+function email_notification_forum ($t, $email) {
+	static $contexte = array();
+
+	if(!isset($contexte[$t['id_forum']])){
+		$url = '';
+		$id_forum = $t['id_forum'];
+
+		if ($t['statut'] == 'prive') # forum prive
+		{
+			if ($t['id_article'])
+				$url = generer_url_ecrire('articles', 'id_article='.$t['id_article']).'#id'.$id_forum;
+			else if ($t['id_breve'])
+				$url = generer_url_ecrire('breves_voir', 'id_breve='.$t['id_breve']).'#id'.$id_forum;
+			else if ($t['id_syndic'])
+				$url = generer_url_ecrire('sites', 'id_syndic='.$t['id_syndic']).'#id'.$id_forum;
+		}
+		else if ($t['statut'] == 'privrac') # forum general
+		{
+			$url = generer_url_ecrire('forum').'#id'.$id_forum;
+		}
+		else if ($t['statut'] == 'privadm') # forum des admins
+		{
+			$url = generer_url_ecrire('forum_admin').'#id'.$id_forum;
+		}
+		else if ($t['statut'] == 'publie') # forum publie
+		{
+			$url = generer_url_entite($id_forum, 'forum');
+		}
+		else #  forum modere, spam, poubelle direct ....
+		{
+			$url = generer_url_ecrire('controle_forum', "debut_id_forum=".$id_forum);
+		}
+
+		if (!$url) {
+			spip_log("forum $id_forum sans referent",'notifications');
+			$url = './';
+		}
+		if ($t['id_article']) {
+			$titre = sql_getfetsel("titre", "spip_articles", "id_article=".sql_quote($t['id_article']));
+		}
+		if ($t['id_message']) {
+			$titre = sql_getfetsel("titre", "spip_messages", "id_message=".sql_quote($t['id_message']));
+		}
+
+		$t['titre_source'] = $titre;
+		$t['url'] = $url;
+
+		// detecter les url des liens du forum
+		// pour la moderation (permet de reperer les SPAMS avec des liens caches)
+		$links = array();
+		foreach ($t as $champ)
+			$links = $links + extraire_balises($champ,'a');
+		$links = extraire_attribut($links,'href');
+		$links = implode("\n",$links);
+		$t['liens'] = $links;
+
+		$contexte[$t['id_forum']] = $t;
+	}
+
+	$t = $contexte[$t['id_forum']];
+		// Rechercher eventuellement la langue du destinataire
+	if (NULL !== ($l = sql_getfetsel('lang', 'spip_auteurs', "email=" . sql_quote($email))))
+		$l = lang_select($l);
+
+	$parauteur = (strlen($t['auteur']) <= 2) ? '' :
+		(" " ._T('forum_par_auteur', array(
+			'auteur' => $t['auteur'])
+		) .
+		 ($t['email_auteur'] ? ' <' . $t['email_auteur'] . '>' : ''));
+
+	$titre = textebrut(typo($t['titre_source']));
+	$forum_poste_par = ($t['id_article']
+		? _T('forum_poste_par', array(
+			'parauteur' => $parauteur, 'titre' => $titre))
+		: $parauteur . ' (' . $titre . ')');
+
+	$t['par_auteur'] = $forum_poste_par;
+
+	$envoyer_mail = charger_fonction('envoyer_mail','inc'); // pour nettoyer_titre_email
+	$corps = recuperer_fond("notifications/forum_poste",$t);
+
+	if ($l)
+		lang_select();
+
+	return $corps;
+}
+
+
 
 ?>

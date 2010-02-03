034f19fdb9af11b3d2edd9171702d75699b86856
cedric <cedric@yterium.com>
2010-02-03 00:13:50 +0000

    passage en beta, mise a jour de spip_version_code et une constante qui ne servait plus
    git-svn-id: svn://trac.rezo.net/spip/branches/spip-2.1@15134 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc_version.php
+++ ecrire/inc_version.php
@@ -395,10 +395,10 @@ $liste_des_authentifications = array(
 // numero de branche, utilise par les plugins 
 // pour specifier les versions de SPIP necessaire
 // il faut s'en tenir a un nombre de decimales fixe ex : 2.0.0, 2.0.0-dev, 2.0.0-beta, 2.0.0-beta2 
-$spip_version_branche = "2.1.0-dev";
+$spip_version_branche = "2.1.0-beta";
 // version des signatures de fonctions PHP
 // (= numero SVN de leur derniere modif cassant la compatibilite et/ou necessitant un recalcul des squelettes)
-$spip_version_code = 13545;
+$spip_version_code = 15133;
 // version de la base SQL (= numero SVN de sa derniere modif, a verifier dans le fichier ecrire/maj/sv10000.php) 
 $spip_version_base = 14559;
 
@@ -428,7 +428,6 @@ if (_FILE_OPTIONS) include_once _FILE_OPTIONS;
 
 // Masquer les warning
 define('SPIP_ERREUR_REPORT',E_ALL ^ E_NOTICE);
-define('SPIP_ERREUR_REPORT_INCLUDE_PLUGINS',0);
 error_reporting(SPIP_ERREUR_REPORT);
 
 // Initialisations critiques non surchargeables par les plugins

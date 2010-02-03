faa104563347f884b1c7f5d7f3982513ac879202
cedric <cedric@yterium.com>
2010-02-03 21:23:40 +0000

    retablir l'usabilite de var_mode=debug dans les 3 modes possibles (erreur sur un skel, var_mode=debug, var_profil=oui) et grosse amelioration ergonomique du var_mode=inclure
    git-svn-id: svn://trac.rezo.net/spip/branches/spip-2.1@15142 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/utils.php
+++ ecrire/inc/utils.php
@@ -1647,6 +1647,7 @@ function erreur_squelette($message='', $lieu='') {
  */
 // http://doc.spip.org/@recuperer_fond
 function recuperer_fond($fond, $contexte=array(), $options = array(), $connect='') {
+	static $var_inclure=false;
 	include_spip('public/assembler');
 	// assurer la compat avec l'ancienne syntaxe
 	// (trim etait le 3eme argument, par defaut a true)
@@ -1689,8 +1690,13 @@ function recuperer_fond($fond, $contexte=array(), $options = array(), $connect='
 			$page['texte'] = encoder_contexte_ajax(array_merge($contexte,array('fond'=>$f)),'',$page['texte']);
 		}
 
-		if ($GLOBALS['var_inclure'])
-			$page['texte'] = "<fieldset class='blocs'><legend>".$page['sourcefile']."</legend>".$page['texte']."</fieldset>";
+		if ($GLOBALS['var_inclure']){
+			$page['texte'] = 
+				"<div class='inclure_blocs'><h6>".$page['sourcefile']."</h6>".$page['texte']."</div>"
+				. ($var_inclure?"":"<script type='text/javascript'>jQuery(function(){jQuery('.inclure_blocs > h6:first-child').hover(function(){jQuery(this).parent().addClass('hover')},function(){jQuery(this).parent().removeClass('hover')})});</script>");
+			$var_inclure = true;
+		}
+
 		$page = pipeline('recuperer_fond',array(
 			'args'=>array('fond'=>$fond,'contexte'=>$contexte,'options'=>$options,'connect'=>$connect),
 			'data'=>$page

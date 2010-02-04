58fceb327180364f37a7f86a24b3090af5cf40a5
cedric <cedric@yterium.com>
2010-02-04 08:36:42 +0000

    oups, mode b_b
    git-svn-id: svn://trac.rezo.net/spip/branches/spip-2.1@15151 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/exec/charger_plugin.php
+++ ecrire/exec/charger_plugin.php
@@ -61,7 +61,7 @@ function exec_charger_plugin_dist($retour='') {
 		echo pipeline('affiche_milieu',
 			array(
 			'args'=>array('exec'=>'charger_plugin'),
-			'data'=>afficher_librairies()
+			'data'=>''
 			)
 		);
 

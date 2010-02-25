e9b48de516af089a32564b1250d3ca866dcbc202
esj <esj@rezo.net>
2010-02-25 12:53:24 +0000

    Report de [15318] et [15319]: scories dans les boutons d'aide.
    git-svn-id: svn://trac.rezo.net/spip/spip@15326 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/exec/aide_index.php
+++ ecrire/exec/aide_index.php
@@ -172,7 +172,7 @@ function help_body($aide) {
 					. '_'));
 		return "<div align='center'>" .
 			_HELP_PANNEAU .
-			preg_replace(",<a ,i", "<a class='target_blank", $c) .
+			preg_replace(",<a ,i", "<a class='target_blank' ", $c) .
 			'</div>';
 	} elseif ($aide == 'spip') {
 		return "<table border='0' width='100%' height='60%'>
@@ -290,7 +290,7 @@ function help_frame_body($titre, $aide, $html, $lang_aide='')
 		$dir .
 		" lang='$lang'>".
 		help_body($aide) .
-	  	$html .
+		($aide ? $html : '').
 		'</body>';
 }
 

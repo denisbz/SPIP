95be33afbd642a8134f5e5b3c9f2fa41b6e0ddae
cedric <cedric@yterium.com>
2010-03-05 13:46:25 +0000

    report de [15274]
    git-svn-id: svn://trac.rezo.net/spip/spip@15387 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/action/tester_taille.php
+++ ecrire/action/tester_taille.php
@@ -13,6 +13,15 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 include_spip('inc/headers');
 
+function action_tester_taille_error_handler($output)
+{
+	// on est ici, donc echec lors de la creation de l'image
+	if ($error = error_get_last()){
+		return redirige_formulaire($GLOBALS['redirect']);
+	}
+	return $output;
+}
+
 
 // Tester nos capacites a creer des images avec GD2 (taille memoire)
 // http://doc.spip.org/@action_tester_taille_dist
@@ -23,64 +32,59 @@ function action_tester_taille_dist() {
 	$max_size = isset($GLOBALS['meta']['max_taille_vignettes'])?$GLOBALS['meta']['max_taille_vignettes']:0;
 	$max_size_echec = isset($GLOBALS['meta']['max_taille_vignettes_echec'])?$GLOBALS['meta']['max_taille_vignettes_echec']:0;
 	$max_size_test = isset($GLOBALS['meta']['max_taille_vignettes_test'])?$GLOBALS['meta']['max_taille_vignettes_test']:0;
-	$taille = intval(_request('arg'));
-	$file = chemin_image('jauge-vert.gif');
-	$file_size = 94;
-	
-	if (($s = $taille*$taille)>$max_size){
-		if (!$max_size_echec OR $s < $max_size_echec) {
-			include_spip('inc/filtres');
-			// des inclusions representatives d'un hit prive et/ou public pour la conso memoire
-			include_spip('public/assembler');
-			include_spip('public/balises');
-			include_spip('public/boucles');
-			include_spip('public/cacher');
-			include_spip('public/compiler');
-			include_spip('public/composer');
-			include_spip('public/criteres');
-			include_spip('public/interfaces');
-			include_spip('public/parametrer');
-			include_spip('public/phraser_html');
-			include_spip('public/references');
 
-			include_spip('inc/presentation');
-			include_spip('inc/charsets');
-			include_spip('inc/documents');
-			$dummy = propre("<doc1>");
+	$taille = _request('arg');
+	$taille = explode('-',$taille);
 
-			$image_source = chemin_image("test.png");
-			$row = sql_fetsel("valeur", "spip_meta", "nom='max_taille_vignettes_test'");
-			if ($row) $max_size_test = $row['valeur'];
-			if (!$max_size_test OR $max_size_test>$s)
-				ecrire_meta('max_taille_vignettes_test',$s,'non');
-			$result = filtrer('image_recadre',$image_source,$taille,$taille);
-			// on est ici, donc pas de plantage
-			if ($max_size_test>$s)
-				ecrire_meta('max_taille_vignettes_test',$max_size_test,'non');
-			else 
-				effacer_meta('max_taille_vignettes_test');
-			$src = extraire_attribut($result,'src');
-		}
-		// et maintenant envoyer la vignette de tests
-		if ($src) {
-			ecrire_meta('max_taille_vignettes',$taille*$taille,'non');
-			spip_unlink($src);
-		}
-		else {
-			if (!$max_size_echec OR $s < $max_size_echec)
-				ecrire_meta('max_taille_vignettes_echec',$taille*$taille,'non');
-			# image echec
-			$file = chemin_image('jauge-rouge.gif');
-			$file_size = 84;
-		}
+	$GLOBALS['taille_max'] = end($taille);
+	$GLOBALS['taille_min'] = 0;
+	if (count($taille)>1)
+		$GLOBALS['taille_min'] = reset($taille);
+
+	// si l'intervalle est assez petit, on garde la valeur min
+	if ($GLOBALS['taille_max']*$GLOBALS['taille_max']-$GLOBALS['taille_min']*$GLOBALS['taille_min']<50000){
+		ecrire_meta('max_taille_vignettes',$t=$GLOBALS['taille_min']*$GLOBALS['taille_min'],'non');
+		echo round($t/1000000,3).' Mpx';
+		die();
 	}
-	
-	header('Content-Type: image/gif');
-	header("Content-Length: $file_size");
-	header('Cache-Control: max-age=20');
-	header('Pragma: public');
-	readfile($file);
-	exit;
+
+	$taille = $GLOBALS['taille_test'] = round(($GLOBALS['taille_max']+$GLOBALS['taille_min'])/2);
+
+	include_spip('inc/filtres');
+	// des inclusions representatives d'un hit prive et/ou public pour la conso memoire
+	include_spip('public/assembler');
+	include_spip('public/balises');
+	include_spip('public/boucles');
+	include_spip('public/cacher');
+	include_spip('public/compiler');
+	include_spip('public/composer');
+	include_spip('public/criteres');
+	include_spip('public/interfaces');
+	include_spip('public/parametrer');
+	include_spip('public/phraser_html');
+	include_spip('public/references');
+
+	include_spip('inc/presentation');
+	include_spip('inc/charsets');
+	include_spip('inc/documents');
+	include_spip('inc/header');
+	$dummy = propre("<doc1>");
+
+	$i = _request('i')+1;
+	$image_source = chemin_image("test.png");
+	$GLOBALS['redirect'] = generer_url_action("tester_taille", "i=$i&arg=".$GLOBALS['taille_min']."-".$GLOBALS['taille_test']);
+
+	ob_start('action_tester_taille_error_handler');
+	$result = filtrer('image_recadre',$image_source,$taille,$taille);
+	$GLOBALS['redirect'] = generer_url_action("tester_taille", "i=$i&arg=$taille-".$GLOBALS['taille_max']);
+	// si la valeur intermediaire a reussi, on teste la valeur maxi qui est peut etre sous estimee
+	$taille = $GLOBALS['taille_max'];
+	$result = filtrer('image_recadre',$image_source,$taille,$taille);
+	$GLOBALS['redirect'] = generer_url_action("tester_taille", "i=$i&arg=$taille-".$GLOBALS['taille_max']);
+	ob_end_clean();
+
+	// on est ici, donc pas de plantage
+	echo redirige_formulaire($GLOBALS['redirect']);
 }
 
 ?>

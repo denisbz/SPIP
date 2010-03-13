0f006eafa295c144290225d8149f1791ed54afcd
esj <esj@rezo.net>
2010-03-13 09:48:47 +0000

    Les fichiers .htaccess doivent eux aussi être créés avec les droits indiqués par _SPIP_CHMOD
    git-svn-id: svn://trac.rezo.net/spip/branches/spip-2.1@15463 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/acces.php
+++ ecrire/inc/acces.php
@@ -209,6 +209,7 @@ function verifier_htaccess($rep, $force=false) {
 	if ($ht = @fopen($htaccess, "w")) {
 		fputs($ht, "deny from all\n");
 		fclose($ht);
+		@chmod($htaccess, _SPIP_CHMOD & 0666);
 		$t = rtrim($rep,"/") . "/.ok";
 		if ($ht = @fopen($t, "w")) {
 			@fclose($ht);

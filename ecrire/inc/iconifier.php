8851e74ed99615aa2fd770c26e40b5ff321b6e7c
cedric <cedric@yterium.com>
2010-04-14 08:51:45 +0000

    report de [15578]
    git-svn-id: svn://trac.rezo.net/spip/spip@15613 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/iconifier.php
+++ ecrire/inc/iconifier.php
@@ -13,6 +13,15 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 include_spip('inc/actions');
+global $logo_libelles;
+$logo_libelles['id_article'] = _T('logo_article').aide ("logoart");
+$logo_libelles['id_auteur'] = _T('logo_auteur').aide ("logoart");
+$logo_libelles['id_breve'] = _T('logo_breve').aide ("breveslogo");
+$logo_libelles['id_syndic'] = _T('logo_site')." ".aide ("rublogo");
+$logo_libelles['id_mot'] = _T('logo_mot_cle').aide("breveslogo");
+$logo_libelles['id_rubrique'] = _T('logo_rubrique')." ".aide ("rublogo");
+$logo_libelles['id_racine'] = _T('logo_standard_rubrique')." ".aide ("rublogo");
+
 
 // http://doc.spip.org/@inc_iconifier_dist
 function inc_iconifier_dist($id_objet, $id,  $script, $visible=false, $flag_modif=true) {

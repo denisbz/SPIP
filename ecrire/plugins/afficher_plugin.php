310b69b36dd80e435730b9b75cd38d646ba8a2b4
esj <esj@rezo.net>
2010-05-13 05:46:00 +0000

    Donner à afficher_plugin un argument optionnel supplémentaire, permettant de remplacer CFG par un autre outil de configuration.
    git-svn-id: svn://trac.rezo.net/spip/branches/spip-2.1@15680 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/plugins/afficher_plugin.php
+++ ecrire/plugins/afficher_plugin.php
@@ -15,98 +15,97 @@ include_spip('inc/charsets');
 include_spip('inc/texte');
 
 // http://doc.spip.org/@ligne_plug
-function plugins_afficher_plugin_dist($url_page, $plug_file, $actif, $expose=false, $class_li="item", $dir_plugins=_DIR_PLUGINS){
-	static $id_input=0;
+function plugins_afficher_plugin_dist($url_page, $plug_file, $actif, $expose=false, $class_li="item", $dir_plugins=_DIR_PLUGINS, $cfg=''){
+
+	static $id_input = 0;
 	static $versions = array();
 
-	$erreur = false;
-	$s = "";
-
-	$force_reload = (_request('var_mode')=='recalcul'?true:false);
-
+	$cfg = !$actif ? $cfg : plugin_bouton_cfg();
+	$force_reload = (_request('var_mode')=='recalcul');
 	$get_infos = charger_fonction('get_infos','plugins');
 	$info = $get_infos($plug_file, $force_reload, $dir_plugins);
+	$prefix = $info['prefix'];
+	$erreur = (!isset($info['erreur']) ? ''
+	: ("<div class='erreur'>" . join('<br >', $info['erreur']) . "</div>"));
 
-	// numerotons les occurences d'un meme prefix
-	$versions[$info['prefix']] = isset($versions[$info['prefix']]) ? $versions[$info['prefix']] + 1 : '';
-	$id = $info['prefix'] . $versions[$info['prefix']];
+	// numerotons les occurrences d'un meme prefix
+	$versions[$prefix] = $id = isset($versions[$prefix]) ? $versions[$prefix] + 1 : '';
 
-	$class = $class_li;
-	$class .= $actif?" actif":"";
-	$class .= $expose?" on":"";
-	$erreur = isset($info['erreur']);
-	if ($erreur)
-		$class .= " erreur";
-	$s .= "<li id='$id' class='$class'>";
+	$class_li .= ($actif?" actif":"") . ($expose?" on":"") . (isset($info['erreur']) ? " erreur" : '');
 
+	return "<li id='$prefix$id' class='$class_li'>"
+	. (($erreur OR $dir_plugins===_DIR_EXTENSIONS)
+	   ? '': plugin_checkbox(++$id_input, $plug_file, $actif))
+	.  plugin_resume($info, $dir_plugins, $plug_file, $url_page)
+	. $cfg
+	. $erreur
+	. (($dir_plugins!==_DIR_EXTENSIONS AND plugin_est_installe($plug_file))
+	    ? plugin_desintalle($plug_file) : '')
+	. "<div class='details'>" // pour l'ajax de exec/info_plugin
+	. (!$expose ? '' : affiche_bloc_plugin($plug_file, $info))
+	. "</div>"
+	."</li>";
+}
 
-	// checkbox pour activer ou desactiver
-	// si ce n'est pas une extension
-	if (!$erreur AND $dir_plugins!==_DIR_EXTENSIONS){
-		$name = 's' . substr(md5("statusplug_$plug_file"),0,16);
-		$id_input++;
-		$check = "\n<input type='checkbox' name='$name' id='label_$id_input' value='O'";
-		$check .= $actif?" checked='checked'":"";
-		$check .= " class='checkbox' />";
-		$check .= "\n<label for='label_$id_input'>"._T('activer_plugin')."</label>";
-		$s .= "<div class='check'>$check</div>";
+function plugin_bouton_cfg()
+{
+	if  (!defined('_DIR_PLUGIN_CFG')) {
+		if (include_spip('inc/cfg')) // test CFG version >= 1.0.5
+			if ($cfg = icone_lien_cfg($dir_plugins.$plug_file))
+				$cfg = "<div class='cfg_link'>$cfg</div>";
 	}
+}
 
-	// Cartouche Resume
-	$s .= "<div class='resume'>";
+// checkbox pour activer ou desactiver
+// si ce n'est pas une extension
 
+function plugin_checkbox($id_input, $file, $actif)
+{
+	$name = substr(md5($file),0,16);
+
+	return "<div class='check'>\n"
+	. "<input type='checkbox' name='s$name' id='label_$id_input'"
+	. ($actif?" checked='checked'":"")
+	. " class='checkbox'  value='O' />"
+	. "\n<label for='label_$id_input'>"._T('activer_plugin')."</label>"
+	. "</div>";
+}
+
+function plugin_resume($info, $dir_plugins, $plug_file, $url_page)
+{
 	$desc = plugin_propre($info['description']);
 	if (($p=strpos($desc, "<br />"))!==FALSE)
 		$desc = substr($desc, 0,$p);
 	$url_stat = parametre_url($url_page, "plugin",$dir_plugins.$plug_file);
 
+	// Cartouche Resume
+	$s = "<div class='resume'>";
 	$s .= "<h3 class='nom'><a href='$url_stat' rel='info'>".typo($info['nom'])."</a></h3>";
 	$s .= " <span class='version'>".$info['version']."</span>";
 	$s .= " <span class='etat'> - ".plugin_etat_en_clair($info['etat'])."</span>";
 	$s .= "<div class='short'>".couper($desc,70)."</div>";
+
 	if (isset($info['icon']) and $info['icon']) {
 		include_spip("inc/filtres_images_mini");
 		$s.= "<div class='icon'><a href='$url_stat' rel='info'>".inserer_attribut(image_reduire($dir_plugins.$plug_file.'/'.trim($info['icon']), 32),'alt','')."</a></div>";
 	}
 	$s .= "</div>";
-
-	// plug pour CFG
-	if ($actif
-	AND defined('_DIR_PLUGIN_CFG')) {
-		if (include_spip('inc/cfg') // test CFG version >= 1.0.5
-		AND $i = icone_lien_cfg($dir_plugins.$plug_file))
-			$s .= '<div class="cfg_link">'.$i.'</div>';
-	}
-
-	if ($erreur){
-		$s .=  "<div class='erreur'>";
-		foreach($info['erreur'] as $err)
-			$s .= "$err <br/>";
-		$s .=  "</div>";
-	}
-
-	// bouton de desinstallation
-	if ($dir_plugins!==_DIR_EXTENSIONS AND plugin_est_installe($plug_file)){
-		$action = redirige_action_auteur('desinstaller_plugin',$plug_file,'admin_plugin');
-		$s .= "<div class='actions'>[".
-		"<a href='$action'
-		onclick='return confirm(\""._T('bouton_desinstaller')
-		." ".basename($plug_file)." ?\\n"._T('info_desinstaller_plugin')."\")'>"
-		._T('bouton_desinstaller')
-		."</a>]</div>"
-		;
-	}
-
-	// afficher les details d'un plug en secours ; la div sert pour l'ajax
-	$s .= "<div class='details'>";
-	if ($expose)
-		$s .= affiche_bloc_plugin($plug_file, $info);
-	$s .= "</div>";
-
-	$s .= "</li>";
 	return $s;
 }
 
+function plugin_desintalle($plug_file){
+
+	$action = redirige_action_auteur('desinstaller_plugin',$plug_file,'admin_plugin');
+	$text = _T('bouton_desinstaller');
+	$text2 = _T('info_desinstaller_plugin');
+	$file = basename($plug_file);
+
+	return "<div class='actions'>[".
+		"<a href='$action'
+		onclick='return confirm(\"$text $file ?\\n$text2\")'>"
+		. $text
+		. "</a>]</div>"	;
+}
 
 function plugin_etat_en_clair($etat){
 	if (!in_array($etat,array('stable','test','experimental')))

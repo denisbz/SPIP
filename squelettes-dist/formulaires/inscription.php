abae13ae8cbf3a76f4bb315e65a2885b933ee265
brunobergot <brunobergot@gmail.com>
2010-05-28 12:02:51 +0000

    report de [15713] sur la branche dev
    git-svn-id: svn://trac.rezo.net/spip/spip@15714 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- squelettes-dist/formulaires/inscription.php
+++ squelettes-dist/formulaires/inscription.php
@@ -102,6 +102,12 @@ function formulaires_inscription_traiter_dist($mode, $focus, $id=0) {
 		list($sujet,$msg,$from,$head) = $f($desc, $nom, $mode, $id);
 		if (!$envoyer_mail($mail_complet, $sujet, $msg, $from, $head))
 			$desc = _T('form_forum_probleme_mail');
+		// Notifications
+		if ($notifications = charger_fonction('notifications', 'inc')) {
+			$notifications('inscription', $desc['id_auteur'],
+				array('nom' => $desc['nom'], 'email' => $desc['email'])
+			);
+		}
 	}
 
 	return array('message_ok'=>is_string($desc) ? $desc : _T('form_forum_identifiant_mail'));

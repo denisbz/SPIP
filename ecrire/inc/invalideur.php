92b630a5b6ad6ff28ccd152d34fb2480d2a29f60
cedric <cedric@yterium.com>
2010-07-22 09:27:38 +0000

    consecutivement a [15851] il n'y a plus de sous repertoire de session on laisse la purge recursive a true pour faire le menage, mais cela ne doit plus impacter le fonctionnement (plus d'erreur d'ecriture dans un sous rep inexistant)
    git-svn-id: svn://trac.rezo.net/spip/branches/spip-2.1@15853 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/invalideur.php
+++ ecrire/inc/invalideur.php
@@ -159,7 +159,7 @@ function appliquer_quota_cache() {
 				array(
 					'atime' => time() - _AGE_CACHE_ATIME,
 					'limit' => $trop,
-					'subdir' => true // supprimer les sous repertoire de session aussi
+					'subdir' => true // supprimer les vieux sous repertoire de session (avant [15851])
 				)
 			);
 			spip_log("$dir : $n/$trop caches supprimes [taille moyenne $taille]","invalideur");

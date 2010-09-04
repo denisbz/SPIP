afdc2902e7c31f34e8e1c6860047c94bfe6d4063
cedric <cedric@yterium.com>
2010-09-04 10:04:13 +0000

    report du js ameliore avec tempo a l'ouverture sur les menu deroulant et desactivation des styles sur les :hover pour que cela fonctionne
    git-svn-id: svn://trac.rezo.net/spip/spip@16029 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- prive/javascript/gadgets.js
+++ prive/javascript/gadgets.js
@@ -5,7 +5,9 @@ function init_gadgets(url_toutsite,url_navrapide,url_agenda,html_messagerie){
 						url: url_toutsite,
 						success: function(c){
 							jQuery('#boutonbandeautoutsite').siblings('ul').remove();
-							jQuery('#boutonbandeautoutsite').after(c);
+							jQuery('#boutonbandeautoutsite')
+							  .after(c)
+								.parent().find('li').menuFocus();
 						}
 					});
 	});
@@ -15,6 +17,21 @@ function focus_zone(selecteur){
 	return false;
 }
 jQuery(document).ready(function(){
+	var is_open = 0;
+	jQuery.fn.menuItemOpen = function(){
+		jQuery(this)
+			.addClass('actif')
+			.parents('li').addClass('actif');
+		jQuery(this).siblings('li').removeClass('actif_tempo');
+		is_open = true;
+		return this;
+	}
+	jQuery.fn.menuItemClose = function(){
+		jQuery(this)
+			.removeClass('actif_tempo');
+		is_open = (jQuery(this).parents('ul').eq(-1).find('li.actif').length>0);
+		return this;
+	}
 	// deplier le menu au focus clavier,
 	// enlever ce depliement si passage a la souris,
 	// delai de fermeture.
@@ -22,23 +39,33 @@ jQuery(document).ready(function(){
 		jQuery(this)
 		// le replier si un hover de souris sur un autre onglet,
 		// timer sur la fermeture des onglets pour ne pas que ca aille trop vite
+		// timer sur l'ouverture des onglets pour ne tolerer les derapages
 		.hover(
 			function(){
-				if (this.timer)
-						clearTimeout(this.timer);
-				this.timer = null;
-				/*jQuery(this).parents('ul').find('>li.actif').removeClass('actif');*/
-				jQuery(this)
-					.addClass('actif')
-					.parents('li').addClass('actif');
-				jQuery(this).siblings('li').removeClass('actif_tempo');
+				if (this.timerout)
+					clearTimeout(this.timerout);
+				this.timerout = null;
+				this.timerin = null;
+				if (is_open)
+					jQuery(this).menuItemOpen();
+				else {
+					var me = jQuery(this);
+					this.timerin= setTimeout(function(){
+						me.menuItemOpen(null);
+					}, 200);
+				}
 			}
 			,
 			function(){
-				var me = jQuery(this).removeClass('actif').addClass('actif_tempo');
-				this.timer = setTimeout(function(){
-					me.removeClass('actif_tempo');
-				}, 400);
+				if (this.timerin)
+						clearTimeout(this.timerin);
+				this.timerin = null;
+				if (is_open){
+					var me = jQuery(this).removeClass('actif').addClass('actif_tempo');
+					this.timerout = setTimeout(function(){
+						me.menuItemClose();
+					}, 400);
+				}
 			}
 		)
 		// navigation au clavier :
@@ -53,7 +80,7 @@ jQuery(document).ready(function(){
 		});
 		return this;
 	}
-	
+
 	// Controler la position verticale des sous-menus
 	// pour l'instant, effectuer a chaque hover, en cas de changement de taille d'affichage par exemple
 	jQuery('#bando_navigation').hover(function(){
@@ -62,7 +89,7 @@ jQuery(document).ready(function(){
 			+  parseInt(jQuery('#bando_navigation').css("padding-bottom"));
 		jQuery('#bando_navigation ul li>ul').css({'top':hauteur});
 	});
-	
+
 	jQuery('#bando_navigation li').menuFocus();
 	jQuery('#bando_outils ul.bandeau_rubriques li').menuFocus();
 

68dc27f1a10675c8af49aeda3d3419c1f2bea9e7
cedric <cedric@yterium.com>
2010-09-07 16:32:30 +0000

    indentation, globale et icone inline
    git-svn-id: svn://trac.rezo.net/spip/spip@16047 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/exec/sites_tous.php
+++ ecrire/exec/sites_tous.php
@@ -15,43 +15,42 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 include_spip('inc/presentation');
 
 // http://doc.spip.org/@exec_sites_tous_dist
-function exec_sites_tous_dist()
-{
-	global $connect_statut,$spip_lang_right;
+function exec_sites_tous_dist() {
+	global $spip_lang_right;
 
-pipeline('exec_init',array('args'=>array('exec'=>'sites_tous'),'data'=>''));
-$commencer_page = charger_fonction('commencer_page', 'inc');
-echo $commencer_page(_T('titre_page_sites_tous'),"naviguer","sites");
-echo debut_gauche('', true);
-echo pipeline('affiche_gauche',array('args'=>array('exec'=>'sites_tous'),'data'=>''));
-echo creer_colonne_droite('', true);
-echo pipeline('affiche_droite',array('args'=>array('exec'=>'sites_tous'),'data'=>''));	  
-echo debut_droite('', true);
+	pipeline('exec_init',array('args'=>array('exec'=>'sites_tous'),'data'=>''));
+	$commencer_page = charger_fonction('commencer_page', 'inc');
+	echo $commencer_page(_T('titre_page_sites_tous'),"naviguer","sites");
+	echo debut_gauche('', true);
+	echo pipeline('affiche_gauche',array('args'=>array('exec'=>'sites_tous'),'data'=>''));
+	echo creer_colonne_droite('', true);
+	echo pipeline('affiche_droite',array('args'=>array('exec'=>'sites_tous'),'data'=>''));
+	echo debut_droite('', true);
 
- echo afficher_objets('site','<b>' . _T('titre_sites_tous') . '</b>', array("FROM" => 'spip_syndic', 'WHERE' => "syndication='non' AND statut='publie'", 'ORDER BY'=> "nom_site"));
+	echo afficher_objets('site','<b>' . _T('titre_sites_tous') . '</b>', array("FROM" => 'spip_syndic', 'WHERE' => "syndication='non' AND statut='publie'", 'ORDER BY'=> "nom_site"));
 
- echo afficher_objets('site','<b>' . _T('titre_sites_syndiques') . '</b>', array('FROM' => 'spip_syndic', 'WHERE' => "(syndication='oui' OR syndication='sus') AND statut='publie'", 'ORDER BY' => "nom_site"));
+	echo afficher_objets('site','<b>' . _T('titre_sites_syndiques') . '</b>', array('FROM' => 'spip_syndic', 'WHERE' => "(syndication='oui' OR syndication='sus') AND statut='publie'", 'ORDER BY' => "nom_site"));
 
- echo afficher_objets('site','<b>' . _T('titre_sites_proposes') . '</b>', array("FROM" => 'spip_syndic', 'WHERE' => "statut='prop'", 'ORDER BY' => "nom_site"));
+	echo afficher_objets('site','<b>' . _T('titre_sites_proposes') . '</b>', array("FROM" => 'spip_syndic', 'WHERE' => "statut='prop'", 'ORDER BY' => "nom_site"));
 
-if (autoriser('bouton','site_creer')) {
-	echo  "<div style='float:$spip_lang_right'>",
-	  icone_inline(_T('icone_referencer_nouveau_site'), generer_url_ecrire('sites_edit'), "site-24.png", "new", 'right'),
-	  "</div><br class='nettoyeur' />";
-}
+	if (autoriser('bouton','site_creer')) {
+		echo
+			icone_inline(_T('icone_referencer_nouveau_site'), generer_url_ecrire('sites_edit'), "site-24.png", "new", $spip_lang_right),
+			"<br class='nettoyeur' />";
+	}
 
-echo pipeline('affiche_milieu',array('args'=>array('exec'=>'sites_tous'),'data'=>''));	  
+	echo pipeline('affiche_milieu',array('args'=>array('exec'=>'sites_tous'),'data'=>''));
 
 
- echo afficher_objets('site','<b>' . _T('avis_sites_probleme_syndication') . '</b>', array("FROM" => 'spip_syndic', 'WHERE' => "syndication='off' AND statut='publie'", 'ORDER BY' => "nom_site"));
+	echo afficher_objets('site','<b>' . _T('avis_sites_probleme_syndication') . '</b>', array("FROM" => 'spip_syndic', 'WHERE' => "syndication='off' AND statut='publie'", 'ORDER BY' => "nom_site"));
 
-if ($connect_statut == '0minirezo') {
-  echo afficher_objets('site','<b>' . _T('info_sites_refuses') . '</b>', array("FROM" => 'spip_syndic', 'WHERE' => "statut='refuse'", 'ORDER BY' => "nom_site"));
-}
+	if ($GLOBALS['visiteur_session']['statut'] == '0minirezo') {
+		echo afficher_objets('site','<b>' . _T('info_sites_refuses') . '</b>', array("FROM" => 'spip_syndic', 'WHERE' => "statut='refuse'", 'ORDER BY' => "nom_site"));
+	}
 
- echo afficher_objets('syndic_article','<b>' . _T('titre_dernier_article_syndique') . '</b>', array('FROM' => 'spip_syndic_articles', 'ORDER BY' => "date DESC"));
+	echo afficher_objets('syndic_article','<b>' . _T('titre_dernier_article_syndique') . '</b>', array('FROM' => 'spip_syndic_articles', 'ORDER BY' => "date DESC"));
 
-echo fin_gauche(), fin_page();
+	echo fin_gauche(), fin_page();
 }
 
 ?>

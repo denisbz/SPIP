808e403e3c796950b460ab447381c7cd53824650
brunobergot <brunobergot@gmail.com>
2010-09-07 16:02:57 +0000

    report de [16043]
    git-svn-id: svn://trac.rezo.net/spip/spip@16044 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/filtres.php
+++ ecrire/inc/filtres.php
@@ -2081,7 +2081,8 @@ function f_jQuery ($texte) {
 			'javascript/jquery.js',
 			'javascript/jquery.form.js',
 			'javascript/jquery.autosave.js',
-			'javascript/ajaxCallback.js'
+			'javascript/ajaxCallback.js',
+			'javascript/jquery.cookie.js'
 		))) as $script)
 			if ($script = find_in_path($script))
 				$x .= "\n<script src=\"$script\" type=\"text/javascript\"></script>\n";

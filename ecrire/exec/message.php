7f713f896225984439c41d4fde54cffdef3270d9
marcimat <marcimat@rezo.net>
2010-09-24 17:29:57 +0000

    Poursuite du déplacement des mots. La fonction de calcul de ressemblance entre des mots n'est pas de l'ordre du plugin mots, mais du core. On l'affecte à inc/rechercher.php en la documentant.
    git-svn-id: svn://trac.rezo.net/spip/spip@16271 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/exec/message.php
+++ ecrire/exec/message.php
@@ -13,7 +13,7 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 include_spip('inc/presentation');
-include_spip('inc/mots');
+include_spip('inc/rechercher'); // mots_ressemblants()
 
 // http://doc.spip.org/@exec_message_dist
 function exec_message_dist()

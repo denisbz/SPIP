36b3553282f9c9b9457a222e5fb457ad346d307e
cedric <cedric@yterium.com>
2010-10-04 11:08:32 +0000

    #FORMULAIRE_CONFIGURER_MODERNISEUR remplace configuration/moderniseur_html
    git-svn-id: svn://trac.rezo.net/spip/spip@16393 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- prive/formulaires/configurer_moderniseur.php
+++ prive/formulaires/configurer_moderniseur.php
@@ -11,18 +11,25 @@
 \***************************************************************************/
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
+include_spip('inc/presentation');
 
-
-// http://doc.spip.org/@action_configurer_moderniseur_html_dist
-function action_configurer_moderniseur_html_dist() {
-	spip_log("On va enregistrer une version de HTML");
-	$securiser_action = charger_fonction('securiser_action', 'inc');
-	$arg = $securiser_action();
-
-	$v = _request('version_html_max');
-	spip_log("C'est-a-dire: $v");
-	if (('html4' == $v) OR ('html5' == $v)) {
-		ecrire_meta('version_html_max', $v);
-	}
+function formulaires_configurer_moderniseur_charger_dist(){
+	$valeurs = array(
+		'version_html_max' => html5_permis() ? 'html5' : 'html4',
+		);
+	return $valeurs;
 }
-?>
+
+
+function formulaires_configurer_moderniseur_traiter_dist(){
+	$res = array('editable'=>true);
+	foreach(array(
+		"version_html_max",
+		) as $m)
+		if (!is_null($v=_request($m)))
+			ecrire_meta($m, $v=='html5'?'html5':'html4');
+
+	$res['message_ok'] = _T('config_info_enregistree');
+	return $res;
+}
+

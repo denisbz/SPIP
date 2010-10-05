c1b638a4cb96ceab022b53b60f4f7ddb54669d42
cedric <cedric@yterium.com>
2010-10-05 12:35:44 +0000

    #FORMULAIRE_RECHERCHE_ECRIRE pour l'espace prive
    git-svn-id: svn://trac.rezo.net/spip/spip@16415 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

---
+++ prive/formulaires/recherche_ecrire.php
@@ -0,0 +1,30 @@
+<?php
+
+/***************************************************************************\
+ *  SPIP, Systeme de publication pour l'internet                           *
+ *                                                                         *
+ *  Copyright (c) 2001-2009                                                *
+ *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
+ *                                                                         *
+ *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
+ *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
+\***************************************************************************/
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+// chargement des valeurs par defaut des champs du formulaire
+function formulaires_recherche_ecrire_charger_dist($action = ''){
+	if ($GLOBALS['spip_lang'] != $GLOBALS['meta']['langue_site'])
+		$lang = $GLOBALS['spip_lang'];
+	else
+		$lang='';
+
+	return 
+		array(
+			'action' => ($action ? $action : generer_url_ecrire('recherche')), # action specifique, ne passe pas par Verifier, ni Traiter
+			'recherche' => _request('recherche'),
+			'lang' => $lang
+		);
+}
+
+?>

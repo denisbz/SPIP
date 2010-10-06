5a10080c9496a8756c61c830d23f4956fc435779
cedric <cedric@yterium.com>
2010-10-06 17:42:47 +0000

    oups, manquait le $
    git-svn-id: svn://trac.rezo.net/spip/spip@16451 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/action/supprimer_lien.php
+++ ecrire/action/supprimer_lien.php
@@ -15,7 +15,7 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 function action_supprimer_lien_dist($arg=null){
 	if (is_null($arg)){
 		$securiser_action = charger_fonction('securiser_action','inc');
-		$arg = securiser_action();
+		$arg = $securiser_action();
 	}
 
 	$arg = explode("-",$arg);

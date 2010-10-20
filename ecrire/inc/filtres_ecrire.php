b06a9ef74b764cccaaf0d12440be4b0f31424510
cedric <cedric@yterium.com>
2010-10-20 16:37:33 +0000

    deplacer les boites dans un fichier dedie qui peut etre surcharge
    git-svn-id: svn://trac.rezo.net/spip/spip@16484 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/filtres_ecrire.php
+++ ecrire/inc/filtres_ecrire.php
@@ -12,6 +12,7 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
+include_spip('inc/filtres_boites');
 
 /**
  * Fonctions utilises au calcul des squelette du prive.
@@ -31,99 +32,6 @@ function interdire_acces($ok=false) {
 	#redirige_formulaire(generer_url_ecrire('403','acces='._request('exec')));
 	exit;
 }
-
-/**
- * #BOITE_OUVRIR{titre[,type]}
- * Racourci pour ouvrir une boite (info, simple, pour noisette ...)
- *
- * @param <type> $p
- * @return <type>
- */
-function balise_BOITE_OUVRIR_dist($p) {
-	$_titre = interprete_argument_balise(1,$p);
-	$_class = interprete_argument_balise(2,$p);
-	$_head_class = interprete_argument_balise(3,$p);
-	$_titre = ($_titre?$_titre:"''");
-	$_class = ($_class?", $_class":", 'simple'");
-	$_head_class = ($_head_class?", $_head_class":"");
-
-	$f = chercher_filtre('boite_ouvrir');
-	$p->code = "$f($_titre$_class$_head_class)";
-	$p->interdire_scripts = false;
-	return $p;
-}
-
-/**
- * #BOITE_PIED{class}
- * Racourci pour passer au pied de la boite, avant sa fermeture
- *
- * @param <type> $p
- * @return <type>
- */
-function balise_BOITE_PIED_dist($p) {
-	$_class = interprete_argument_balise(1,$p);
-	$_class = ($_class?"$_class":"");
-
-	$f = chercher_filtre('boite_pied');
-	$p->code = "$f($_class)";
-	$p->interdire_scripts = false;
-	return $p;
-}
-
-/**
- * #BOITE_FERMER
- * Racourci pour fermer une boite ouverte
- *
- * @param <type> $p
- * @return <type>
- */
-function balise_BOITE_FERMER_dist($p) {
-	$f = chercher_filtre('boite_fermer');
-	$p->code = "$f()";
-	$p->interdire_scripts = false;
-	return $p;
-}
-
-/**
- * Ouvrir une boite
- *
- * @param string $titre
- * @param string $class
- * @return <type>
- */
-function boite_ouvrir($titre, $class='', $head_class=''){
-	$class = "box $class";
-	$head_class = "hd $head_class";
-	if (strlen($titre) AND strpos($titre,'<h')===false)
-		$titre = "<h3>$titre</h3>";
-	return '<div class="'.$class.'">'
-	.'<b class="top"><b class="tl"></b><b class="tr"></b></b>'
-	.'<div class="inner">'
-	.($titre?'<div class="'.$head_class.'">'.$titre.'</div>':'')
-	.'<div class="bd">';
-}
-
-/**
- * Passer au pied d'une boite
- * @param <type> $class
- * @return <type>
- */
-function boite_pied($class='act'){
-	$class = "ft $class";
-	return 	'</div>'
-	.'<div class="'.$class.'">';
-}
-
-/**
- * Fermer une boite
- * @return <type>
- */
-function boite_fermer(){
-	return '</div></div>'
-	.'<b class="bottom"><b class="bl"></b><b class="br"></b></b>'
-	.'</div>';
-}
-
 
 // http://doc.spip.org/@chercher_rubrique
 function chercher_rubrique($msg,$id, $id_parent, $type, $id_secteur, $restreint,$actionable = false, $retour_sans_cadre=false){

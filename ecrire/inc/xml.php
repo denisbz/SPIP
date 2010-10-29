a51c5659162d738fe0a8aabdb7b1e12935c0478b
denisb <denisb@laposte.net>
2010-10-29 07:39:47 +0000

    reprenons [16525] et corrigeons en amont plus proprement. plus petit commentaire.
    git-svn-id: svn://trac.rezo.net/spip/spip@16528 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/xml.php
+++ ecrire/inc/xml.php
@@ -91,7 +91,8 @@ function spip_xml_parse(&$texte, $strict=true, $clean=true, $profondeur = -1){
 				}
 				else return importer_charset($texte,$charset);//$texte // un tag qui constitue du texte a reporter dans $before
 			}
-			$content = substr($txt,0,$p);
+			// un trim pour les tag indentes et/ou ecrits sur plusieurs lignes
+			$content = trim(substr($txt,0,$p));
 			$txt = substr($txt,$p+$ncclos);
 			if ($profondeur==0 OR strpos($content,"<")===FALSE) // eviter une recursion si pas utile
 				$out[$tag][] = importer_charset($content,$charset);//$content;
@@ -128,8 +129,7 @@ function spip_xml_aplatit($arbre,$separateur = " "){
 			else
 				$s.="$feuille$separateur";
 		}
-	return strlen($separateur) ?
-				trim(substr($s, 0, -strlen($separateur))) : trim($s);
+	return strlen($separateur) ? substr($s, 0, -strlen($separateur)) : $s;
 }
 
 // http://doc.spip.org/@spip_xml_tagname

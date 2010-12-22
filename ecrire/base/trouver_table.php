1e298667f749fe9902c72f68b66efa899bc4f92a
esj <esj@rezo.net>
2010-12-22 15:54:32 +0000

    Report adapté de r16809.  Amélioration de r16807: gérer le champ titre par défaut au niveau de trouver_table, afin d'éviter toute erreur SQL, en particulier celle potentielle dans les URL arbos.
    git-svn-id: svn://trac.rezo.net/spip/branches/r16004+plugins@16811 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/base/trouver_table.php
+++ ecrire/base/trouver_table.php
@@ -130,8 +130,10 @@ function base_trouver_table_dist($nom, $serveur=''){
 		$table = table_objet(preg_replace(',^id_,', '', reset(explode(',', $desc['key']["PRIMARY KEY"]))));
 		$desc['table']= $nom_sql;
 		$desc['connexion']= $serveur;
-		$desc['titre'] =
-		  isset($GLOBALS['table_titre'][$table]) ? $GLOBALS['table_titre'][$table] : '';
+		$desc['titre'] = isset($GLOBALS['table_titre'][$table])
+		? $GLOBALS['table_titre'][$table]
+		: (isset($desc['field']['titre']) ? 'titre' : '');
+
 		$desc['date'] =
 		  isset($GLOBALS['table_date'][$table]) ? $GLOBALS['table_date'][$table] : '';
 		$connexion['tables'][$nom] = $desc;

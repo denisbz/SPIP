0b639dd67a4f1f8e4f27a7772e616ee467f8c4a9
cedric <cedric@yterium.com>
2011-01-20 09:16:14 +0000

    l'echappement est du ressort du critere, pas du squelette !
    git-svn-id: svn://trac.rezo.net/spip/spip@16999 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- prive/listes/auteurs_fonctions.php
+++ prive/listes/auteurs_fonctions.php
@@ -34,7 +34,7 @@ function critere_compteur_articles_filtres_dist($idb, &$boucles, $crit, $left=fa
 
 	$boucle->from['articles'] = 'spip_articles';
 	$boucle->from_type['articles'] = 'left';
-	$boucle->join['articles'] = array("'LAA'","'id_article'","'id_objet'","'(articles.statut IS NULL OR '.sql_in('articles.statut',$_statut$not).')'");
+	$boucle->join['articles'] = array("'LAA'","'id_article'","'id_objet'","'(articles.statut IS NULL OR '.sql_in('articles.statut',_q($_statut)$not).')'");
 
 	$boucle->select[]= "COUNT(articles.id_article) AS compteur_articles";
 	$boucle->group[] = 'auteurs.id_auteur';

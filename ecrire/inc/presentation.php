71a0b4c5a10b3e32974188a6d6c0f091be69b32f
cedric <cedric@yterium.com>
2011-02-12 10:23:31 +0000

    evacuation dans le grenier
    git-svn-id: svn://trac.rezo.net/spip/spip@17170 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/presentation.php
+++ ecrire/inc/presentation.php
@@ -22,56 +22,7 @@ include_spip('inc/puce_statut');
 include_spip('inc/filtres_ecrire');
 
 define('_ACTIVER_PUCE_RAPIDE', true);
-define('_SIGNALER_ECHOS', true);
-define('_INTERFACE_ONGLETS', false);
 
-// Faux HR, avec controle de couleur
-// http://doc.spip.org/@hr
-/*
-function hr($color, $retour = false) {
-	$ret = "\n<div style='height: 1px; margin-top: 5px; padding-top: 5px; border-top: 1px solid $color;'></div>";
-
-	if ($retour) return $ret; else echo_log('hr',$ret);
-}
-*/
-//
-// Cadres
-//
-// http://doc.spip.org/@afficher_onglets_pages
-/*
-function afficher_onglets_pages($ordre,$onglets){
-	static $onglet_compteur = 0;
-	$res = "";
-	$corps = "";
-	$cpt = 0;
-	$actif = 0;
-	// ordre des onglets
-	foreach($ordre as $id => $label) {
-		$cpt++;
-		$disabled = strlen(trim($onglets[$id]))?"":" class='tabs-disabled'";
-		if (!$actif && !$disabled) $actif = $cpt;
-		$res .= "<li$disabled><a rel='$cpt' href='#$id'><span>" . $label . "</span></a></li>";
-	}
-	$res = "<ul class='tabs-nav'>$res</ul>";
-	foreach((_INTERFACE_ONGLETS ? array_keys($ordre):array_keys($onglets)) as $id){
-		$res .= "<div id='$id' class='tabs-container'>" . $onglets[$id] . "<br class='nettoyeur' /></div>";
-	}
-	$onglet_compteur++;
-	return "<div class='boite_onglets' id='boite_onglet_$onglet_compteur'>$res</div>"
-	. (_INTERFACE_ONGLETS ?
-	   http_script("$('#boite_onglet_$onglet_compteur').tabs(".($actif?"$actif,":"")."{ fxAutoHeight: true });
-	 if (!$.browser.safari)
-	 $('ul.tabs-nav li').hover(
-	 	function(){
-	 		\$('#boite_onglet_$onglet_compteur').triggerTab(parseInt(\$(this).attr('rel')));
-	 		return false;
-	 	}
-	 	,
-	 	function(){}
-	 	);")
-	   :"");
-}
-*/
 // http://doc.spip.org/@debut_cadre
 function debut_cadre($style, $icone = "", $fonction = "", $titre = "", $id="", $class="", $padding=true) {
 	global $spip_display, $spip_lang_left;

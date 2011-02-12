b4405dbb7a11c91d4c540f27104991ddce2572dc
cedric <cedric@yterium.com>
2011-02-12 01:05:50 +0000

    un filtre generique trouver_rubrique_creer_objet pour determiner la rubrique par defaut ou creer un objet lorsque rien n'est precise S'utilise ainsi : [(#ID_RUBRIQUE|trouver_rubrique_creer_objet{breve})]
    git-svn-id: svn://trac.rezo.net/spip/spip@17159 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/filtres_ecrire.php
+++ ecrire/inc/filtres_ecrire.php
@@ -199,4 +199,24 @@ function afficher_qui_edite($id_objet,$objet){
 	return $qui[$objet][$id_objet] = _T('texte_travail_article', $modif);
 }
 
+
+function trouver_rubrique_creer_objet($id_rubrique,$objet){
+	global $connect_id_rubrique;
+	if (!$id_rubrique){
+		$in = !count($connect_id_rubrique)
+			? ''
+			: (" AND ".sql_in('id_rubrique', $connect_id_rubrique));
+
+		$id_rubrique = sql_getfetsel('id_rubrique', 'spip_rubriques', "id_parent=0$in", '', "id_rubrique DESC", 1);
+
+		if (!autoriser("creer{$objet}dans", 'rubrique', $id_rubrique)){
+			// manque de chance, la rubrique n'est pas autorisee, on cherche un des secteurs autorises
+			$res = sql_select("id_rubrique", "spip_rubriques", "id_parent=0");
+			while (!autoriser("creer{$objet}dans", 'rubrique', $id_rubrique) && $row_rub = sql_fetch($res)){
+				$id_rubrique = $row_rub['id_rubrique'];
+			}
+		}
+	}
+  return $id_rubrique;
+}
 ?>

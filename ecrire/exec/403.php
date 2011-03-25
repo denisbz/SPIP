4ec1d98c4d8d1ea836d336fb1be17f243e492bb7
cedric <cedric@yterium.com>
2011-03-25 10:19:58 +0000

    Lors d'une requete var_zajax d'un morceau de page, n'envoyer que le message acces interdit, pas toute la page html
    git-svn-id: svn://trac.rezo.net/spip/spip@17509 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/exec/403.php
+++ ecrire/exec/403.php
@@ -23,23 +23,31 @@ function exec_403_dist(){
 	$navigation = "";
 	$extra = "";
 
-	include_spip('inc/presentation'); // alleger les inclusions avec un inc/presentation_mini
-	$commencer_page = charger_fonction('commencer_page','inc');
-	echo $commencer_page($titre);
+	$contenu = "<h1>"._T('info_acces_interdit')."</h1>"
+		. _L("Vous n'avez pas le droit d'acc&eacute;der à la page <b>@exec@</b>.",array('exec'=>_request('exec')));
 
-	echo debut_gauche("403_$exec",true);
-	echo recuperer_fond('prive/squelettes/navigation/dist',array());
-	echo pipeline('affiche_gauche',array('args'=>array('exec'=>'403','exec_erreur'=>$exec),'data'=>''));
+	if (_request('var_zajax')) {
+		include_spip('inc/actions');
+		ajax_retour($contenu);
+	}
+	else {
+		include_spip('inc/presentation'); // alleger les inclusions avec un inc/presentation_mini
 
-	echo creer_colonne_droite("403",true);
-	echo pipeline('affiche_droite',array('args'=>array('exec'=>'403','exec_erreur'=>$exec),'data'=>''));
+		$commencer_page = charger_fonction('commencer_page','inc');
+		echo $commencer_page($titre);
 
-	echo debut_droite("403",true);
-	echo "<h1>"._T('info_acces_interdit')."</h1>";
-	echo _L("Vous n'avez pas le droit d'acc&eacute;der à la page <b>@exec@</b>.",array('exec'=>_request('exec')));
-	echo pipeline('affiche_milieu',array('args'=>array('exec'=>'403','exec_erreur'=>$exec),'data'=>''));
+		echo debut_gauche("403_$exec",true);
+		echo recuperer_fond('prive/squelettes/navigation/dist',array());
+		echo pipeline('affiche_gauche',array('args'=>array('exec'=>'403','exec_erreur'=>$exec),'data'=>''));
 
-	echo fin_gauche(),fin_page();
+		echo creer_colonne_droite("403",true);
+		echo pipeline('affiche_droite',array('args'=>array('exec'=>'403','exec_erreur'=>$exec),'data'=>''));
+
+		echo debut_droite("403",true);
+		echo pipeline('affiche_milieu',array('args'=>array('exec'=>'403','exec_erreur'=>$exec),'data'=>$contenu));
+
+		echo fin_gauche(),fin_page();
+	}
 }
 
 ?>

85b16621d719188b2ef82c7fc9b1e6b5941d39be
esj <esj@rezo.net>
2011-03-27 23:11:06 +0000

    Nouvelle DTD des plugins: prise en compte de la balise "spip" permettant de tenir compte des versions de SPIP, ce qui n'était pas encore fait jusqu'à présent. La fonction plugins_infos_paquet_dist, si on l'appelle avec une description XML mais sans donner le nom du plugin en 2 argument, retourne le tableau de toutes les versions disponibles, ce qui permettra à SVP et STEP d'opérer.
    Reste à ajouter la prise en compte de la version effective de SPIP pour l'activation (l'équivalent de "verifier_conformite" pour plugin.xml").
    
    
    git-svn-id: svn://trac.rezo.net/spip/branches/r16004+plugins@17536 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/plugins/infos_paquet.php
+++ ecrire/plugins/infos_paquet.php
@@ -27,12 +27,16 @@ function plugins_infos_paquet($desc, $plug='', $dir_plugins = _DIR_PLUGINS)
 	$valider_xml = charger_fonction('valider', 'xml');
 	$vxml = $valider_xml($desc, false, $process,  'paquet.dtd');
 	if (!$vxml->err) {
-		// compatibilite avec l'existant:
-		$vxml->tree['icon'] = $vxml->tree['logo']; 
-		$vxml->tree['path'] = $vxml->tree['chemin'] ? $vxml->tree['chemin'] : array(array('dir'=>'')); // initialiser par defaut
-		if ($plug) paquet_readable_files($vxml, "$dir_plugins$plug/");
+		// On veut toutes les variantes selon la version de SPIP
+		if (!$plug)  return $vxml->versions;
 
-		return $vxml->tree;
+		// compatibilite avec l'existant:
+		$tree = $vxml->versions['0'];
+		$tree['icon'] = $tree['logo']; 
+		$tree['path'] = $tree['chemin'] ? $tree['chemin'] : array(array('dir'=>'')); // initialiser par defaut
+		$tree['auteur'] = join("\n", $tree['auteur']);
+		paquet_readable_files($tree, "$dir_plugins$plug/");
+		return $tree;
 	}
 	// Prendre les messages d'erreur sans les numeros de lignes
 	$msg = array_map('array_shift', $vxml->err);
@@ -46,44 +50,46 @@ function plugins_infos_paquet($desc, $plug='', $dir_plugins = _DIR_PLUGINS)
 	return array('erreur' => $msg);
 }
 
-function paquet_readable_files(&$vxml, $dir)
+function paquet_readable_files(&$tree, $dir)
 {
-	$prefix = $vxml->tree['prefix'];
+	$prefix = $tree['prefix'];
 
-	$vxml->tree['options'] =  (is_readable($dir . $f = ($prefix . '_options.php'))) ? array($f) : array();
+	$tree['options'] =  (is_readable($dir . $f = ($prefix . '_options.php'))) ? array($f) : array();
 
-	$vxml->tree['fonctions'] = (is_readable($dir . $f = ($prefix . '_fonctions.php'))) ? array($f) : array();
+	$tree['fonctions'] = (is_readable($dir . $f = ($prefix . '_fonctions.php'))) ? array($f) : array();
 
-	$vxml->tree['install'] = (is_readable($dir . $f = ($prefix . '_actions.php'))) ? array($f) : array();
+	$tree['install'] = (is_readable($dir . $f = ($prefix . '_actions.php'))) ? array($f) : array();
 }
 
-// Appeler l'indenteur pour sa gestion de la profondeur, 
-// et memoriser les attributs dans le tableau avec l'oppose de la profondeur
-// comme index (la valeur positive a son developpe en chaine, on n'en veut pas)
-// sauf pour la balise racine qui permet d'initialiser.
-// Cette initialisation consiste a memoriser les attributs de la balise "paquet"
+// Appeler le validateur, qui memorise le texte dans le tableau "versions"
+// On  memorise en plus dans les index de numero de version de SPIP
+// les attributs de la balise rencontree
 // qu'on complete par des entrees nommees par les sous-balises de "paquet",
 // et initialisees par un tableau vide, rempli a leur rencontre.
+// La sous-balise "spip", qui ne peut apparaitre qu'apres les autres,
+// reprend les valeurs recuperees precedement (valeurs par defaut)
 
 function paquet_debutElement($phraseur, $name, $attrs)
 {
 	xml_debutElement($phraseur, $name, $attrs);
-	$depth = $phraseur->depth;
-	if (!$phraseur->err) {
-		if ($name == 'paquet') {
-		  $attrs['lib']=array();
-		  $attrs['onglet']=array();
-		  $attrs['bouton']=array();
-		  $attrs['chemin']=array();
-		  $attrs['utilise']=array();
-		  $attrs['pipeline']=array();
-		  $attrs['necessite']=array();
-		  $phraseur->tree = $attrs;
-		} else {
-		  $phraseur->contenu[(0-$depth)] = $attrs;
-		}
-	}
-	$phraseur->contenu[(0-$depth)][''] = '';
+	if ($phraseur->err) return;
+	if (($name == 'paquet') OR ($name == 'spip')) {
+	    if ($name == 'spip') {
+		$n = $attrs['compatible'];
+		$attrs = $phraseur->contenu['paquet'];
+		$attrs['compatible'] = $n;
+	    } else { $n = '0'; $phraseur->contenu['paquet'] = $attrs;}
+	      $phraseur->contenu['compatible'] = $n;
+	      $attrs['lib']=array();
+	      $attrs['onglet']=array();
+	      $attrs['bouton']=array();
+	      $attrs['chemin']=array();
+	      $attrs['utilise']=array();
+	      $attrs['pipeline']=array();
+	      $attrs['necessite']=array();
+	      $phraseur->versions[$phraseur->contenu['compatible']] = $attrs;
+	} else $phraseur->versions[$phraseur->contenu['compatible']][$name][0] = $attrs;
+	$phraseur->versions[$phraseur->contenu['compatible']][''] = '';
 }
 
 // Appeler l'indenteur pour sa gestion de la profondeur,
@@ -92,10 +98,8 @@ function paquet_debutElement($phraseur, $name, $attrs)
 function paquet_textElement($phraseur, $data)
 {
 	xml_textElement($phraseur, $data);
-	if (!$phraseur->err) {
-		$depth = $phraseur->depth;
-		$phraseur->contenu[(0-$depth)][''] .= trim($data);
-	}
+	if ($phraseur->err OR !($data = trim($data))) return;
+	$phraseur->versions[$phraseur->contenu['compatible']][''] .= $data;
 }
 
 // Si on sait deja que le texte n'est pas valide on ne fait rien.
@@ -104,17 +108,24 @@ function paquet_textElement($phraseur, $data)
 
 function paquet_finElement($phraseur, $name)
 {
-	if (!$phraseur->err) {
-		$depth = 0-$phraseur->depth;
-		$attrs = $phraseur->contenu[$depth];
-		$texte = $attrs[''];
-		unset($attrs['']);
-		$f = 'info_paquet_' . $name;
-		if (function_exists($f))
-		  $f($phraseur, $attrs, $texte);
-		elseif (!$attrs)
-		  $phraseur->tree[$name] = $texte;
-		else $phraseur->tree[$name][$attrs['nom']] = $attrs;
+	if ($phraseur->err) return;
+	$n = $phraseur->contenu['compatible'];
+
+	if (is_array($phraseur->versions[$n][$name][0])) {
+	  $attrs = $phraseur->versions[$n][$name][0];
+	  unset($phraseur->versions[$n][$name][0]);
+	}
+	$texte = $phraseur->versions[$n][''];
+	$phraseur->versions[$n][''] = '';
+
+	$f = 'info_paquet_' . $name;
+	if (function_exists($f))
+	  $f($phraseur, $attrs, $texte);
+	elseif (!$attrs)
+	  $phraseur->versions[$n][$name] = $texte;
+	else {
+	  $phraseur->versions[$n][$name][$attrs['nom']] = $attrs;
+#	  echo("<br>pour $name $n " . $attrs['nom']); var_dump($phraseur->versions[$n]);
 	}
 	xml_finElement($phraseur, $name, $attrs);
 }
@@ -123,26 +134,25 @@ function info_paquet_licence($phraseur, $attrs, $texte)
 { 
   if (isset($attrs['lien'])) 
     $texte = "<a href='" . $attrs['lien'] . "'>" . $texte . "</a>";
-  $phraseur->tree['licence'] = $texte;
+  $n = $phraseur->contenu['compatible'];
+  $phraseur->versions[$n]['licence'] = $texte;
 }
 
 function info_paquet_auteur($phraseur, $attrs, $texte)
 {
+#  echo 'auteur ', $texte;  var_dump($attrs);
   if (isset($attrs['mail'])) {
     if (strpos($attrs['mail'], '@') )
 	$attrs['mail'] = str_replace('@', ' AT ', $attrs['mail']);
-    $texte .= ' (' . $attrs['mail'] . ')';
-  }
+    $mail = ' (' . $attrs['mail'] . ')';
+  } else $mail = '';
+
   if (isset($attrs['lien'])) 
-    $texte = "<a href='" . $attrs['lien'] . "'>" . $texte . "</a>";
+    $lien = "<a href='" . $attrs['lien'] . "'>" . $texte . "</a>";
+  else $lien = $texte;
 
-  $phraseur->tree['auteur'] = isset($phraseur->tree['auteur']) ? 
-    ($phraseur->tree['auteur'] . ' ' . $texte) : $texte;
+  $n = $phraseur->contenu['compatible'];
+  $phraseur->versions[$n]['auteur'][$texte] = $lien . $mail;
 }
-
-// Sous-cas de compatibilite; A traiter.
-function info_paquet_spip($phraseur, $attrs, $texte)
-{ $phraseur->tree['spip'] = $texte; }
-
 
 ?>

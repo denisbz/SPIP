03ef6ddd2eec598e6869826bae10435dabc80e1b
cedric <cedric@yterium.com>
2011-03-30 14:27:19 +0000

    code mort : l'auto-association des documents avec un objet est geree par l'extension medias, de façon plus générique.
    git-svn-id: svn://trac.rezo.net/spip/spip@17574 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/modifier.php
+++ ecrire/inc/modifier.php
@@ -185,13 +185,6 @@ function modifier_contenu($type, $id, $options, $c=false, $serveur='') {
 				suivre_invalideur($options['invalideur']);
 		}
 
-		// marquer les documents vus dans le texte si il y a lieu
-		include_spip('inc/autoriser');
-		if (autoriser('autoassocierdocument',$type,$id)){
-			include_spip('base/auxiliaires');
-			marquer_doublons_documents($champs,$id,$type,$id_table_objet,$table_objet,$spip_table_objet, $desc, $serveur);
-		}
-
 		// Notifications, gestion des revisions...
 		// en standard, appelle |nouvelle_revision ci-dessous
 		pipeline('post_edition',
@@ -231,59 +224,6 @@ function modifier_contenu($type, $id, $options, $c=false, $serveur='') {
 	));
 
 	return true;
-}
-
-// http://doc.spip.org/@marquer_doublons_documents
-function marquer_doublons_documents($champs,$id,$type,$id_table_objet,$table_objet,$spip_table_objet, $desc=array(), $serveur=''){
-	if (!isset($champs['texte']) AND !isset($champs['chapo'])) return;
-	if (!$desc){
-		$trouver_table = charger_fonction('trouver_table', 'base');
-		$desc = $trouver_table($table_objet, $serveur);
-	}
-	$load = "";
-
-	// charger le champ manquant en cas de modif partielle de l'objet
-	// seulement si le champ existe dans la table demande
-	if (!isset($champs['texte']) && isset($desc['field']['texte'])) $load = 'texte';
-	if (!isset($champs['chapo']) && isset($desc['field']['chapo'])) $load = 'chapo';
-	if ($load){
-		$champs[$load] = "";
-		$row = sql_fetsel($load, $spip_table_objet, "$id_table_objet=".sql_quote($id));
-		if ($row AND isset($row[$load]))
-			$champs[$load] = $row[$load];
-	}
-	include_spip('inc/texte');
-	include_spip('base/abstract_sql');
-	$GLOBALS['doublons_documents_inclus'] = array();
-	// echapper le html
-	// et detecter les modeles de document dans le texte restant
-	traiter_modeles(echappe_html($champs['chapo'].$champs['texte']),true);
-	sql_updateq("spip_documents_liens", array("vu" => 'non'), "id_objet=$id AND objet=".sql_quote($type));
-	if (count($GLOBALS['doublons_documents_inclus'])){
-		// on repasse par une requete sur spip_documents pour verifier que les documents existent bien !
-		$in_liste = sql_in('id_document',
-			$GLOBALS['doublons_documents_inclus']);
-		$res = sql_select("id_document", "spip_documents", $in_liste);
-		while ($row = sql_fetch($res)) {
-			// Mettre le lien a jour ou le creer s'il n'existe pas deja
-			if (!sql_updateq("spip_documents_liens", array("vu" => 'oui'), "id_objet=$id AND objet=".sql_quote($type)." AND id_document=".$row['id_document']) OR
-			!sql_getfetsel("id_document", "spip_documents_liens", "id_document=".$row['id_document']." AND id_objet=$id AND objet=".sql_quote($type))) {
-				sql_insertq("spip_documents_liens", array('id_objet' => $id, 'objet' => $type, 'id_document' => $row['id_document'], 'vu' => 'oui'));
-				pipeline('post_edition',
-					array(
-						'args' => array(
-							'operation' => 'lier_document',
-							'table' => 'spip_documents',
-							'id_objet' => $row['id_document'],
-							'objet' => $type,
-							'id' => $id
-						),
-						'data' => null
-					)
-				);
-			}
-		}
-	}
 }
 
 // Enregistre une revision d'article

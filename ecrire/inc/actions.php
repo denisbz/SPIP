fb20e77bb2a1621fe8bc4b05c572e0cfc061c05d
cedric <cedric@yterium.com>
2011-04-13 11:10:52 +0000

    format du code (indentation etc..)
    git-svn-id: svn://trac.rezo.net/spip/spip@17717 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/actions.php
+++ ecrire/inc/actions.php
@@ -14,46 +14,44 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 
 // http://doc.spip.org/@generer_action_auteur
-function generer_action_auteur($action, $arg, $redirect="", $mode=false, $att='', $public=false)
-{
+function generer_action_auteur($action, $arg, $redirect = "", $mode = false, $att = '', $public = false){
 	$securiser_action = charger_fonction('securiser_action', 'inc');
 	return $securiser_action($action, $arg, $redirect, $mode, $att, $public);
 }
 
 // http://doc.spip.org/@redirige_action_auteur
-function redirige_action_auteur($action, $arg, $ret, $gra='', $mode=false, $atts='') {
+function redirige_action_auteur($action, $arg, $ret, $gra = '', $mode = false, $atts = ''){
 
-	$r = _DIR_RESTREINT . generer_url_ecrire($ret, $gra, true, true);
+	$r = _DIR_RESTREINT.generer_url_ecrire($ret, $gra, true, true);
 
 	return generer_action_auteur($action, $arg, $r, $mode, $atts);
 }
 
 // http://doc.spip.org/@redirige_action_post
-function redirige_action_post($action, $arg, $ret, $gra, $corps, $att='') {
-	$r = _DIR_RESTREINT . generer_url_ecrire($ret, $gra, false, true);
-	return generer_action_auteur($action, $arg, $r, $corps, $att . " method='post'");
+function redirige_action_post($action, $arg, $ret, $gra, $corps, $att = ''){
+	$r = _DIR_RESTREINT.generer_url_ecrire($ret, $gra, false, true);
+	return generer_action_auteur($action, $arg, $r, $corps, $att." method='post'");
 }
 
 
 // http://doc.spip.org/@ajax_retour
-function ajax_retour($corps,$xml = true)
-{
+function ajax_retour($corps, $xml = true){
 	$e = "";
 	if (isset($_COOKIE['spip_admin'])
-	AND ((_request('var_mode') == 'debug') OR !empty($GLOBALS['tableau_des_temps'])))
+	    AND ((_request('var_mode')=='debug') OR !empty($GLOBALS['tableau_des_temps'])))
 		$e = erreur_squelette();
-	if (isset($GLOBALS['transformer_xml']) OR $GLOBALS['exec'] == 'valider_xml') {
-	$debut = _DOCTYPE_ECRIRE
-	. "<html><head><title>Debug Spip Ajax</title></head>"
-	.  "<body><div>\n\n"
-	. "<!-- %%%%%%%%%%%%%%%%%%% Ajax %%%%%%%%%%%%%%%%%%% -->\n";
+	if (isset($GLOBALS['transformer_xml']) OR $GLOBALS['exec']=='valider_xml'){
+		$debut = _DOCTYPE_ECRIRE
+		         ."<html><head><title>Debug Spip Ajax</title></head>"
+		         ."<body><div>\n\n"
+		         ."<!-- %%%%%%%%%%%%%%%%%%% Ajax %%%%%%%%%%%%%%%%%%% -->\n";
 
-	$fin = '</div></body></html>';
+		$fin = '</div></body></html>';
 
 	} else {
 		$c = $GLOBALS['meta']["charset"];
-		header('Content-Type: text/html; charset='. $c);
-		$debut = (($xml AND strlen(trim($corps)))?'<' . "?xml version='1.0' encoding='" . $c . "'?" . ">\n":'');
+		header('Content-Type: text/html; charset='.$c);
+		$debut = (($xml AND strlen(trim($corps))) ? '<'."?xml version='1.0' encoding='".$c."'?".">\n" : '');
 	}
 	echo $debut, $corps, $fin, $e;
 }

38b87599d5a824d210ee476a3c26c065b09c3c6b
salvatore <salvatore@rezo.net>
2011-04-28 21:10:01 +0000

    langues (public)
    git-svn-id: svn://trac.rezo.net/spip/spip@17773 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/lang/public_en.php
+++ ecrire/lang/public_en.php
@@ -8,22 +8,22 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 // A
-'accueil_site' => 'Home page',
+'accueil_site' => 'Homepage',
 'article' => 'Article',
 'articles' => 'Articles',
-'articles_auteur' => 'This author\'s articles',
-'articles_populaires' => 'The most read articles',
-'articles_rubrique' => 'This section\'s articles',
-'aucun_article' => 'There is no article at this address',
-'aucun_auteur' => 'There is no author at this address',
-'aucun_site' => 'There is no site at this address',
-'aucune_breve' => 'There is no news item at this address',
-'aucune_rubrique' => 'There is no section at this address',
+'articles_auteur' => 'Articles by this author',
+'articles_populaires' => 'Most popular articles',
+'articles_rubrique' => 'Articles in this section',
+'aucun_article' => 'No articles here',
+'aucun_auteur' => 'No authors here',
+'aucun_site' => 'No linked sites here',
+'aucune_breve' => 'No news items here',
+'aucune_rubrique' => 'No sections here',
 'auteur' => 'Author',
 'autres' => 'Others',
 'autres_breves' => 'Other news',
-'autres_groupes_mots_clefs' => 'Other groups of key words',
-'autres_sites' => 'Other sites',
+'autres_groupes_mots_clefs' => 'Other groups of keywords',
+'autres_sites' => 'Other websites',
 
 // B
 'bonjour' => 'Hello',
@@ -32,50 +32,50 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 // C
 'commenter_site' => 'Comment on this site',
-'copie_document_impossible' => 'Impossible to copy the document',
+'copie_document_impossible' => 'Impossible to copy this document',
 
 // D
 'date' => 'Date',
-'dernier_ajout' => 'Latest addition',
+'dernier_ajout' => 'Latest update',
 'dernieres_breves' => 'Latest news',
 'derniers_articles' => 'Latest articles',
 'derniers_commentaires' => 'Latest comments',
-'derniers_messages_forum' => 'Lastest messages published in the forums',
-'documents_interdits_forum' => 'Documents prohibited in the forum',
+'derniers_messages_forum' => 'Latest forum posts',
+'documents_interdits_forum' => 'Documents not allowed in this forum',
 
 // E
 'edition_mode_texte' => 'Text mode only',
 'en_reponse' => 'Replying to:',
 'en_resume' => 'Summary',
-'envoyer_message' => 'To send a message',
+'envoyer_message' => 'Send a message',
 'espace_prive' => 'Private area',
 
 // F
-'formats_acceptes' => 'Accepted formats: @formats@.',
+'formats_acceptes' => 'Valid formats: @formats@.',
 'forum' => 'Forum',
 
 // H
-'hierarchie_site' => 'The site\'s hierarchy',
+'hierarchie_site' => 'Site map',
 
 // J
 'jours' => 'days',
 
 // L
-'lien_connecter' => 'Sign In',
+'lien_connecter' => 'Sign in',
 
 // M
 'meme_auteur' => 'By the same author',
-'meme_rubrique' => 'In the same section',
+'meme_rubrique' => 'Also in this section',
 'memes_auteurs' => 'By the same authors',
 'message' => 'Message',
 'messages_forum' => 'Forum messages',
-'messages_recents' => 'Most recent forum messages',
+'messages_recents' => 'Most recent forum posts',
 'mots_clef' => 'Keyword',
 'mots_clefs' => 'Keywords',
-'mots_clefs_meme_groupe' => 'Keywords in the same group',
+'mots_clefs_meme_groupe' => 'Other keywords in this group',
 
 // N
-'navigation' => 'Navigation',
+'navigation' => 'Browsing',
 'nom' => 'Name',
 'nouveautes' => 'What\'s new',
 'nouveautes_web' => 'What\'s new on the Web',
@@ -86,40 +86,40 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'page_precedente' => 'previous page',
 'page_suivante' => 'next page',
 'par_auteur' => 'by ',
-'participer_site' => 'You can take an active part in this website and write your own articles by registering below. You will then receive immediately an email giving you an access code to the private area of the site.',
+'participer_site' => 'You can take active part in this website and write your own articles by signing up here. You will receive an email with your account information for the private area of the site.',
 'plan_site' => 'Site Map',
 'popularite' => 'Popularity',
 'poster_message' => 'Post a message',
-'proposer_site' => 'You can suggest a site to be added to this section:',
+'proposer_site' => 'You can suggest a website for inclusion in this section:',
 
 // R
-'repondre_article' => 'Reply to this article',
-'repondre_breve' => 'Reply to this news item',
+'repondre_article' => 'Comment on this article',
+'repondre_breve' => 'Comment on this news item',
 'repondre_message' => 'Reply to this message',
 'resultats_recherche' => 'Search results',
-'retour_debut_forums' => 'Back to the start of the forums',
+'retour_debut_forums' => 'Back to forum top',
 'rubrique' => 'Section',
 'rubriques' => 'Sections',
 
 // S
 'signatures_petition' => 'Signatures',
-'site_realise_avec_spip' => 'Site created with SPIP',
-'sites_web' => 'Web sites',
-'sous_rubriques' => 'Sub-sections',
+'site_realise_avec_spip' => 'Site powered by SPIP',
+'sites_web' => 'Websites',
+'sous_rubriques' => 'Subsections',
 'spam' => 'Spam',
 'suite' => 'continue',
-'sur_web' => 'On the Web',
-'syndiquer_rubrique' => 'Syndicate this section',
-'syndiquer_site' => 'Syndicate the whole site',
+'sur_web' => 'Around the Web',
+'syndiquer_rubrique' => 'Subscribe to this section',
+'syndiquer_site' => 'Subscribe to the whole site',
 
 // T
-'texte_lettre_information' => 'Here is the site\'s newsletter',
-'texte_lettre_information_2' => 'This letter summarizes the articles and news items published since',
+'texte_lettre_information' => 'Here is the site newsletter',
+'texte_lettre_information_2' => 'This letter summarises articles and news items published since',
 
 // V
 'ver_imprimer' => 'Printable version',
-'voir_en_ligne' => 'See online',
-'voir_squelette' => 'ٍShow the template of this page'
+'voir_en_ligne' => 'View online',
+'voir_squelette' => 'show the template of this page'
 );
 
 ?>

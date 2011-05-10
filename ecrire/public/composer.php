3409640fd676f046c140ec6d2b70bd4d139c1477
guy.cesaro <guy.cesaro@gmail.com>
2011-05-10 17:23:53 +0000

    On a besoin de generer_action_auteur a la fin de l'installation pour que SPIP puisse s'installer sans medias.
    git-svn-id: svn://trac.rezo.net/spip/spip@17851 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/public/composer.php
+++ ecrire/public/composer.php
@@ -17,6 +17,7 @@ include_spip('inc/documents');
 include_spip('inc/distant');
 include_spip('inc/rubriques'); # pour calcul_branche (cf critere branche)
 include_spip('inc/acces'); // Gestion des acces pour ical
+include_spip('inc/actions');
 include_spip('public/iterateur');
 include_spip('public/interfaces');
 include_spip('public/quete');

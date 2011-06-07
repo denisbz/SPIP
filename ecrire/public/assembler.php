12a54234055a48a0bd3a6b90507851ecba1f8363
cedric <cedric@yterium.com>
2011-06-07 20:44:05 +0000

    #2116 : ne pas se laisser embrouiller par un header HTTP/1.1 301 Moved Permanently
    git-svn-id: svn://trac.rezo.net/spip/spip@17975 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/public/assembler.php
+++ ecrire/public/assembler.php
@@ -567,7 +567,7 @@ function page_base_href(&$texte){
 function envoyer_entetes($entetes) {
 	foreach ($entetes as $k => $v)
 	#	if (strncmp($k, 'X-Spip-', 7))
-			@header("$k: $v");
+			@header($v?"$k: $v":$k);
 }
 
 ?>

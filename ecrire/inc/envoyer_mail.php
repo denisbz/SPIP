6020e71f0a249cb0c67d29870730d9c5e9b9b622
cedric <cedric@yterium.com>
2011-07-06 13:33:31 +0000

    report de r18188 sur l'evolution de envoyer_mail
    git-svn-id: svn://trac.rezo.net/spip/spip@18190 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/envoyer_mail.php
+++ ecrire/inc/envoyer_mail.php
@@ -40,51 +40,30 @@ function nettoyer_caracteres_mail($t) {
 	return $t;
 }
 
+// Envoi d'un mail avec d'eventuelles pieces jointes
+// specifiees en dernier argument sous forme d'un tableau de sous-tableaux
+// de longueur 2 (headers / body)
+
 // http://doc.spip.org/@inc_envoyer_mail_dist
-function inc_envoyer_mail_dist($email, $sujet, $texte, $from = "", $headers = "") {
+function inc_envoyer_mail_dist($email, $sujet, $texte, $from = "", $headers = "", $parts=array()) {
 
 	if (!email_valide($email)) return false;
 	if ($email == _T('info_mail_fournisseur')) return false; // tres fort
-
-	// Traiter les headers existants
-	if (strlen($headers)) $headers = trim($headers)."\n";
 
 	// Fournir si possible un Message-Id: conforme au RFC1036,
 	// sinon SpamAssassin denoncera un MSGID_FROM_MTA_HEADER
 
 	$email_envoi = $GLOBALS['meta']["email_envoi"];
-	if (email_valide($email_envoi)) {
-		preg_match('/(@\S+)/', $email_envoi, $domain);
-		$mid = 'Message-Id: <' . time() . '_' . rand() . '_' . md5($email . $texte) . $domain[1] . ">\n";
-	} else {
+	if (!email_valide($email_envoi)) {
 		spip_log("Meta email_envoi invalide. Le mail sera probablement vu comme spam.");
 		$email_envoi = $email;
-		$mid = '';
 	}
+
 	if (!$from) $from = $email_envoi;
 
 	// ceci est la RegExp NO_REAL_NAME faisant hurler SpamAssassin
 	if (preg_match('/^["\s]*\<?\S+\@\S+\>?\s*$/', $from))
 		$from .= ' (' . str_replace(')','', translitteration(str_replace('@', ' at ', $from))) . ')';
-
-	// Et maintenant le champ From:
-	$headers .= "From: $from\n";
-
-	// indispensable pour les sites qui colle d'office From: serveur-http
-	// sauf si deja mis par l'envoyeur
-	if (strpos($headers,"Reply-To:")===FALSE)
-		$headers .= "Reply-To: $from\n";
-
-	$charset = $GLOBALS['meta']['charset'];
-
-	// Ajouter le Content-Type et consort s'il n'y est pas deja
-	if (strpos($headers, "Content-Type: ") === false)
-		$headers .=
-		"Content-Type: text/plain; charset=$charset\n".
-		"Content-Transfer-Encoding: 8bit\n" .
-		"MIME-Version: 1.0\n";
-
-	$headers .= $mid;
 
 	// nettoyer les &eacute; &#8217, &emdash; etc...
 	// les 'cliquer ici' etc sont a eviter;  voir:
@@ -96,13 +75,24 @@ function inc_envoyer_mail_dist($email, $sujet, $texte, $from = "", $headers = ""
 	if (init_mb_string()) {
 		# un bug de mb_string casse mb_encode_mimeheader si l'encoding interne
 		# est UTF-8 et le charset iso-8859-1 (constate php5-mac ; php4.3-debian)
+		$charset = $GLOBALS['meta']['charset'];
 		mb_internal_encoding($charset);
 		$sujet = mb_encode_mimeheader($sujet, $charset, 'Q', "\n");
 		mb_internal_encoding('utf-8');
 	}
 
-	spip_log("mail $email\n$sujet\n$headers",'mails');
+	if (function_exists('wordwrap') && (preg_match(',multipart/mixed,',$headers) == 0))
+		$texte = wordwrap($texte);
 
+	list($headers, $texte) = mail_normaliser_headers($headers, $from, $email, $texte, $parts);
+
+	if (_OS_SERVEUR == 'windows') {
+		$texte = preg_replace ("@\r*\n@","\r\n", $texte);
+		$headers = preg_replace ("@\r*\n@","\r\n", $headers);
+		$sujet = preg_replace ("@\r*\n@","\r\n", $sujet);
+	}
+
+	spip_log("mail $email\n$sujet\n$headers",'mails');
 	// mode TEST : forcer l'email
 	if (defined('_TEST_EMAIL_DEST')) {
 		if (!_TEST_EMAIL_DEST)
@@ -111,18 +101,54 @@ function inc_envoyer_mail_dist($email, $sujet, $texte, $from = "", $headers = ""
 			$email = _TEST_EMAIL_DEST;
 	}
 
-	// Ajouter le \n final
-	if ($headers = trim($headers)) $headers .= "\n";
-	if (function_exists('wordwrap'))
-		$texte = wordwrap($texte);
-
-	if (_OS_SERVEUR == 'windows') {
-		$texte = preg_replace ("@\r*\n@","\r\n", $texte);
-		$headers = preg_replace ("@\r*\n@","\r\n", $headers);
-		$sujet = preg_replace ("@\r*\n@","\r\n", $sujet);
-	}
-
 	return @mail($email, $sujet, $texte, $headers);
 }
 
+function mail_normaliser_headers($headers, $from, $to, $texte, $parts)
+{
+	$charset = $GLOBALS['meta']['charset'];
+
+	// Ajouter le Content-Type et consort s'il n'y est pas deja
+	if (strpos($headers, "Content-Type: ") === false)
+		$type =
+		"Content-Type: text/plain;charset=\"$charset\";\n".
+		"Content-Transfer-Encoding: 8bit\n";
+	else $type = '';
+
+	// calculer un identifiant unique
+	preg_match('/@\S+/', $from, $domain);
+	$uniq = rand() . '_' . md5($to . $texte) . $domain[0];
+
+	// Si multi-part, s'en servir comme borne ...
+	if ($parts) {
+		$texte = "--$uniq\n$type\n" . $texte ."\n";
+		foreach ($parts as $part) {
+			$n = strlen($part[1]) . ($part[0] ? "\n" : '');
+			$e = join("\n", $part[0]);
+			$texte .= "\n--$uniq\nContent-Length: $n$e\n\n" . $part[1];
+		}
+		$texte .= "\n\n--$uniq--\n";
+		// Si boundary n'est pas entre guillemets,
+		// elle est comprise mais le charset est ignoree !
+		$type = "Content-Type: multipart/mixed; boundary=\"$uniq\"\n";
+	}
+
+	// .. et s'en servir pour plaire a SpamAssassin
+
+	$mid = 'Message-Id: <' . $uniq . ">";
+
+	// indispensable pour les sites qui collent d'office From: serveur-http
+	// sauf si deja mis par l'envoyeur
+	$rep = (strpos($headers,"Reply-To:")!==FALSE) ? '' : "Reply-To: $from\n";
+
+	// Nettoyer les en-tetes envoyees
+	// Ajouter le \n final
+	if (strlen($headers = trim($headers))) $headers .= "\n";
+
+	// Et mentionner l'indeboulonable nomenclature ratee 
+
+	$headers .= "From: $from\n$type$rep$mid\nMIME-Version: 1.0\n";
+
+	return array($headers, $texte);
+}
 ?>

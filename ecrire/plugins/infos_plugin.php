61ef9e535ce78d8252ce0d255e8b0825c4ae6a35
eric <eric@smellup.net>
2011-08-06 20:27:05 +0000

    Manquait l'include nécessaire à la fonction propre()
    git-svn-id: svn://trac.rezo.net/spip/spip@18308 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/plugins/infos_plugin.php
+++ ecrire/plugins/infos_plugin.php
@@ -22,6 +22,8 @@ function plugins_infos_plugin($desc, $plug='', $dir_plugins=_DIR_PLUGINS) {
 	$verifie_conformite($plug, $arbre, $dir_plugins);
 
 	include_spip('inc/charsets');
+	include_spip('inc/texte');
+	
 	if (isset($arbre['categorie']))
 		$ret['categorie'] = trim(spip_xml_aplatit($arbre['categorie']));
 	if (isset($arbre['nom']))

ce95d2c2c069d17a793725f5752f7de0393ad628
marcimat <marcimat@rezo.net>
2011-08-21 13:40:00 +0000

    Rendre un peu plus générique la sélection des contenus sur le chargement d'un formulaire d'édition, notamment lors d'une création d'une traduction. On généralise les tests de inc/article_select.php dans inc/editer_select.php : si l'objet nouveau demandé est la traduction d'un autre, on charge le contenu de la traduction ; si un champ id_rubrique est présente, on tente de trouver une rubrique dans la langue du rédacteur pour accueillir l'objet si la configuration du multilinguisme est basée sur les rubriques.
    git-svn-id: svn://trac.rezo.net/spip/spip@18362 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/article_select.php
+++ ecrire/inc/article_select.php
@@ -12,105 +12,29 @@
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
-include_spip('inc/autoriser'); // necessaire si appel de l'espace public
+include_spip('inc/editer_select');
+
 
 // Recupere les donnees d'un article pour composer un formulaire d'edition
-// (utilise par exec/article_edit)
 // id_article = numero d'article existant
 // id_rubrique = ou veut-on l'installer (pas obligatoire)
 // lier_trad = l'associer a l'article numero $lier_trad
 // new=oui = article a creer si on valide le formulaire
 // http://doc.spip.org/@inc_article_select_dist
 function inc_article_select_dist($id_article, $id_rubrique=0, $lier_trad=0) {
-	global $connect_id_rubrique, $spip_lang; 
-
-	if (is_numeric($id_article))
-		return sql_fetsel("*", "spip_articles", "id_article=$id_article");
-
-	// id_article non numerique, c'est une demande de creation.
-	// Si c'est une demande de nouvelle traduction, init specifique
-	if ($lier_trad){
-		$row = article_select_trad($lier_trad, $id_rubrique);
-		$row['statut'] = ''; // le nouvel article n'a pas encore de statut !
-	}
-	else {
-		$row['titre'] = '';//filtrer_entites(_T('info_nouvel_article'));
-		//$row['onfocus'] = " onfocus=\"if(!antifocus){this.value='';antifocus=true;}\"";
-		$row['id_rubrique'] = $id_rubrique;
-	}
-
-	// appel du script a la racine, faut choisir 
-	// admin restreint ==> sa premiere rubrique
-	// autre ==> la derniere rubrique cree
-	if (!$row['id_rubrique']) {
-		if ($connect_id_rubrique)
-			$row['id_rubrique'] = $id_rubrique = $connect_id_rubrique[0]; 
-		else {
-			$row_rub = sql_fetsel("id_rubrique", "spip_rubriques", "", "", "id_rubrique DESC", 1);
-			$row['id_rubrique'] = $id_rubrique = $row_rub['id_rubrique'];
-		}
-		if (!autoriser('creerarticledans','rubrique',$row['id_rubrique'] )){
-			// manque de chance, la rubrique n'est pas autorisee, on cherche un des secteurs autorises
-			$res = sql_select("id_rubrique", "spip_rubriques", "id_parent=0");
-			while (!autoriser('creerarticledans','rubrique',$row['id_rubrique'] ) && $row_rub = sql_fetch($res)){
-				$row['id_rubrique'] = $row_rub['id_rubrique'];
-			}
-		}
-	}
-
-	// recuperer le secteur, pour affecter les bons champs extras
-	if (!$row['id_secteur']) {
-		$row_rub = sql_getfetsel("id_secteur", "spip_rubriques", "id_rubrique=" . sql_quote($id_rubrique));
-		$row['id_secteur'] = $row_rub;
-	}
-
-	return $row;
+	return select_objet('article', $id_article, $id_rubrique, $lier_trad, 'titre');
 }
+
 
 //
 // Si un article est demande en creation (new=oui) avec un lien de trad,
 // on initialise les donnees de maniere specifique
 //
-// http://doc.spip.org/@article_select_trad
-function article_select_trad($lier_trad, $id_rubrique=0) {
-	// Recuperer les donnees de l'article original
-	$row = sql_fetsel("*", "spip_articles", "id_article=$lier_trad");
-	if ($row) {
-		$row['titre'] = filtrer_entites(_T('info_nouvelle_traduction')).' '.$row["titre"];
-
-	} else $row = array();
-	if ($id_rubrique) {
-		$row['id_rubrique'] = $id_rubrique;
-		return $row;
-	}
-	$id_rubrique = $row['id_rubrique'];
-	// Regler la langue, si possible, sur celle du redacteur
-	// Cela implique souvent de choisir une rubrique ou un secteur
-	if (in_array($GLOBALS['spip_lang'],
-	explode(',', $GLOBALS['meta']['langues_multilingue']))) {
-		// Si le menu de langues est autorise sur les articles,
-		// on peut changer la langue quelle que soit la rubrique
-		// donc on reste dans la meme rubrique
-		if (in_array('spip_articles', explode(',',$GLOBALS['meta']['multi_objets'] == 'oui'))) {
-			$row['id_rubrique'] = $row['id_rubrique']; # explicite :-)
-		}
-		else if ($GLOBALS['meta']['multi_rubriques'] == 'oui') {
-			// Sinon, chercher la rubrique la plus adaptee pour
-			// accueillir l'article dans la langue du traducteur
-			if ($GLOBALS['meta']['multi_secteurs'] == 'oui') {
-				$id_parent = 0;
-			} else {
-				// on cherche une rubrique soeur dans la bonne langue
-				$row_rub = sql_fetsel("id_parent", "spip_rubriques", "id_rubrique=$id_rubrique");
-
-				$id_parent = $row_rub['id_parent'];
-			}
-			$row_rub = sql_fetsel("id_rubrique", "spip_rubriques", "lang='".$GLOBALS['spip_lang']."' AND id_parent=$id_parent");
-			if ($row_rub)
-				$row['id_rubrique'] = $row_rub['id_rubrique'];
-		}
-	}
-	return $row;
+// (fonction facultative si pas de changement dans les traitements)
+function inc_article_select_trad_dist($id_article, $id_rubrique=0, $lier_trad=0) {
+	return select_objet_trad('article', $id_article, $id_rubrique, $lier_trad, 'titre');
 }
+
+
 
 ?>

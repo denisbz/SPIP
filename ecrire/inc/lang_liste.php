8eb05230fd126cff22ac037674036748c2262297
eric <eric@smellup.net>
2011-09-10 18:33:00 +0000

    Suppression d'un index en double es_mx_pop
    git-svn-id: svn://trac.rezo.net/spip/spip@18493 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/lang_liste.php
+++ ecrire/inc/lang_liste.php
@@ -60,7 +60,6 @@ $GLOBALS['codes_langues'] = array(
 	'en_sm' => "Smurf",
 	'eo' => "Esperanto",
 	'es' => "Espa&#241;ol",
-	'es_mx_pop' => "Mexicano a lo g&#252;ey",
 	'es_co' => "Colombiano",
 	'es_mx_pop' => "Mexicano a lo g&#252;ey",
 	'et' => "eesti",
